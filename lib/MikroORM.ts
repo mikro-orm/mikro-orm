aafab09f3f2e4cb530ab4c9f8a8e6e36e069fb5f
Martin Adámek <banan23@gmail.com>
2018-08-13 14:13:43 +0200

    Fix masking pw when logging connection status

--- lib/MikroORM.ts
+++ lib/MikroORM.ts
@@ -66,7 +66,7 @@ export class MikroORM {
   async connect(): Promise<Db> {
     this.client = await MongoClient.connect(this.options.clientUrl as string, { useNewUrlParser: true });
     this.db = this.client.db(this.options.dbName);
-    const clientUrl = this.options.clientUrl.replace(/\/\/(\w+):(\w+)@/, '//$1:*****@');
+    const clientUrl = this.options.clientUrl.replace(/\/\/([^:]+):(\w+)@/, '//$1:*****@');
     this.options.logger(`MikroORM: successfully connected to database ${this.options.dbName} on ${clientUrl}`);
 
     return this.db;
