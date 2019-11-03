0f96591a0905d8ef755b20869c9aaf6f1b2d2764
Ranvir Singh <ranvir.singh1114@gmail.com>
2019-11-04 01:51:20 +0530

    docs: add database creation guide to CONTRIBUTION file (#219)

---
+++ tests/scripts/database_setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+echo "Creating MySQL database."
+MYSQL_CONTAINER_NAME=$(docker ps --format="{{.Names}}" | grep 'mysql')
+if [[ -z "$MYSQL_CONTAINER_NAME" ]]; then
+    echo "MySQL container not running. Please check the name of the container."
+    exit
+fi
+docker exec $MYSQL_CONTAINER_NAME bash -c "mysql -u root -e 'CREATE DATABASE IF NOT EXISTS mikro_orm_test;'"
+echo "Creating PostgreSQL database."
+POSTGRES_CONTAINER_NAME=$(docker ps --format="{{.Names}}" | grep 'postgre')
+if [[ -z "$POSTGRES_CONTAINER_NAME" ]]; then
+    echo "PostgreSQL container not running. Please check the name of the container."
+    exit
+fi
+docker exec $POSTGRES_CONTAINER_NAME bash -c "psql -c 'CREATE DATABASE mikro_orm_test;' -U postgres"
