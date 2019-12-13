f87cb966011b784540177fb9ff526759ef092ce8
Martin Adámek <banan23@gmail.com>
2019-12-13 10:34:25 +0100

    fix(sql): support uuid like PKs in M:N references
    Closes #268

---
+++ tests/issues/GH268.test.ts
@@ -0,0 +1,75 @@
+import { unlinkSync } from 'fs';
+import { v4 } from 'uuid';
+
+import { Collection, Entity, ManyToMany, MikroORM, PrimaryKey, Property, ReflectMetadataProvider, UuidEntity } from '../../lib';
+import { BASE_DIR } from '../bootstrap';
+import { SqliteDriver } from '../../lib/drivers/SqliteDriver';
+
+@Entity()
+export class A implements UuidEntity<A> {
+
+  @PrimaryKey()
+  uuid: string = v4();
+
+  @Property()
+  name!: string;
+
+  @ManyToMany(() => B, b => b.aCollection)
+  bCollection = new Collection<B>(this);
+
+}
+
+@Entity()
+export class B implements UuidEntity<B> {
+
+  @PrimaryKey()
+  uuid: string = v4();
+
+  @Property()
+  name!: string;
+
+  @ManyToMany(() => A, undefined, { fixedOrder: true })
+  aCollection = new Collection<A>(this);
+
+}
+
+describe('GH issue 268', () => {
+
+  let orm: MikroORM<SqliteDriver>;
+
+  beforeAll(async () => {
+    orm = await MikroORM.init({
+      entities: [A, B],
+      dbName: BASE_DIR + '/../temp/mikro_orm_test_gh268.db',
+      debug: false,
+      highlight: false,
+      type: 'sqlite',
+      metadataProvider: ReflectMetadataProvider,
+      cache: { enabled: false },
+    });
+    await orm.getSchemaGenerator().dropSchema();
+    await orm.getSchemaGenerator().createSchema();
+  });
+
+  afterAll(async () => {
+    await orm.close(true);
+    unlinkSync(orm.config.get('dbName'));
+  });
+
+  test('m:n with uuid PKs', async () => {
+    const a1 = new A();
+    a1.name = 'a1';
+    const a2 = new A();
+    a2.name = 'a2';
+    const a3 = new A();
+    a3.name = 'a3';
+    const b = new B();
+    b.name = 'b';
+    b.aCollection.add(a1, a2, a3);
+    await orm.em.persistAndFlush(b);
+
+    const res = await orm.em.getConnection().execute('select * from b_to_a');
+    expect(res[0]).toEqual({ id: 1, a_uuid: a1.uuid, b_uuid: b.uuid });
+  });
+
+});
