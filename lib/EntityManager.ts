b4879c33b28de0eed565331483de6b750acc122f
Martin Adámek <banan23@gmail.com>
2019-12-13 02:14:23 +0100

    feat(core): add `refresh` parameter to `FindOptions`
    This also fixes loading of 1:1 owners via populate when `autoJoinOneToOneOwner` option is disabled.
    
    Closes #269

--- lib/EntityManager.ts
+++ lib/EntityManager.ts
@@ -73,12 +73,12 @@ export class EntityManager<D extends IDatabaseDriver = IDatabaseDriver> {
     const ret: T[] = [];
 
     for (const data of results) {
-      const entity = this.merge<T>(entityName, data);
+      const entity = this.merge<T>(entityName, data, options.refresh);
       ret.push(entity);
     }
 
     const unique = Utils.unique(ret);
-    await this.entityLoader.populate(entityName, unique, options.populate || [], where, options.orderBy || {});
+    await this.entityLoader.populate(entityName, unique, options.populate || [], where, options.orderBy || {}, options.refresh);
 
     return unique;
   }
@@ -393,7 +393,7 @@ export class EntityManager<D extends IDatabaseDriver = IDatabaseDriver> {
       await this.lock(entity, options.lockMode, options.lockVersion);
     }
 
-    await this.entityLoader.populate(entity.constructor.name, [entity], options.populate || [], where, options.orderBy || {});
+    await this.entityLoader.populate(entity.constructor.name, [entity], options.populate || [], where, options.orderBy || {}, options.refresh);
 
     return entity;
   }
@@ -409,6 +409,7 @@ export interface FindOptions {
   orderBy?: QueryOrderMap;
   limit?: number;
   offset?: number;
+  refresh?: boolean;
   fields?: string[];
 }
 
