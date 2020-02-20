885299642897c4f86f2298cb4f23e048520b8aad
Martin Adamek <martin.adamek@green-light.com>
2020-02-20 10:07:18 +0100

    docs: fix release notes links on versions page

--- docs/src/pages/versions.js
+++ docs/src/pages/versions.js
@@ -13,13 +13,14 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Link from '@docusaurus/Link';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
+import pkg from '../../../package.json';
 import versions from '../../versions.json';
 
 function Version() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
-  const latestVersion = versions[0];
-  const pastVersions = versions.filter(version => version !== latestVersion);
+  const latestVersion = pkg.version;
+  const pastVersions = versions.filter(version => version !== versions[0]).map(v => v + '.0');
   const repoUrl = `https://github.com/${siteConfig.organizationName}/${siteConfig.projectName}`;
   return (
     <Layout
