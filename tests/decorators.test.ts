b069d47bec412caa2fa24e35c858b47988984e14
Martin Adámek <banan23@gmail.com>
2020-04-17 19:38:23 +0200

    feat(discovery): use both entity name and path as key in Metadat… (#488)
    Decorators are using static MetadataStorage, now the keys includes also the path to entity, so technically it is possible to have multiple entities with the same name (although not in the same ORM context, but with multiple ORM instances).
    
    This should also fix issues with HMR and "multiple property decorators used" validation error.

--- tests/decorators.test.ts
+++ tests/decorators.test.ts
@@ -1,4 +1,4 @@
-import { ManyToMany, ManyToOne, OneToMany, OneToOne, Property, MetadataStorage, ReferenceType } from '@mikro-orm/core';
+import { ManyToMany, ManyToOne, OneToMany, OneToOne, Property, MetadataStorage, ReferenceType, Utils } from '@mikro-orm/core';
 import { Test } from './entities';
 
 class Test2 {}
@@ -9,39 +9,46 @@ class Test6 {}
 
 describe('decorators', () => {
 
+  const lookupPathFromDecorator = jest.spyOn(Utils, 'lookupPathFromDecorator');
+  lookupPathFromDecorator.mockReturnValue('/path/to/entity');
+
   test('ManyToMany', () => {
     const storage = MetadataStorage.getMetadata();
+    const key = 'Test2-' + Utils.hash('/path/to/entity');
     ManyToMany({ entity: () => Test })(new Test2(), 'test0');
-    expect(storage.Test2.properties.test0).toMatchObject({ reference: ReferenceType.MANY_TO_MANY, name: 'test0' });
-    expect(storage.Test2.properties.test0.entity()).toBe(Test);
+    expect(storage[key].properties.test0).toMatchObject({ reference: ReferenceType.MANY_TO_MANY, name: 'test0' });
+    expect(storage[key].properties.test0.entity()).toBe(Test);
   });
 
   test('ManyToOne', () => {
-    expect(() => ManyToOne(() => Test, { fk: 'test' } as any)(new Test3(), 'test1')).toThrowError(`@ManyToOne({ fk })' is deprecated, use 'inversedBy' instead in 'Test3.test1`);
     const storage = MetadataStorage.getMetadata();
+    const key = 'Test3-' + Utils.hash('/path/to/entity');
     ManyToOne({ entity: () => Test })(new Test3(), 'test1');
-    expect(storage.Test3.properties.test1).toMatchObject({ reference: ReferenceType.MANY_TO_ONE, name: 'test1' });
-    expect(storage.Test3.properties.test1.entity()).toBe(Test);
+    expect(storage[key].properties.test1).toMatchObject({ reference: ReferenceType.MANY_TO_ONE, name: 'test1' });
+    expect(storage[key].properties.test1.entity()).toBe(Test);
   });
 
   test('OneToOne', () => {
     const storage = MetadataStorage.getMetadata();
+    const key = 'Test6-' + Utils.hash('/path/to/entity');
     OneToOne({ entity: () => Test, inversedBy: 'test5' } as any)(new Test6(), 'test1');
-    expect(storage.Test6.properties.test1).toMatchObject({ reference: ReferenceType.ONE_TO_ONE, name: 'test1', inversedBy: 'test5' });
-    expect(storage.Test6.properties.test1.entity()).toBe(Test);
+    expect(storage[key].properties.test1).toMatchObject({ reference: ReferenceType.ONE_TO_ONE, name: 'test1', inversedBy: 'test5' });
+    expect(storage[key].properties.test1.entity()).toBe(Test);
   });
 
   test('OneToMany', () => {
     const storage = MetadataStorage.getMetadata();
+    const key = 'Test4-' + Utils.hash('/path/to/entity');
     OneToMany({ entity: () => Test, mappedBy: 'test' } as any)(new Test4(), 'test2');
-    expect(storage.Test4.properties.test2).toMatchObject({ reference: ReferenceType.ONE_TO_MANY, name: 'test2', mappedBy: 'test' });
-    expect(storage.Test4.properties.test2.entity()).toBe(Test);
+    expect(storage[key].properties.test2).toMatchObject({ reference: ReferenceType.ONE_TO_MANY, name: 'test2', mappedBy: 'test' });
+    expect(storage[key].properties.test2.entity()).toBe(Test);
   });
 
   test('Property', () => {
     const storage = MetadataStorage.getMetadata();
+    const key = 'Test5-' + Utils.hash('/path/to/entity');
     Property()(new Test5(), 'test3');
-    expect(storage.Test5.properties.test3).toMatchObject({ reference: ReferenceType.SCALAR, name: 'test3' });
+    expect(storage[key].properties.test3).toMatchObject({ reference: ReferenceType.SCALAR, name: 'test3' });
   });
 
 });
