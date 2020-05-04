fe3806a442fb6b552e1820fbb03741fb5a2ceaa3
Martin Adámek <banan23@gmail.com>
2020-05-04 18:07:11 +0200

    feat(mongo): add `getCollection()` method to `MongoEntityManager`
    shortcut for `em.getConnnection().getCollection()`

--- packages/mongodb/src/MongoEntityManager.ts
+++ packages/mongodb/src/MongoEntityManager.ts
@@ -1,6 +1,7 @@
 import { AnyEntity, EntityManager, EntityName, EntityRepository, Utils } from '@mikro-orm/core';
 import { MongoDriver } from './MongoDriver';
 import { MongoEntityRepository } from './MongoEntityRepository';
+import { Collection } from 'mongodb';
 
 /**
  * @inheritDoc
@@ -13,6 +14,10 @@ export class MongoEntityManager<D extends MongoDriver = MongoDriver> extends Ent
   async aggregate(entityName: EntityName<AnyEntity>, pipeline: any[]): Promise<any[]> {
     entityName = Utils.className(entityName);
     return this.getDriver().aggregate(entityName, pipeline);
+  }
+
+  getCollection(entityName: EntityName<AnyEntity>): Collection {
+    return this.getConnection().getCollection(entityName);
   }
 
   getRepository<T extends AnyEntity<T>, U extends EntityRepository<T> = MongoEntityRepository<T>>(entityName: EntityName<T>): U {
