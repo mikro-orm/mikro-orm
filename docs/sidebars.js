f786088919abd89dc6251274b2977ea92e388330
Will Soto <willsoto@users.noreply.github.com>
2020-05-27 09:07:34 -0400

    Update sidebars.js

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -28,7 +28,7 @@ module.exports = {
       'entity-helper',
       'property-validation',
       'lifecycle-hooks',
-      'loading-strategy',
+      'loading-strategies',
       'naming-strategy',
       'custom-types',
       'entity-schema',
