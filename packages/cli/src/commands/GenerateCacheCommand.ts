acfb4796a4c717ff56399c480e4711fec387de5a
Kevin Brown <kevin.brown@techin.site>
2020-07-17 18:47:55 +1000

    refactor(cli): warn users when cache is disabled (#662)

--- packages/cli/src/commands/GenerateCacheCommand.ts
+++ packages/cli/src/commands/GenerateCacheCommand.ts
@@ -13,6 +13,12 @@ export class GenerateCacheCommand implements CommandModule {
    */
   async handler(args: Arguments) {
     const config = await CLIHelper.getConfiguration(false);
+
+    if (!config.get('cache').enabled) {
+      CLIHelper.dump(chalk.red('Metadata cache is disabled in your configuration. Set cache.enabled to true to use this command.'));
+      return;
+    }
+
     config.set('logger', CLIHelper.dump.bind(null));
     config.set('debug', true);
     const discovery = new MetadataDiscovery(MetadataStorage.init(), config.getDriver().getPlatform(), config);
--- tests/cli/GenerateCacheCommand.test.ts
+++ tests/cli/GenerateCacheCommand.test.ts
@@ -2,11 +2,12 @@ import { Configuration, MetadataDiscovery, MetadataStorage } from '@mikro-orm/co
 import { CLIHelper } from '@mikro-orm/cli';
 import { GenerateCacheCommand } from '../../packages/cli/src/commands/GenerateCacheCommand';
 
+(global as any).console.log = jest.fn();
+
 const getConfigurationMock = jest.spyOn(CLIHelper, 'getConfiguration');
-getConfigurationMock.mockResolvedValue(new Configuration({ type: 'mysql', getDriver: () => ({ getPlatform: jest.fn() }) } as any, false));
+getConfigurationMock.mockResolvedValue(new Configuration({ type: 'mysql', cache: { enabled: true }, getDriver: () => ({ getPlatform: jest.fn() }) } as any, false));
 const discoverMock = jest.spyOn(MetadataDiscovery.prototype, 'discover');
 discoverMock.mockResolvedValue({} as MetadataStorage);
-(global as any).console.log = jest.fn();
 
 describe('GenerateCacheCommand', () => {
 
@@ -19,4 +20,15 @@ describe('GenerateCacheCommand', () => {
     expect(discoverMock.mock.calls[0][0]).toBe(false);
   });
 
+  test('handler throws when cache is disabled', async () => {
+    getConfigurationMock.mockResolvedValue(new Configuration({ type: 'mysql', cache: { enabled: false }, getDriver: () => ({ getPlatform: jest.fn() }) } as any, false));
+    discoverMock.mockReset();
+    discoverMock.mockResolvedValue({} as MetadataStorage);
+
+    const cmd = new GenerateCacheCommand();
+
+    expect(discoverMock.mock.calls.length).toBe(0);
+    await expect(cmd.handler({} as any)).resolves.toBeUndefined();
+    expect(discoverMock.mock.calls.length).toBe(0);
+  });
 });
