60f3d35e0aa6649dc2f6c7387d08e4e35fc2b4ce
Martin Adámek <banan23@gmail.com>
2020-08-09 08:50:01 +0200

    test: improve coverage

--- tests/issues/GH349.test.ts
+++ tests/issues/GH349.test.ts
@@ -67,6 +67,7 @@ describe('GH issue 349', () => {
       clientUrl: 'mongodb://localhost:27017,localhost:27018,localhost:27019/mikro-orm-test?replicaSet=rs0',
       type: 'mongo',
       debug: ['discovery'],
+      logger: i => i,
     });
   });
 
