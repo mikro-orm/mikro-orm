9800dc114ae32d31bb3621fc771a8eb5324ae044
Martin Adámek <banan23@gmail.com>
2020-09-10 22:35:32 +0200

    feat(migrations): do not use ts-morph in migrations
    There was no real need for ts-morph here, and it brought the full TS
    as a dependency. Ideally we should do this also for entity generator,
    so we could then install ts-morph only for actual reflection.

--- packages/migrations/src/MigrationGenerator.ts
+++ packages/migrations/src/MigrationGenerator.ts
@@ -1,17 +1,12 @@
 import { ensureDir, writeFile } from 'fs-extra';
-import { CodeBlockWriter, IndentationText, Project, QuoteKind } from 'ts-morph';
 import { MigrationsOptions, NamingStrategy, Utils } from '@mikro-orm/core';
 import { AbstractSqlDriver } from '@mikro-orm/knex';
 
 export class MigrationGenerator {
 
-  private readonly project = new Project();
-
   constructor(protected readonly driver: AbstractSqlDriver,
               protected readonly namingStrategy: NamingStrategy,
-              protected readonly options: MigrationsOptions) {
-    this.project.manipulationSettings.set({ quoteKind: QuoteKind.Single, indentationText: IndentationText.TwoSpaces });
-  }
+              protected readonly options: MigrationsOptions) { }
 
   async generate(diff: string[], path?: string): Promise<[string, string]> {
     path = Utils.normalizePath(path || this.options.path!);
@@ -19,58 +14,51 @@ export class MigrationGenerator {
     const timestamp = new Date().toISOString().replace(/[-T:]|\.\d{3}z$/ig, '');
     const className = this.namingStrategy.classToMigrationName(timestamp);
     const fileName = `${this.options.fileName!(timestamp)}.${this.options.emit}`;
-    const migration = this.project.createSourceFile(path + '/' + fileName, writer => {
-      if (this.options.emit === 'js') {
-        this.generateJSMigrationFile(writer, className, diff);
-      } else {
-        this.generateTSMigrationFile(writer, className, diff);
-      }
-    });
-    const ret = migration.getFullText();
-    await writeFile(migration.getFilePath(), ret);
+    let ret: string;
+
+    if (this.options.emit === 'js') {
+      ret = this.generateJSMigrationFile(className, diff);
+    } else {
+      ret = this.generateTSMigrationFile(className, diff);
+    }
+
+    await writeFile(path + '/' + fileName, ret);
 
     return [ret, fileName];
   }
 
-  createStatement(writer: CodeBlockWriter, sql: string): void {
+  createStatement(sql: string, padLeft: number): string {
     if (sql) {
-      writer.writeLine(`this.addSql('${sql.replace(/['\\]/g, '\\\'')}');`);
-    } else {
-      writer.blankLine();
+      const padding = ' '.repeat(padLeft);
+      return `${padding}this.addSql('${sql.replace(/['\\]/g, '\\\'')}');\n`;
     }
+
+    return '\n';
   }
 
-  generateJSMigrationFile(writer: CodeBlockWriter, className: string, diff: string[]) {
-    writer.writeLine(`'use strict';`);
-    writer.writeLine(`Object.defineProperty(exports, '__esModule', { value: true });`);
-    writer.writeLine(`const Migration = require('@mikro-orm/migrations').Migration;`);
-    writer.blankLine();
-    writer.write(`class ${className} extends Migration`);
+  generateJSMigrationFile(className: string, diff: string[]): string {
+    let ret = `'use strict';\n`;
+    ret += `Object.defineProperty(exports, '__esModule', { value: true });\n`;
+    ret += `const Migration = require('@mikro-orm/migrations').Migration;\n\n`;
+    ret += `class ${className} extends Migration {\n\n`;
+    ret += `  async up() {\n`;
+    diff.forEach(sql => ret += this.createStatement(sql, 4));
+    ret += `  }\n\n`;
+    ret += `}\n`;
+    ret += `exports.${className} = ${className};\n`;
 
-    writer.block(() => {
-      writer.blankLine();
-      writer.write(`async up()`);
-      writer.block(() => diff.forEach(sql => this.createStatement(writer, sql)));
-      writer.blankLine();
-    });
-
-    writer.writeLine(`exports.${className} = ${className};`);
-    writer.write('');
+    return ret;
   }
 
-  generateTSMigrationFile(writer: CodeBlockWriter, className: string, diff: string[]) {
-    writer.writeLine(`import { Migration } from '@mikro-orm/migrations';`);
-    writer.blankLine();
-    writer.write(`export class ${className} extends Migration`);
+  generateTSMigrationFile(className: string, diff: string[]): string {
+    let ret = `import { Migration } from '@mikro-orm/migrations';\n\n`;
+    ret += `export class ${className} extends Migration {\n\n`;
+    ret += `  async up(): Promise<void> {\n`;
+    diff.forEach(sql => ret += this.createStatement(sql, 4));
+    ret += `  }\n\n`;
+    ret += `}\n`;
 
-    writer.block(() => {
-      writer.blankLine();
-      writer.write('async up(): Promise<void>');
-      writer.block(() => diff.forEach(sql => this.createStatement(writer, sql)));
-      writer.blankLine();
-    });
-
-    writer.write('');
+    return ret;
   }
 
 }
