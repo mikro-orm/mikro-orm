30f62ddbb6f5f307a3dc6fb1339ddb833dc85e1e
Martin Adámek <banan23@gmail.com>
2020-09-25 08:30:38 +0200

    chore: do not tag with `bug` label
    People like to call everything bugs, which makes me sad, so let's remove that bit, lol.

--- tests/cli-config.ts
+++ tests/cli-config.ts
@@ -1,13 +1,15 @@
-import { JavaScriptMetadataProvider } from '@mikro-orm/core';
+import { JavaScriptMetadataProvider, Options } from '@mikro-orm/core';
 import { SqliteDriver } from '@mikro-orm/sqlite';
 import { BASE_DIR } from './bootstrap';
 
 const { BaseEntity4, Test3 } = require('./entities-js/index');
 
-export = {
+const config: Options = {
   entities: [Test3, BaseEntity4],
   dbName: './mikro_orm_test.db',
   baseDir: BASE_DIR,
   driver: SqliteDriver,
   metadataProvider: JavaScriptMetadataProvider,
 };
+
+export default config;
