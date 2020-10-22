225dab09de40773bd28150be5014f44fa6a0f4ff
Kevin Brown <kevin.brown@techin.site>
2020-10-22 17:36:01 +1100

    Added reproduction for transactional() with pool of 1 timeout.

---
+++ tests/issues/GH947.test.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryKey, MikroORM } from '@mikro-orm/core';
+
+@Entity()
+class A {
+
+  @PrimaryKey()
+  id!: number;
+
+}
+
+describe('GH issue 947', () => {
+
+  test(`transactional works with a single postgres connection`, async () => {
+    const orm = await MikroORM.init({
+      entities: [A],
+      dbName: `mikro_orm_test_gh_947`,
+      type: 'postgresql',
+      pool: {
+        min: 1,
+        max: 1,
+      },
+    });
+    await orm.getSchemaGenerator().ensureDatabase();
+    await orm.getSchemaGenerator().dropSchema();
+    await orm.getSchemaGenerator().createSchema();
+
+    await orm.em.transactional(async () => {
+      await orm.em.find(A, {});
+    });
+
+    await orm.close();
+
+  });
+
+});
