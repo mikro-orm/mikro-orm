33574e8b46235637128f516ce83d290c57e2a7ba
Jason Wilson <jrwilson@gmail.com>
2021-01-24 02:20:35 -0700

    fix(entity-generator): emit collection name in decorator (#1338)
    Updates the entity generator to emit the collection name when it doesn't match the namingStrategy output.
    
    Closes: #1328

--- packages/entity-generator/src/SourceFile.ts
+++ packages/entity-generator/src/SourceFile.ts
@@ -12,7 +12,7 @@ export class SourceFile {
 
   generate(): string {
     this.coreImports.add('Entity');
-    let ret = `@Entity()\n`;
+    let ret = `@Entity(${this.getCollectionDecl()})\n`;
 
     this.meta.indexes.forEach(index => {
       this.coreImports.add('Index');
@@ -52,6 +52,12 @@ export class SourceFile {
 
   getBaseName() {
     return this.meta.className + '.ts';
+  }
+
+  private getCollectionDecl() {
+    const needsCollection = this.meta.collection !== this.namingStrategy.classToTableName(this.meta.className);
+
+    return needsCollection ? `{ collection: '${this.meta.collection}' }` : '';
   }
 
   private getPropertyDefinition(prop: EntityProperty, padLeft: number): string {
