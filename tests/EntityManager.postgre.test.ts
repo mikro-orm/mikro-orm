317189d34de0e8d9b789e6c40b6766bf237828bb
patrick <patrick.pircher@ibm.com>
2021-02-22 09:25:24 +0100

    fix

--- tests/EntityManager.postgre.test.ts
+++ tests/EntityManager.postgre.test.ts
@@ -1721,6 +1721,8 @@ describe('EntityManagerPostgre multiple schemas', () => {
   afterAll(async () => {
     await orm1.em.getConnection().execute(`drop schema ${orm1.em.getConnection().getSchema()} cascade`);
     await orm2.em.getConnection().execute(`drop schema ${orm2.em.getConnection().getSchema()} cascade`);
+    await orm1.close();
+    await orm2.close();
   });
 
   it('migrate correctly', async () => {
