0b0cdab68d7d7693115ab613aaa0e137b689c341
Martin Adámek <banan23@gmail.com>
2021-03-15 18:19:17 +0100

    chore: fix tests

--- packages/core/src/utils/Configuration.ts
+++ packages/core/src/utils/Configuration.ts
@@ -252,7 +252,7 @@ export class Configuration<D extends IDatabaseDriver = IDatabaseDriver> {
       throw new Error('No platform type specified, please fill in `type` or provide custom driver class in `driver` option. Available platforms types: ' + inspect(Object.keys(Configuration.PLATFORMS)));
     }
 
-    if (!(this.options.type! in Configuration.PLATFORMS)) {
+    if (this.options.type && !(this.options.type in Configuration.PLATFORMS)) {
       throw new Error(`Invalid platform type specified: '${this.options.type}', please fill in valid \`type\` or provide custom driver class in \`driver\` option. Available platforms types: ${inspect(Object.keys(Configuration.PLATFORMS))}`);
     }
 
