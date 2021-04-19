ec4c933d62c572cb232cb9e909edfbe8cb24facd
Martin Adámek <banan23@gmail.com>
2021-04-19 21:38:08 +0200

    chore: fix tests

--- tests/features/joined-strategy.postgre.test.ts
+++ tests/features/joined-strategy.postgre.test.ts
@@ -355,7 +355,7 @@ describe('Joined loading strategy', () => {
     await orm.em.find(FooBaz2, { id: baz.id }, { populate: ['bar'], strategy: LoadStrategy.JOINED, flags: [QueryFlag.PAGINATE], limit: 3, offset: 10 });
     expect(mock.mock.calls).toHaveLength(5);
     expect(mock.mock.calls[4][0]).toMatch('select "e0"."id", "e0"."name", "e0"."version", ' +
-      '"b1"."id" as "b1__id", "b1"."name" as "b1__name", "b1"."baz_id" as "b1__baz_id", "b1"."foo_bar_id" as "b1__foo_bar_id", "b1"."version" as "b1__version", "b1"."blob" as "b1__blob", "b1"."array" as "b1__array", "b1"."object_property" as "b1__object_property", (select 123) as "b1__random", "b1"."id" as "bar_id" ' +
+      '"b1"."id" as "b1__id", "b1"."name" as "b1__name", "b1"."baz_id" as "b1__baz_id", "b1"."foo_bar_id" as "b1__foo_bar_id", "b1"."version" as "b1__version", "b1"."blob" as "b1__blob", "b1"."array" as "b1__array", "b1"."object" as "b1__object", (select 123) as "b1__random", "b1"."id" as "bar_id" ' +
       'from "foo_baz2" as "e0" ' +
       'left join "foo_bar2" as "b1" on "e0"."id" = "b1"."baz_id" ' +
       'where "e0"."id" in (' +
