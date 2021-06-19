c769871867a5307b74dce6746f14fb89af38e856
Martin Adámek <banan23@gmail.com>
2021-06-19 15:41:28 +0200

    fix(mysql): use current schema when reading enum definitions
    Closes #1923
    Closes #1866

--- packages/mysql-base/src/MySqlSchemaHelper.ts
+++ packages/mysql-base/src/MySqlSchemaHelper.ts
@@ -57,7 +57,7 @@ export class MySqlSchemaHelper extends SchemaHelper {
   }
 
   getListTablesSQL(): string {
-    return `select table_name as table_name from information_schema.tables where table_type = 'BASE TABLE' and table_schema = schema()`;
+    return `select table_name as table_name, nullif(table_schema, schema()) as schema_name, table_comment as table_comment from information_schema.tables where table_type = 'BASE TABLE' and table_schema = schema()`;
   }
 
   getRenameColumnSQL(tableName: string, from: Column, to: EntityProperty, idx = 0): string {
@@ -87,7 +87,7 @@ export class MySqlSchemaHelper extends SchemaHelper {
 
   async getEnumDefinitions(connection: AbstractSqlConnection, tableName: string, schemaName?: string): Promise<Dictionary> {
     const sql =  `select column_name as column_name, column_type as column_type from information_schema.columns
-      where data_type = 'enum' and table_name = '${tableName}'`;
+      where data_type = 'enum' and table_name = '${tableName}' and table_schema = database()`;
     const enums = await connection.execute<any[]>(sql);
 
     return enums.reduce((o, item) => {
