677fe614a167c8d56dc0467f03e8f54f7ffefd9d
Martin Adámek <banan23@gmail.com>
2021-09-11 11:10:14 +0200

    chore: use `mikro-orm` metapackage to track right numbers on GH
    `mikro-orm` is a metapackage. Originally it was used in v3 and lower, with v4 and monorepo rewrite,
    it served as a way to easy the transition to using `@mikro-orm/core` and other packages like `cli`.
    This caused issues with dependency resolutions and using `@mikro-orm/core` was always the encouraged
    way. In v5, the package has no dependencies and is used purely for tracking the right numbers on GH
    page and other stats (e.g. npmtrends), where people often compared other libraries with `mikro-orm`
    instead of `@mikro-orm/core`. The `@mikro-orm/core` now directly depends on this package, so the
    download numbers are propagated to the `mikro-orm` package too.

--- packages/mikro-orm/src/index.ts
+++ packages/mikro-orm/src/index.ts
@@ -1,5 +1,10 @@
-export * from '@mikro-orm/core';
-export * from '@mikro-orm/cli';
-export * from '@mikro-orm/entity-generator';
-export * from '@mikro-orm/migrations';
-export * from '@mikro-orm/reflection';
+// THIS PACKAGE SHOULD NOT BE USED DIRECTLY
+// use `@mikro-orm/core` instead
+
+// `mikro-orm` is a metapackage. Originally it was used in v3 and lower, with v4 and monorepo rewrite,
+// it served as a way to easy the transition to using `@mikro-orm/core` and other packages like `cli`.
+// This caused issues with dependency resolutions and using `@mikro-orm/core` was always the encouraged way.
+// In v5, the package has no dependencies and is used purely for tracking the right numbers on GH page and
+// other stats (e.g. npmtrends), where people often compared other libraries with `mikro-orm` instead of
+// `@mikro-orm/core`. The `@mikro-orm/core` now directly depends on this package, so the download numbers
+// are propagated to the `mikro-orm` package too.
