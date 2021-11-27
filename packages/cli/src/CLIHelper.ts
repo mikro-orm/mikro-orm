3cd49cd953bf8312d8a580e76ce83434892d8cc6
Martin Adámek <banan23@gmail.com>
2021-11-27 19:02:38 +0100

    refactor: allow using global context in CLI to fix running seeder

--- packages/cli/src/CLIHelper.ts
+++ packages/cli/src/CLIHelper.ts
@@ -17,6 +17,7 @@ export class CLIHelper {
 
   static async getORM(warnWhenNoEntities?: boolean, opts: Partial<Options> = {}): Promise<MikroORM> {
     const options = await CLIHelper.getConfiguration(warnWhenNoEntities, opts);
+    options.set('allowGlobalContext', true);
     const settings = await ConfigurationLoader.getSettings();
     options.getLogger().setDebugMode(false);
 
