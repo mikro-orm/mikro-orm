4ef2623680734403453961559c13383a979c8ae6
Martin Adámek <banan23@gmail.com>
2021-12-18 16:41:29 +0100

    perf(core): define `Reference` properties on prototype

--- packages/core/src/entity/Reference.ts
+++ packages/core/src/entity/Reference.ts
@@ -8,7 +8,6 @@ export class Reference<T extends AnyEntity<T>> {
   constructor(private entity: T) {
     this.set(entity);
     const meta = this.entity.__meta!;
-    Object.defineProperty(this, '__reference', { value: true });
 
     meta.primaryKeys.forEach(primaryKey => {
       Object.defineProperty(this, primaryKey, {
@@ -94,11 +93,6 @@ export class Reference<T extends AnyEntity<T>> {
     }
 
     this.entity = entity;
-    Object.defineProperty(this, '__meta', { value: this.entity.__meta!, writable: true });
-    Object.defineProperty(this, '__platform', { value: this.entity.__platform!, writable: true });
-    Object.defineProperty(this, '__helper', { value: this.entity.__helper!, writable: true });
-    Object.defineProperty(this, '$', { value: this.entity, writable: true });
-    Object.defineProperty(this, 'get', { value: () => this.entity, writable: true });
   }
 
   unwrap(): T {
@@ -130,3 +124,12 @@ export class Reference<T extends AnyEntity<T>> {
   }
 
 }
+
+Object.defineProperties(Reference.prototype, {
+  __reference: { value: true, enumerable: false },
+  __meta: { get() { return this.entity.__meta!; } },
+  __platform: { get() { return this.entity.__platform!; } },
+  __helper: { get() { return this.entity.__helper!; } },
+  $: { get() { return this.entity; } },
+  get: { get() { return () => this.entity; } },
+});
