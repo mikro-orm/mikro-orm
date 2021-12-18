mikro-orm/mikro-orm

commit 40367166f9e74a042e2f6314f31877f27a15a14d

feat(migrations): store migrations without extensions
Running migrations in production via node and ts-node is now handled the same.
This should actually not be breaking, as old format with extension is still
supported (e.g. they still can be rolled back), but newly logged migrations
will not contain the extension.

Closes #2239

packages/migrations/src/MigrationStorage.ts
@@ -1,4 +1,5 @@
 import type { MigrationsOptions, Transaction } from '@mikro-orm/core';
+import { Utils } from '@mikro-orm/core';
 import type { AbstractSqlDriver, Table } from '@mikro-orm/knex';
 import type { MigrationRow } from './typings';
 
@@ -14,15 +15,25 @@ export class MigrationStorage {
 
   async executed(): Promise<string[]> {
     const migrations = await this.getExecutedMigrations();
-    return migrations.map(row => row.name);
+    const ext = this.options.emit === 'js' || !Utils.detectTsNode() ? 'js' : 'ts';
+
+    return migrations.map(({ name }) => `${this.getMigrationName(name)}.${ext}`);
   }
 
   async logMigration(name: string): Promise<void> {
+    name = this.getMigrationName(name);
     await this.driver.nativeInsert(this.options.tableName!, { name }, { ctx: this.masterTransaction });
   }
 
   async unlogMigration(name: string): Promise<void> {
-    await this.driver.nativeDelete(this.options.tableName!, { name }, { ctx: this.masterTransaction });
+    const withoutExt = this.getMigrationName(name);
+    const qb = this.knex.delete().from(this.options.tableName!).where('name', 'in', [name, withoutExt]);
+
+    if (this.masterTransaction) {
+      qb.transacting(this.masterTransaction);
+    }
+
+    await this.connection.execute(qb);
   }
 
   async getExecutedMigrations(): Promise<MigrationRow[]> {
@@ -55,6 +66,11 @@ export class MigrationStorage {
 
   unsetMasterMigration() {
     delete this.masterTransaction;
+  }
+
+  protected getMigrationName(name: string) {
+    // strip extension
+    return name.replace(/\.\w+$/, '');
   }
 
 }

tests/features/migrations/Migrator.test.ts
@@ -199,9 +199,9 @@ describe('Migrator', () => {
     const storage = migrator.storage;
 
     await storage.ensureTable(); // creates the table
-    await storage.logMigration('test');
+    await storage.logMigration('test.ts'); // can have extension
     await expect(storage.getExecutedMigrations()).resolves.toMatchObject([{ name: 'test' }]);
-    await expect(storage.executed()).resolves.toEqual(['test']);
+    await expect(storage.executed()).resolves.toEqual(['test.ts']);
 
     await storage.ensureTable(); // table exists, no-op
     await storage.unlogMigration('test');

packages/core/src/utils/Configuration.ts
@@ -76,7 +76,7 @@ export class Configuration<D extends IDatabaseDriver = IDatabaseDriver> {
     migrations: {
       tableName: 'mikro_orm_migrations',
       path: './migrations',
-      pattern: /^[\w-]+\d+\.ts$/,
+      pattern: /^[\w-]+\d+\.[jt]s$/,
       transactional: true,
       disableForeignKeys: true,
       allOrNothing: true,

tests/features/migrations/Migrator.postgres.test.ts
@@ -219,7 +219,7 @@ describe('Migrator (postgres)', () => {
     await storage.ensureTable(); // creates the table
     await storage.logMigration('test');
     await expect(storage.getExecutedMigrations()).resolves.toMatchObject([{ name: 'test' }]);
-    await expect(storage.executed()).resolves.toEqual(['test']);
+    await expect(storage.executed()).resolves.toEqual(['test.ts']);
 
     await storage.ensureTable(); // table exists, no-op
     await storage.unlogMigration('test');

tests/features/migrations/Migrator.sqlite.test.ts
@@ -191,7 +191,7 @@ describe('Migrator (sqlite)', () => {
     await storage.ensureTable(); // creates the table
     await storage.logMigration('test');
     await expect(storage.getExecutedMigrations()).resolves.toMatchObject([{ name: 'test' }]);
-    await expect(storage.executed()).resolves.toEqual(['test']);
+    await expect(storage.executed()).resolves.toEqual(['test.ts']);
 
     await storage.ensureTable(); // table exists, no-op
     await storage.unlogMigration('test');

tests/features/migrations/__snapshots__/Migrator.postgres.test.ts.snap
@@ -677,7 +677,7 @@ Array [
   "select * from \\"mikro_orm_migrations\\" order by \\"id\\" asc",
   "begin",
   "commit",
-  "delete from \\"mikro_orm_migrations\\" where \\"name\\" = $1",
+  "delete from \\"mikro_orm_migrations\\" where \\"name\\" in ($1, $2)",
   "select 1 from pg_database where datname = 'mikro_orm_test'",
   "select table_name, nullif(table_schema, 'public') as schema_name, (select pg_catalog.obj_description(c.oid) from pg_catalog.pg_class c where c.oid = (select ('\\"' || table_schema || '\\".\\"' || table_name || '\\"')::regclass::oid) and c.relname = table_name) as table_comment from information_schema.tables where table_schema not like 'pg_%' and table_schema != 'information_schema' and table_name != 'geometry_columns' and table_name != 'spatial_ref_sys' and table_type != 'VIEW' order by table_name",
   "select * from \\"mikro_orm_migrations\\" order by \\"id\\" asc",
@@ -691,7 +691,7 @@ Array [
   "select * from \\"mikro_orm_migrations\\" order by \\"id\\" asc",
   "begin",
   "commit",
-  "delete from \\"mikro_orm_migrations\\" where \\"name\\" = $1",
+  "delete from \\"mikro_orm_migrations\\" where \\"name\\" in ($1, $2)",
   "select 1 from pg_database where datname = 'mikro_orm_test'",
   "select table_name, nullif(table_schema, 'public') as schema_name, (select pg_catalog.obj_description(c.oid) from pg_catalog.pg_class c where c.oid = (select ('\\"' || table_schema || '\\".\\"' || table_name || '\\"')::regclass::oid) and c.relname = table_name) as table_comment from information_schema.tables where table_schema not like 'pg_%' and table_schema != 'information_schema' and table_name != 'geometry_columns' and table_name != 'spatial_ref_sys' and table_type != 'VIEW' order by table_name",
   "select * from \\"mikro_orm_migrations\\" order by \\"id\\" asc",
@@ -711,7 +711,7 @@ Array [
   "select * from \\"mikro_orm_migrations\\" order by \\"id\\" asc",
   "begin",
   "commit",
-  "delete from \\"mikro_orm_migrations\\" where \\"name\\" = $1",
+  "delete from \\"mikro_orm_migrations\\" where \\"name\\" in ($1, $2)",
 ]
 `;
 
@@ -735,7 +735,7 @@ Array [
   "select * from \\"mikro_orm_migrations\\" order by \\"id\\" asc",
   "savepointtrx\\\\d+",
   "release savepointtrx\\\\d+",
-  "delete from \\"mikro_orm_migrations\\" where \\"name\\" = $1",
+  "delete from \\"mikro_orm_migrations\\" where \\"name\\" in ($1, $2)",
   "commit",
   "select 1 from pg_database where datname = 'mikro_orm_test'",
   "select table_name, nullif(table_schema, 'public') as schema_name, (select pg_catalog.obj_description(c.oid) from pg_catalog.pg_class c where c.oid = (select ('\\"' || table_schema || '\\".\\"' || table_name || '\\"')::regclass::oid) and c.relname = table_name) as table_comment from information_schema.tables where table_schema not like 'pg_%' and table_schema != 'information_schema' and table_name != 'geometry_columns' and table_name != 'spatial_ref_sys' and table_type != 'VIEW' order by table_name",
@@ -753,7 +753,7 @@ Array [
   "select * from \\"mikro_orm_migrations\\" order by \\"id\\" asc",
   "savepointtrx\\\\d+",
   "release savepointtrx\\\\d+",
-  "delete from \\"mikro_orm_migrations\\" where \\"name\\" = $1",
+  "delete from \\"mikro_orm_migrations\\" where \\"name\\" in ($1, $2)",
   "commit",
   "select 1 from pg_database where datname = 'mikro_orm_test'",
   "select table_name, nullif(table_schema, 'public') as schema_name, (select pg_catalog.obj_description(c.oid) from pg_catalog.pg_class c where c.oid = (select ('\\"' || table_schema || '\\".\\"' || table_name || '\\"')::regclass::oid) and c.relname = table_name) as table_comment from information_schema.tables where table_schema not like 'pg_%' and table_schema != 'information_schema' and table_name != 'geometry_columns' and table_name != 'spatial_ref_sys' and table_type != 'VIEW' order by table_name",
@@ -779,7 +779,7 @@ Array [
   "select * from \\"mikro_orm_migrations\\" order by \\"id\\" asc",
   "savepointtrx\\\\d+",
   "release savepointtrx\\\\d+",
-  "delete from \\"mikro_orm_migrations\\" where \\"name\\" = $1",
+  "delete from \\"mikro_orm_migrations\\" where \\"name\\" in ($1, $2)",
   "commit",
 ]
 `;
@@ -808,7 +808,7 @@ Array [
   "select * from \\"mikro_orm_migrations\\" order by \\"id\\" asc",
   "savepointtrx_xx",
   "release savepointtrx_xx",
-  "delete from \\"mikro_orm_migrations\\" where \\"name\\" = $1",
+  "delete from \\"mikro_orm_migrations\\" where \\"name\\" in ($1, $2)",
   "select 1 from pg_database where datname = 'mikro_orm_test'",
   "select table_name, nullif(table_schema, 'public') as schema_name, (select pg_catalog.obj_description(c.oid) from pg_catalog.pg_class c where c.oid = (select ('\\"' || table_schema || '\\".\\"' || table_name || '\\"')::regclass::oid) and c.relname = table_name) as table_comment from information_schema.tables where table_schema not like 'pg_%' and table_schema != 'information_schema' and table_name != 'geometry_columns' and table_name != 'spatial_ref_sys' and table_type != 'VIEW' order by table_name",
   "select * from \\"mikro_orm_migrations\\" order by \\"id\\" asc",
@@ -816,7 +816,7 @@ Array [
   "select * from \\"mikro_orm_migrations\\" order by \\"id\\" asc",
   "savepointtrx_xx",
   "release savepointtrx_xx",
-  "delete from \\"mikro_orm_migrations\\" where \\"name\\" = $1",
+  "delete from \\"mikro_orm_migrations\\" where \\"name\\" in ($1, $2)",
   "select 1 from pg_database where datname = 'mikro_orm_test'",
   "select table_name, nullif(table_schema, 'public') as schema_name, (select pg_catalog.obj_description(c.oid) from pg_catalog.pg_class c where c.oid = (select ('\\"' || table_schema || '\\".\\"' || table_name || '\\"')::regclass::oid) and c.relname = table_name) as table_comment from information_schema.tables where table_schema not like 'pg_%' and table_schema != 'information_schema' and table_name != 'geometry_columns' and table_name != 'spatial_ref_sys' and table_type != 'VIEW' order by table_name",
   "select * from \\"mikro_orm_migrations\\" order by \\"id\\" asc",

tests/features/migrations/__snapshots__/Migrator.sqlite.test.ts.snap
@@ -242,7 +242,7 @@ Array [
   "select * from \`mikro_orm_migrations\` order by \`id\` asc",
   "begin",
   "commit",
-  "delete from \`mikro_orm_migrations\` where \`name\` = ?",
+  "delete from \`mikro_orm_migrations\` where \`name\` in (?, ?)",
   "select name as table_name from sqlite_master where type = 'table' and name != 'sqlite_sequence' and name != 'geometry_columns' and name != 'spatial_ref_sys' union all select name as table_name from sqlite_temp_master where type = 'table' order by name",
   "select * from \`mikro_orm_migrations\` order by \`id\` asc",
   "begin",
@@ -254,7 +254,7 @@ Array [
   "select * from \`mikro_orm_migrations\` order by \`id\` asc",
   "begin",
   "commit",
-  "delete from \`mikro_orm_migrations\` where \`name\` = ?",
+  "delete from \`mikro_orm_migrations\` where \`name\` in (?, ?)",
   "select name as table_name from sqlite_master where type = 'table' and name != 'sqlite_sequence' and name != 'geometry_columns' and name != 'spatial_ref_sys' union all select name as table_name from sqlite_temp_master where type = 'table' order by name",
   "select * from \`mikro_orm_migrations\` order by \`id\` asc",
   "select * from \`mikro_orm_migrations\` order by \`id\` asc",
@@ -271,7 +271,7 @@ Array [
   "select * from \`mikro_orm_migrations\` order by \`id\` asc",
   "begin",
   "commit",
-  "delete from \`mikro_orm_migrations\` where \`name\` = ?",
+  "delete from \`mikro_orm_migrations\` where \`name\` in (?, ?)",
 ]
 `;
 
@@ -293,7 +293,7 @@ Array [
   "select * from \`mikro_orm_migrations\` order by \`id\` asc",
   "savepointtrx\\\\d+",
   "release savepointtrx\\\\d+",
-  "delete from \`mikro_orm_migrations\` where \`name\` = ?",
+  "delete from \`mikro_orm_migrations\` where \`name\` in (?, ?)",
   "commit",
   "select name as table_name from sqlite_master where type = 'table' and name != 'sqlite_sequence' and name != 'geometry_columns' and name != 'spatial_ref_sys' union all select name as table_name from sqlite_temp_master where type = 'table' order by name",
   "begin",
@@ -309,7 +309,7 @@ Array [
   "select * from \`mikro_orm_migrations\` order by \`id\` asc",
   "savepointtrx\\\\d+",
   "release savepointtrx\\\\d+",
-  "delete from \`mikro_orm_migrations\` where \`name\` = ?",
+  "delete from \`mikro_orm_migrations\` where \`name\` in (?, ?)",
   "commit",
   "select name as table_name from sqlite_master where type = 'table' and name != 'sqlite_sequence' and name != 'geometry_columns' and name != 'spatial_ref_sys' union all select name as table_name from sqlite_temp_master where type = 'table' order by name",
   "begin",
@@ -332,7 +332,7 @@ Array [
   "select * from \`mikro_orm_migrations\` order by \`id\` asc",
   "savepointtrx\\\\d+",
   "release savepointtrx\\\\d+",
-  "delete from \`mikro_orm_migrations\` where \`name\` = ?",
+  "delete from \`mikro_orm_migrations\` where \`name\` in (?, ?)",
   "commit",
 ]
 `;

tests/features/migrations/__snapshots__/Migrator.test.ts.snap
@@ -862,7 +862,7 @@ Array [
   "select * from \`mikro_orm_migrations\` order by \`id\` asc (via write connection '127.0.0.1')",
   "begin (via write connection '127.0.0.1')",
   "commit (via write connection '127.0.0.1')",
-  "delete from \`mikro_orm_migrations\` where \`name\` = ? (via write connection '127.0.0.1')",
+  "delete from \`mikro_orm_migrations\` where \`name\` in (?, ?) (via write connection '127.0.0.1')",
   "select 1 from information_schema.schemata where schema_name = 'mikro_orm_test' (via write connection '127.0.0.1')",
   "select table_name as table_name, nullif(table_schema, schema()) as schema_name, table_comment as table_comment from information_schema.tables where table_type = 'BASE TABLE' and table_schema = schema() (via write connection '127.0.0.1')",
   "select * from \`mikro_orm_migrations\` order by \`id\` asc (via write connection '127.0.0.1')",
@@ -876,7 +876,7 @@ Array [
   "select * from \`mikro_orm_migrations\` order by \`id\` asc (via write connection '127.0.0.1')",
   "begin (via write connection '127.0.0.1')",
   "commit (via write connection '127.0.0.1')",
-  "delete from \`mikro_orm_migrations\` where \`name\` = ? (via write connection '127.0.0.1')",
+  "delete from \`mikro_orm_migrations\` where \`name\` in (?, ?) (via write connection '127.0.0.1')",
   "select 1 from information_schema.schemata where schema_name = 'mikro_orm_test' (via write connection '127.0.0.1')",
   "select table_name as table_name, nullif(table_schema, schema()) as schema_name, table_comment as table_comment from information_schema.tables where table_type = 'BASE TABLE' and table_schema = schema() (via write connection '127.0.0.1')",
   "select * from \`mikro_orm_migrations\` order by \`id\` asc (via write connection '127.0.0.1')",
@@ -896,7 +896,7 @@ Array [
   "select * from \`mikro_orm_migrations\` order by \`id\` asc (via write connection '127.0.0.1')",
   "begin (via write connection '127.0.0.1')",
   "commit (via write connection '127.0.0.1')",
-  "delete from \`mikro_orm_migrations\` where \`name\` = ? (via write connection '127.0.0.1')",
+  "delete from \`mikro_orm_migrations\` where \`name\` in (?, ?) (via write connection '127.0.0.1')",
 ]
 `;
 
@@ -920,7 +920,7 @@ Array [
   "select * from \`mikro_orm_migrations\` order by \`id\` asc (via write connection '127.0.0.1')",
   "savepointtrx\\\\d+ (via write connection '127.0.0.1')",
   "release savepointtrx\\\\d+ (via write connection '127.0.0.1')",
-  "delete from \`mikro_orm_migrations\` where \`name\` = ? (via write connection '127.0.0.1')",
+  "delete from \`mikro_orm_migrations\` where \`name\` in (?, ?) (via write connection '127.0.0.1')",
   "commit (via write connection '127.0.0.1')",
   "select 1 from information_schema.schemata where schema_name = 'mikro_orm_test' (via write connection '127.0.0.1')",
   "select table_name as table_name, nullif(table_schema, schema()) as schema_name, table_comment as table_comment from information_schema.tables where table_type = 'BASE TABLE' and table_schema = schema() (via write connection '127.0.0.1')",
@@ -938,7 +938,7 @@ Array [
   "select * from \`mikro_orm_migrations\` order by \`id\` asc (via write connection '127.0.0.1')",
   "savepointtrx\\\\d+ (via write connection '127.0.0.1')",
   "release savepointtrx\\\\d+ (via write connection '127.0.0.1')",
-  "delete from \`mikro_orm_migrations\` where \`name\` = ? (via write connection '127.0.0.1')",
+  "delete from \`mikro_orm_migrations\` where \`name\` in (?, ?) (via write connection '127.0.0.1')",
   "commit (via write connection '127.0.0.1')",
   "select 1 from information_schema.schemata where schema_name = 'mikro_orm_test' (via write connection '127.0.0.1')",
   "select table_name as table_name, nullif(table_schema, schema()) as schema_name, table_comment as table_comment from information_schema.tables where table_type = 'BASE TABLE' and table_schema = schema() (via write connection '127.0.0.1')",
@@ -964,7 +964,7 @@ Array [
   "select * from \`mikro_orm_migrations\` order by \`id\` asc (via write connection '127.0.0.1')",
   "savepointtrx\\\\d+ (via write connection '127.0.0.1')",
   "release savepointtrx\\\\d+ (via write connection '127.0.0.1')",
-  "delete from \`mikro_orm_migrations\` where \`name\` = ? (via write connection '127.0.0.1')",
+  "delete from \`mikro_orm_migrations\` where \`name\` in (?, ?) (via write connection '127.0.0.1')",
   "commit (via write connection '127.0.0.1')",
 ]
 `;
@@ -993,7 +993,7 @@ Array [
   "select * from \`mikro_orm_migrations\` order by \`id\` asc (via write connection '127.0.0.1')",
   "savepointtrx_xx (via write connection '127.0.0.1')",
   "release savepointtrx_xx (via write connection '127.0.0.1')",
-  "delete from \`mikro_orm_migrations\` where \`name\` = ? (via write connection '127.0.0.1')",
+  "delete from \`mikro_orm_migrations\` where \`name\` in (?, ?) (via write connection '127.0.0.1')",
   "select 1 from information_schema.schemata where schema_name = 'mikro_orm_test' (via write connection '127.0.0.1')",
   "select table_name as table_name, nullif(table_schema, schema()) as schema_name, table_comment as table_comment from information_schema.tables where table_type = 'BASE TABLE' and table_schema = schema() (via write connection '127.0.0.1')",
   "select * from \`mikro_orm_migrations\` order by \`id\` asc (via write connection '127.0.0.1')",
@@ -1001,7 +1001,7 @@ Array [
   "select * from \`mikro_orm_migrations\` order by \`id\` asc (via write connection '127.0.0.1')",
   "savepointtrx_xx (via write connection '127.0.0.1')",
   "release savepointtrx_xx (via write connection '127.0.0.1')",
-  "delete from \`mikro_orm_migrations\` where \`name\` = ? (via write connection '127.0.0.1')",
+  "delete from \`mikro_orm_migrations\` where \`name\` in (?, ?) (via write connection '127.0.0.1')",
   "select 1 from information_schema.schemata where schema_name = 'mikro_orm_test' (via write connection '127.0.0.1')",
   "select table_name as table_name, nullif(table_schema, schema()) as schema_name, table_comment as table_comment from information_schema.tables where table_type = 'BASE TABLE' and table_schema = schema() (via write connection '127.0.0.1')",
   "select * from \`mikro_orm_migrations\` order by \`id\` asc (via write connection '127.0.0.1')",

commit ecf00d620fcb31020983301bb642c8ba18583766

fix(migrations): prevent excessive extension stripping
The original code stripped test.migration.ts to test instead of test.migration.
This resulted in migrations reverts (down) to fail since the migration file test.ts did not exist.

Bug introduced in 40367166f9e74a042e2f6314f31877f27a15a14d.

packages/migrations/src/MigrationStorage.ts
@@ -1,6 +1,7 @@
 import type { MigrationsOptions, Transaction } from '@mikro-orm/core';
 import { Utils } from '@mikro-orm/core';
 import type { AbstractSqlDriver, Table } from '@mikro-orm/knex';
+import * as path from 'path';
 import type { MigrationRow } from './typings';
 
 export class MigrationStorage {
@@ -70,8 +71,13 @@ export class MigrationStorage {
   }
 
   protected getMigrationName(name: string) {
-    // strip extension
-    return name.replace(/\.\w+$/, '');
+    const parsedName = path.parse(name);
+    if (['.js', '.ts'].includes(parsedName.ext)) {
+      // strip extension
+      return parsedName.name;
+    }
+
+    return name;
   }
 
 }

tests/features/migrations/Migrator.test.ts
@@ -210,6 +210,24 @@ describe('Migrator', () => {
     await expect(migrator.getPendingMigrations()).resolves.toEqual([]);
   });
 
+  test('remove extension only', async () => {
+    await orm.em.getKnex().schema.dropTableIfExists(orm.config.get('migrations').tableName!);
+    const migrator = new Migrator(orm.em);
+    // @ts-ignore
+    const storage = migrator.storage;
+
+    await storage.ensureTable(); // creates the table
+    await storage.logMigration('test.migration.ts'); // can have extension
+    await expect(storage.getExecutedMigrations()).resolves.toMatchObject([{ name: 'test.migration' }]);
+    await expect(storage.executed()).resolves.toEqual(['test.migration.ts']);
+
+    await storage.ensureTable(); // table exists, no-op
+    await storage.unlogMigration('test.migration');
+    await expect(storage.executed()).resolves.toEqual([]);
+
+    await expect(migrator.getPendingMigrations()).resolves.toEqual([]);
+  });
+
   test('runner', async () => {
     await orm.em.getKnex().schema.dropTableIfExists(orm.config.get('migrations').tableName!);
     const migrator = new Migrator(orm.em);