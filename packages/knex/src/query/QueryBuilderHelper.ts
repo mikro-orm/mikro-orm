d0af02a9cac61f82f772280f10e3b179937334a8
Steven Tsang <tsangste@gmail.com>
2022-03-18 17:37:32 +0000

    fix(knex): `order by` with a formula field should not include `as` for sub-queries

--- packages/knex/src/query/QueryBuilderHelper.ts
+++ packages/knex/src/query/QueryBuilderHelper.ts
@@ -505,7 +505,7 @@ export class QueryBuilderHelper {
 
       Utils.splitPrimaryKeys(field).forEach(f => {
         const prop = this.getProperty(f, alias);
-        const noPrefix = (prop && prop.persist === false) || QueryBuilderHelper.isCustomExpression(f);
+        const noPrefix = (prop && prop.persist === false && !prop.formula) || QueryBuilderHelper.isCustomExpression(f);
         const column = this.mapper(noPrefix ? f : `${alias}.${f}`, type, undefined, null);
         /* istanbul ignore next */
         const rawColumn = Utils.isString(column) ? column.split('.').map(e => this.knex.ref(e)).join('.') : column;
