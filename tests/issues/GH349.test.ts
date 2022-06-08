dba783af0cb7363384d83701e2e399a5d157d376
Martin Adámek <banan23@gmail.com>
2022-06-08 22:42:02 +0200

    test: use mongodb-memory-server for replica sets and docker for rest

--- tests/issues/GH349.test.ts
+++ tests/issues/GH349.test.ts
@@ -64,7 +64,7 @@ describe('GH issue 349', () => {
   beforeAll(async () => {
     orm = await MikroORM.init({
       entities: [A, B, C],
-      clientUrl: 'mongodb://localhost:27017,localhost:27018,localhost:27019/mikro-orm-test?replicaSet=rs',
+      clientUrl: 'mongodb://localhost:27017/mikro-orm-test',
       type: 'mongo',
       debug: ['discovery'],
       logger: i => i,
