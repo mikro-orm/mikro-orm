542a5b847889bf0116f9154525146f5e93c8f51d
Daniel Bersenkowitsch <daniel@projectbay.io>
2022-07-28 14:07:28 +0200

    fix(#3339): test refactored

--- tests/issues/GH3339.test.ts
+++ tests/issues/GH3339.test.ts
@@ -5,7 +5,7 @@ import type { SqliteDriver } from '@mikro-orm/sqlite';
 @Entity({
   tableName: 'gh3339.Customer',
 })
-export class Customer {
+export class Customer1 {
 
   @PrimaryKey({ fieldName: 'ID', type: 'number' })
   id!: number;
@@ -15,46 +15,63 @@ export class Customer {
 
 }
 
-describe('GH issue 3339', () => {
-  it('should reinit the schema', async () => {
-    const orm: MikroORM<SqliteDriver> = await MikroORM.init({
-        entities: [Customer], // make sure name Field of Customer is 'customerName'
-        dbName: `mikro_orm_test_gh_3339`,
-        type: 'postgresql',
-        schema: 'gh3339',
-    });
+@Entity({
+  tableName: 'gh3339.Customer',
+})
+export class Customer2 {
 
+  @PrimaryKey({ fieldName: 'ID', type: 'number' })
+  id!: number;
+
+  @Property()
+  name?: string;
+
+}
+
+@Entity({
+  tableName: 'gh3339.Customer',
+})
+export class Customer3 {
+
+  @PrimaryKey({ fieldName: 'ID', type: 'number' })
+  id!: number;
+
+  @Property()
+  c_name?: string;
+
+}
+
+describe('GH issue 3339', () => {
+
+  let orm: MikroORM;
+
+  beforeAll(async () => {
+    orm = await MikroORM.init({
+      dbName: `mikro_orm_test_gh_3339`,
+      type: 'postgresql',
+      schema: 'gh3339',
+      entities: [ Customer1 ],
+    });
     const generator = orm.getSchemaGenerator();
     await generator.ensureDatabase();
-    await generator.execute(`drop schema if exists "gh3339" cascade`);
+    await generator.dropSchema();
     await generator.createSchema();
     await generator.refreshDatabase();
-    await orm.close(true);
   });
 
-  it('should reference the schema name inside the sql update query', async () => {
+  afterAll(() => orm.close(true));
 
-    // make sure name Field of Customer is 'name'
+  test('reference schema name when updating column names inside sql', async () => {
+    await orm.discoverEntity(Customer2);
+    orm.getMetadata().reset('Customer1');
+    const diff1 = await orm.getSchemaGenerator().getUpdateSchemaSQL({ wrap: false });
+    expect(diff1).toContain('"gh3339"."Customer"');
+    await orm.getSchemaGenerator().execute(diff1);
 
-    const orm = await MikroORM.init({
-        entities: [Customer],
-        dbName: `mikro_orm_test_gh_3339`,
-        type: 'postgresql',
-        schema: 'gh3339',
-      });
-
-    const generator = orm.getSchemaGenerator();
-
-    const sql = await generator.getUpdateSchemaSQL({ schema: 'gh3339' });
-    await generator.dropSchema();
-    await generator.refreshDatabase();
-    await orm.close(true);
-
-    try {
-        expect(sql).toContain('gh3339');
-    } catch {
-        expect(sql).toBe('');
-    }
-
+    await orm.discoverEntity(Customer3);
+    orm.getMetadata().reset('Customer2');
+    const diff2 = await orm.getSchemaGenerator().getUpdateSchemaSQL({ wrap: false });
+    expect(diff2).toContain('"gh3339"."Customer"');
+    await orm.getSchemaGenerator().execute(diff2);
   });
 });
