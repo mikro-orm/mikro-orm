979b5ec8ee1443041864d02090fd5ad6b1247110
Jesper <jesperw@hotmail.nl>
2022-07-31 22:07:35 +0200

    perf(postgre): return string for `compareAsType` in `FullTextType`

--- packages/postgresql/src/types/FullTextType.ts
+++ packages/postgresql/src/types/FullTextType.ts
@@ -2,6 +2,10 @@ import { Type } from '@mikro-orm/core';
 
 export class FullTextType extends Type<string, string> {
 
+  compareAsType(): string {
+    return 'string';
+  }
+
   getColumnType(): string {
     return 'tsvector';
   }
