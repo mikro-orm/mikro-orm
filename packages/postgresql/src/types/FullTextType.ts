9241dd9b035d04f130365a86852d9bc8acdd412e
Jesper <jesperw@hotmail.nl>
2022-07-31 22:05:09 +0200

    perf(postgre): don't override `compareAsType` in full text type

--- packages/postgresql/src/types/FullTextType.ts
+++ packages/postgresql/src/types/FullTextType.ts
@@ -2,10 +2,6 @@ import { Type } from '@mikro-orm/core';
 
 export class FullTextType extends Type<string, string> {
 
-  compareAsType(): string {
-    return 'tsvector';
-  }
-
   getColumnType(): string {
     return 'tsvector';
   }
