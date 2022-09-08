d87219e04a359cafa2223e8e1f6bdb2ee2cfff72
鸿则 <hungtcs@outlook.com>
2022-09-08 16:18:09 +0800

    feat(core): allow custom ORM prop name in `@UseRequestContext()` (#3475)
    Co-authored-by: Martin Adámek <banan23@gmail.com>

--- packages/core/src/decorators/UseRequestContext.ts
+++ packages/core/src/decorators/UseRequestContext.ts
@@ -1,12 +1,12 @@
 import { MikroORM } from '../MikroORM';
 import { RequestContext } from '../utils/RequestContext';
 
-export function UseRequestContext(getContext?: MikroORM | (() => MikroORM)) {
+export function UseRequestContext<T>(getContext?: MikroORM | ((type?: T) => MikroORM)): MethodDecorator {
   return function (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
-    descriptor.value = async function (this: { orm: MikroORM }, ...args: any[]) {
+    descriptor.value = async function (this: T, ...args: any[]) {
       /* istanbul ignore next */
-      const orm = getContext instanceof MikroORM ? getContext : (getContext?.() ?? this.orm);
+      const orm = getContext instanceof MikroORM ? getContext : (getContext?.(this) ?? (this as any).orm);
 
       if (!(orm as unknown instanceof MikroORM)) {
         throw new Error('@UseRequestContext() decorator can only be applied to methods of classes with `orm: MikroORM` property, or with a callback parameter like `@UseRequestContext(() => orm)`');
