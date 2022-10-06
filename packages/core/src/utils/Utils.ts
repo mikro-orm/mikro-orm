aacda1aef59237dc17e01f23847abf36dc6aadc8
Adam Pancutt <adam@pancutt.com>
2022-10-06 21:56:50 +0200

    Add a fallback strategy in cases where a sourcemap obscures the call to __decorate()

--- packages/core/src/utils/Utils.ts
+++ packages/core/src/utils/Utils.ts
@@ -610,13 +610,14 @@ export class Utils {
   static lookupPathFromDecorator(name: string, stack?: string[]): string {
     // use some dark magic to get source path to caller
     stack = stack || new Error().stack!.split('\n');
-    let line = stack.findIndex(line => line.includes('__decorate'))!;
-
+    // In some situations (e.g. swc 1.3.4+), the presence of a source map can obscure the call to
+    // __decorate(), replacing it with the constructor name. To support these cases we look for
+    // Reflect.decorate() as well.
+    let line = stack.findIndex(line => line.includes('__decorate') || line.includes('Reflect.decorate'))!;
     if (line === -1) {
       return name;
     }
-
-    if (Utils.normalizePath(stack[line]).includes('node_modules/tslib/tslib')) {
+    if (stack[line].includes('Reflect.decorate') || Utils.normalizePath(stack[line]).includes('node_modules/tslib/tslib')) {
       line++;
     }
 
