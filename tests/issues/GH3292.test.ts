6d9b5f27c2b7dc1d46d87cb512e265d433e9435e
Phosphorus <fernando.n.p.d.moscu@gmail.com>
2022-10-11 01:17:59 -0300

    test: change the indention & remove useless lines

--- tests/issues/GH3292.test.ts
+++ tests/issues/GH3292.test.ts
@@ -1,5 +1,4 @@
 import type { MikroORM } from '@mikro-orm/core';
-import { mockLogger } from '../helpers';
 import { initORMPostgreSql } from '../bootstrap';
 import { Author2, Book2 } from '../entities-sql';
 
@@ -10,31 +9,19 @@ beforeEach(async () => orm.schema.clearDatabase());
 afterAll(() => orm.close(true));
 
 test('test findOne without a offset', async () => {
-    mockLogger(orm, ['query', 'query-params']);
 
-    const author = new Author2('Bartleby', 'bartelby@writer.org');
-    const book = new Book2('My Life on The Wall, part 1', author);
-    new Book2('My Life on The Wall, part 2', author);
+  const author = new Author2('Bartleby', 'bartelby@writer.org');
+  const book = new Book2('My Life on The Wall, part 1', author);
+  new Book2('My Life on The Wall, part 2', author);
 
-    await orm.em.fork().persistAndFlush(author);
+  await orm.em.fork().persistAndFlush(author);
 
-    async function requestCommonService() {
-      const b = await orm.em.findOne(Book2, {});
-      return b?.title;
-    }
+  const myBook = await orm.em.transactional(async () => await orm.em.findOne(Book2, {}));
 
-    const { titleA, titleB } = await orm.em.transactional(async () => {
-      const b = await orm.em.findOneOrFail(Book2, book.uuid);
-      const titleA = await requestCommonService();
-      const titleB = b.title;
-      return { titleA, titleB };
-    });
-
-    expect(titleA).toEqual(titleB);
+  expect(myBook?.title).toEqual(book.title);
 });
 
 test('test findOne but with a offset', async () => {
-  mockLogger(orm, ['query', 'query-params']);
 
   const author = new Author2('Bartleby', 'bartelby@writer.org');
   new Book2('My Life on The Wall, part 1', author);
@@ -42,19 +29,7 @@ test('test findOne but with a offset', async () => {
 
   await orm.em.fork().persistAndFlush(author);
 
-  async function requestCommonService() {
-    const b = await orm.em.findOne(Book2, {}, {
-        offset: 1,
-    });
-    return b?.title;
-  }
+  const myBook = await orm.em.transactional(async () => await orm.em.findOne(Book2, {}, { offset: 1 }));
 
-  const { titleA, titleB } = await orm.em.transactional(async () => {
-    const b = await orm.em.findOneOrFail(Book2, book2.uuid);
-    const titleA = await requestCommonService();
-    const titleB = b.title;
-    return { titleA, titleB };
-  });
-
-  expect(titleA).toEqual(titleB);
+  expect(myBook?.title).toEqual(book2.title);
 });
