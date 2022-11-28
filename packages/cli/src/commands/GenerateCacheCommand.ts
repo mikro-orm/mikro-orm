e961f2f4d4b7812745e46a34643d8013a677dac7
Kristofer Pervin <7747148+kpervin@users.noreply.github.com>
2022-11-28 11:09:05 -0400

    feat(generate:cache): added option to generate cache via ts-node

--- packages/cli/src/commands/GenerateCacheCommand.ts
+++ packages/cli/src/commands/GenerateCacheCommand.ts
@@ -1,17 +1,25 @@
-import type { ArgumentsCamelCase, CommandModule } from 'yargs';
+import type { ArgumentsCamelCase, Argv, CommandModule } from 'yargs';
 import { MetadataDiscovery, MetadataStorage, colors } from '@mikro-orm/core';
 import { CLIHelper } from '../CLIHelper';
 
-export class GenerateCacheCommand implements CommandModule {
+export class GenerateCacheCommand<T> implements CommandModule<T, {tsNode?: boolean}> {
 
   command = 'cache:generate';
-  describe = 'Generate metadata cache for production';
+  describe = 'Generate metadata cache for production or test use';
+  builder = (args: Argv<T>) => {
+    args.option('ts', {
+      alias: 'tsNode',
+      type: 'boolean',
+      desc: 'Use tsNode to generate `.ts` cache',
+    });
+    return args as Argv<{tsNode?: boolean}>;
+  };
 
   /**
    * @inheritDoc
    */
-  async handler(args: ArgumentsCamelCase) {
-    const config = await CLIHelper.getConfiguration();
+  async handler(args: ArgumentsCamelCase<{tsNode?: boolean}>) {
+    const config = await CLIHelper.getConfiguration(true, args.tsNode ? { tsNode: true } : {});
 
     if (!config.get('cache').enabled) {
       return CLIHelper.dump(colors.red('Metadata cache is disabled in your configuration. Set cache.enabled to true to use this command.'));
@@ -20,9 +28,9 @@ export class GenerateCacheCommand implements CommandModule {
     config.set('logger', CLIHelper.dump.bind(null));
     config.set('debug', true);
     const discovery = new MetadataDiscovery(MetadataStorage.init(), config.getDriver().getPlatform(), config);
-    await discovery.discover(false);
+    await discovery.discover(args.tsNode ?? false);
 
-    CLIHelper.dump(colors.green('Metadata cache was successfully generated'));
+    CLIHelper.dump(colors.green(`${args.tsNode ? 'TS' : 'JS'} metadata cache was successfully generated`));
   }
 
 }
--- tests/features/cli/GenerateCacheCommand.test.ts
+++ tests/features/cli/GenerateCacheCommand.test.ts
@@ -1,12 +1,18 @@
 import type { MetadataStorage } from '@mikro-orm/core';
 import { Configuration, MetadataDiscovery } from '@mikro-orm/core';
 import { CLIHelper } from '@mikro-orm/cli';
-import { GenerateCacheCommand } from '../../../packages/cli/src/commands/GenerateCacheCommand';
+import {
+  GenerateCacheCommand,
+} from '../../../packages/cli/src/commands/GenerateCacheCommand';
 
 (global as any).console.log = jest.fn();
 
 const getConfigurationMock = jest.spyOn(CLIHelper, 'getConfiguration');
-getConfigurationMock.mockResolvedValue(new Configuration({ type: 'mysql', cache: { enabled: true }, getDriver: () => ({ getPlatform: jest.fn() }) } as any, false));
+getConfigurationMock.mockResolvedValue(new Configuration({
+  type: 'mysql',
+  cache: { enabled: true },
+  getDriver: () => ({ getPlatform: jest.fn() }),
+} as any, false));
 const discoverMock = jest.spyOn(MetadataDiscovery.prototype, 'discover');
 discoverMock.mockResolvedValue({} as MetadataStorage);
 
@@ -15,14 +21,31 @@ describe('GenerateCacheCommand', () => {
   test('handler', async () => {
     const cmd = new GenerateCacheCommand();
 
+    const mockOption = jest.fn();
+    const args = { option: mockOption };
+    cmd.builder(args as any);
+    expect(mockOption).toBeCalledWith('ts', {
+      alias: 'tsNode',
+      type: 'boolean',
+      desc: 'Use tsNode to generate `.ts` cache',
+    });
+
     expect(discoverMock.mock.calls.length).toBe(0);
     await expect(cmd.handler({} as any)).resolves.toBeUndefined();
     expect(discoverMock.mock.calls.length).toBe(1);
     expect(discoverMock.mock.calls[0][0]).toBe(false);
+
+    await expect(cmd.handler({ tsNode: true } as any)).resolves.toBeUndefined();
+    expect(discoverMock.mock.calls.length).toBe(2);
+    expect(discoverMock.mock.calls[1][0]).toBe(true);
   });
 
   test('handler throws when cache is disabled', async () => {
-    getConfigurationMock.mockResolvedValue(new Configuration({ type: 'mysql', cache: { enabled: false }, getDriver: () => ({ getPlatform: jest.fn() }) } as any, false));
+    getConfigurationMock.mockResolvedValue(new Configuration({
+      type: 'mysql',
+      cache: { enabled: false },
+      getDriver: () => ({ getPlatform: jest.fn() }),
+    } as any, false));
     discoverMock.mockReset();
     discoverMock.mockResolvedValue({} as MetadataStorage);
 
