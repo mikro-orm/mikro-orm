5ffa81c02ac01cc3420ca345b05835e331c879e9
Martin Adámek <banan23@gmail.com>
2022-12-02 11:40:35 +0100

    fix(postgres): quote array literal items containing a comma
    Closes #3810

--- packages/postgresql/src/PostgreSqlPlatform.ts
+++ packages/postgresql/src/PostgreSqlPlatform.ts
@@ -142,7 +142,7 @@ export class PostgreSqlPlatform extends AbstractSqlPlatform {
   }
 
   marshallArray(values: string[]): string {
-    const quote = (v: string) => v === '' || v.match(/["{}]/) ? JSON.stringify(v) : v;
+    const quote = (v: string) => v === '' || v.match(/["{},]/) ? JSON.stringify(v) : v;
     return `{${values.map(v => quote('' + v)).join(',')}}`;
   }
 
