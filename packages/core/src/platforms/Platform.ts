1776e879fb602e96c63c499d40fa548d768a48e7
Abe Neuwirth <16637880+yeedle@users.noreply.github.com>
2022-12-08 11:44:17 -0500

    fix: respect postgresql no timestamptz precision default (#3832)
    See details in https://github.com/mikro-orm/mikro-orm/issues/3824. This
    PR removes the default fallback to a precision of 0 so that for
    postgresql the correct type for Date (timestamptz with no precision) is
    generated, and it adds the fallback to 0 downstream in platform-specific
    contexts.
    (this PR is a duplicate of
    https://github.com/mikro-orm/mikro-orm/pull/3826, reopened against the
    `v6` branch)

--- packages/core/src/platforms/Platform.ts
+++ packages/core/src/platforms/Platform.ts
@@ -110,7 +110,7 @@ export abstract class Platform {
     return 'current_timestamp' + (length ? `(${length})` : '');
   }
 
-  getDateTimeTypeDeclarationSQL(column: { length?: number }): string {
+  getDateTimeTypeDeclarationSQL(column: { length?: number } = { length: 0 }): string {
     return 'datetime' + (column.length ? `(${column.length})` : '');
   }
 
