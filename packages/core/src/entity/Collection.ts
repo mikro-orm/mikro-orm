8b77c0ff6717cee0d12c0d603d37682debcb901c
Boris Damiryan <b.damiryan@cyrm.ru>
2022-12-20 12:34:51 +0300

    fix(core): allow adding array of refs to collection

--- packages/core/src/entity/Collection.ts
+++ packages/core/src/entity/Collection.ts
@@ -118,7 +118,7 @@ export class Collection<T extends object, O extends object = object> extends Arr
     return super.toJSON();
   }
 
-  add(entity: T | Reference<T> | T[], ...entities: (T | Reference<T>)[]): void {
+  add(entity: T | Reference<T> | (T | Reference<T>)[], ...entities: (T | Reference<T>)[]): void {
     entities = Utils.asArray(entity).concat(entities);
     const unwrapped = entities.map(i => Reference.unwrapReference(i)) as T[];
     unwrapped.forEach(entity => this.validateItemType(entity));
