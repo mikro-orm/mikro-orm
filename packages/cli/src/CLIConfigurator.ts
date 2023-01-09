af5bd0937706e3aeea0eb2040692082050db7be3
Martin Adámek <banan23@gmail.com>
2023-01-09 21:58:12 +0100

    feat(core): allow overriding ORM config path via `--config` (#3924)
    Allows to override the config path via `--config` option:
    
    ```sh
    $ npx mikro-orm debug --config ./my-config.ts
    ```
    
    This option will be respected also when you run your app, not just when
    you use the CLI.

--- packages/cli/src/CLIConfigurator.ts
+++ packages/cli/src/CLIConfigurator.ts
@@ -32,12 +32,15 @@ export class CLIConfigurator {
       }
     }
 
-    // noinspection HtmlDeprecatedTag
     return yargs
       .scriptName('mikro-orm')
       .version(version)
       .usage('Usage: $0 <command> [options]')
       .example('$0 schema:update --run', 'Runs schema synchronization')
+      .option('config', {
+        type: 'string',
+        desc: `Set path to the ORM configuration file`,
+      })
       .alias('v', 'version')
       .alias('h', 'help')
       .command(new ClearCacheCommand())
