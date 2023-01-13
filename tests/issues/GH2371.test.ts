f8d84f2f4a907ba912f7aec74ac0ae2c3ebc9888
Funduck <qlfunduck@gmail.com>
2023-01-13 13:46:34 +0300

    fix(core): propagate ManyToOne only to matching collections
    refactored test and one-liner

--- tests/issues/GH2371.test.ts
+++ tests/issues/GH2371.test.ts
@@ -30,6 +30,9 @@ class Garage {
   @OneToMany(() => Car, v => v.garage)
   cars = new Collection<Car>(this);
 
+  @OneToMany(() => Truck, v => v.garage)
+  trucks = new Collection<Truck>(this);
+
 }
 
 describe('GH issue 2371', () => {
@@ -53,81 +56,13 @@ describe('GH issue 2371', () => {
 
     expect(garage.cars.contains(car)).toBe(true);
     expect(garage.vehicles.contains(car)).toBe(true);
+    expect(garage.trucks.length).toBe(0);
     await orm.em.fork().persistAndFlush(garage);
 
-    const g = await orm.em.findOneOrFail(Garage, garage, { populate: ['cars', 'vehicles'] });
+    const g = await orm.em.findOneOrFail(Garage, garage, { populate: ['cars', 'vehicles', 'trucks'] });
     const c = await orm.em.findOneOrFail(Car, car);
     expect(g.cars.contains(c)).toBe(true);
     expect(g.vehicles.contains(c)).toBe(true);
-  });
-});
-
-
-class BaseEntity {
-
-  constructor(data = {}) {
-    Object.assign(this, data);
-  }
-
-  @PrimaryKey()
-  _id = +new Date() + Math.random();
-
-}
-
-@Entity({ tableName: 'basket' })
-class Basket extends BaseEntity {
-
-  @OneToMany(() => Apple, f => f.basket)
-  apples = new Collection<Apple>(this);
-
-  @OneToMany(() => Banana, f => f.basket)
-  bananas = new Collection<Banana>(this);
-
-}
-
-@Entity({ tableName: 'fruit', discriminatorColumn: 'type', abstract: true })
-class Fruit extends BaseEntity  {
-
-  @ManyToOne(() => Basket)
-  basket!: Basket;
-
-}
-
-@Entity({ discriminatorValue: 'apple' })
-class Apple extends Fruit {}
-
-@Entity({ discriminatorValue: 'banana' })
-class Banana extends Fruit {}
-
-describe('GH issue 2371 (M)', () => {
-  let orm: MikroORM<SqliteDriver>;
-
-  beforeAll(async () => {
-    orm = await MikroORM.init({
-      entities: [Fruit, Apple, Banana, Basket],
-      dbName: ':memory:',
-      driver: SqliteDriver,
-    });
-    await orm.schema.createSchema();
-  });
-
-  afterAll(() => orm.close(true));
-
-  it('should propagate setting m:1 property to matching collections only', async () => {
-    const basket = new Basket({});
-    const apple = new Apple({ basket });
-
-    expect(basket.apples.contains(apple)).toBe(true);
-    expect(basket.bananas.contains(apple)).toBe(false);
-    expect(basket.bananas.length).toBe(0);
-
-    await orm.em.fork().persistAndFlush(basket);
-
-    const b = await orm.em.findOneOrFail(Basket, { _id: basket._id }, { populate: ['apples', 'bananas'] });
-    const a = await orm.em.findOneOrFail(Apple, { _id: apple._id });
-    expect(b.apples.contains(a)).toBe(true);
-    expect(b.bananas.contains(a)).toBe(false);
-    expect(b.bananas.length).toBe(0);
-
+    expect(g.trucks.length).toBe(0);
   });
 });
