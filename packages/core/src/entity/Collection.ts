8f869b486419d3d3f04698dbf158cdc7d6a9e6ef
Manuel <urbanomanuel1@gmail.com>
2023-01-22 19:25:54 +0100

    style: improves code readability

--- packages/core/src/entity/Collection.ts
+++ packages/core/src/entity/Collection.ts
@@ -73,13 +73,13 @@ export class Collection<T extends object, O extends object = object> extends Arr
         this._count = count;
       }
       return count;
-    }
+    } else {
       const count = await em.count(this.property.type, this.createLoadCountCondition(options.where ?? {} as FilterQuery<T>, pivotMeta));
       if (!options.where) {
         this._count = count;
       }
-      return count;
-
+      return count; 
+    }
   }
 
   async matching<P extends string = never>(options: MatchingOptions<T, P>): Promise<Loaded<T, P>[]> {
