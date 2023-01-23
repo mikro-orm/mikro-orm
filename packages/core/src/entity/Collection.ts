11e4660531b97ccb2db06bf4045f2db85ddca70b
Manuel Urbano <urbanomanuel1@gmail.com>
2023-01-23 17:40:37 +0100

    chore: adds eslint ignore comment Eslint doesn't like this else statement here.

--- packages/core/src/entity/Collection.ts
+++ packages/core/src/entity/Collection.ts
@@ -75,12 +75,13 @@ export class Collection<T extends object, O extends object = object> extends Arr
         this._count = count;
       }
       return count;
+      /* eslint-disable no-else-return */
     } else {
       const count = await em.count(this.property.type, this.createLoadCountCondition(options.where ?? {} as FilterQuery<T>, pivotMeta));
       if (!options.where) {
         this._count = count;
       }
-      return count; 
+      return count;
     }
   }
 
