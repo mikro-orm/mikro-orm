4c46d8d6caa5b94266c03faea4060f508ded416d
Martin Adámek <banan23@gmail.com>
2023-01-31 19:44:38 +0100

    feat(core): add cursor-based pagination via `em.findByCursor()` (#3975)
    As an alternative to the offset based pagination with `limit` and
    `offset`, we can paginate based on a cursor. A cursor is an opaque
    string that defines specific place in ordered entity graph. You can use
    `em.findByCursor()` to access those options. Under the hood, it will
    call `em.find()` and `em.count()` just like the `em.findAndCount()`
    method, but will use the cursor options instead.
    
    Supports `before`, `after`, `first` and `last` options while disallowing
    `limit` and `offset`. Explicit `orderBy` option is required.
    
    Use `first` and `after` for forward pagination, or `last` and `before`
    for backward pagination.
    
    - `first` and `last` are numbers and serve as an alternative to
    `offset`, those options are mutually exclusive, use only one at a time
    - `before` and `after` specify the previous cursor value, it can be one
    of the:
    	- `Cursor` instance
    	- opaque string provided by `startCursor/endCursor` properties
    	- POJO/entity instance
    
    ```ts
    const currentCursor = await em.findByCursor(User, {}, {
      first: 10,
      after: previousCursor, // cursor instance
      orderBy: { id: 'desc' },
    });
    
    // to fetch next page
    const nextCursor = await em.findByCursor(User, {}, {
      first: 10,
      after: currentCursor.endCursor, // opaque string
      orderBy: { id: 'desc' },
    });
    
    // to fetch next page
    const nextCursor2 = await em.findByCursor(User, {}, {
      first: 10,
      after: { id: lastSeenId }, // entity-like POJO
      orderBy: { id: 'desc' },
    });
    ```
    
    The `Cursor` object provides following interface:
    
    ```ts
    Cursor<User> {
      items: [
        User { ... },
        User { ... },
        User { ... },
        ...
      ],
      totalCount: 50,
      length: 10,
      startCursor: 'WzRd',
      endCursor: 'WzZd',
      hasPrevPage: true,
      hasNextPage: true,
    }
    ```

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,7 +6,7 @@
 export {
   Constructor, ConnectionType, Dictionary, PrimaryKeyType, PrimaryKeyProp, Primary, IPrimaryKey, ObjectQuery, FilterQuery, IWrappedEntity, EntityName, EntityData, Highlighter,
   AnyEntity, EntityClass, EntityProperty, EntityMetadata, QBFilterQuery, PopulateOptions, Populate, Loaded, New, LoadedReference, LoadedCollection, IMigrator, IMigrationGenerator,
-  GetRepository, EntityRepositoryType, MigrationObject, DeepPartial, PrimaryProperty, Cast, IsUnknown, EntityDictionary, EntityDTO, MigrationDiff,
+  GetRepository, EntityRepositoryType, MigrationObject, DeepPartial, PrimaryProperty, Cast, IsUnknown, EntityDictionary, EntityDTO, MigrationDiff, FilterObject,
   IEntityGenerator, ISeedManager, EntityClassGroup, OptionalProps, RequiredEntityData, CheckCallback, SimpleColumnMeta, Rel, Ref, ISchemaGenerator,
   UmzugMigration, MigrateOptions, MigrationResult, MigrationRow,
 } from './typings';
