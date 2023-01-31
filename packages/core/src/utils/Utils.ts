a5ba8a247562e475ae8e420ebe30c97898f4d778
Martin Adámek <banan23@gmail.com>
2023-01-31 19:44:38 +0100

    feat(core): add cursor-based pagination via `em.findByCursor()` (#3975)
    As an alternative to the offset based pagination with `limit` and
    `offset`, we can paginate based on a cursor. A cursor is an opaque
    string that defines specific place in ordered entity graph. You can use
    `em.findByCursor()` to access those options. Under the hood, it will
    call `em.find()` and `em.count()` just like the `em.findAndCount()`
    method, but will use the cursor options instead.
    
    Supports `before`, `after`, `first` and `last` options while disallowing
    `limit` and `offset`. Explicit `orderBy` option is required.
    
    Use `first` and `after` for forward pagination, or `last` and `before`
    for backward pagination.
    
    - `first` and `last` are numbers and serve as an alternative to
    `offset`, those options are mutually exclusive, use only one at a time
    - `before` and `after` specify the previous cursor value, it can be one
    of the:
    	- `Cursor` instance
    	- opaque string provided by `startCursor/endCursor` properties
    	- POJO/entity instance
    
    ```ts
    const currentCursor = await em.findByCursor(User, {}, {
      first: 10,
      after: previousCursor, // cursor instance
      orderBy: { id: 'desc' },
    });
    
    // to fetch next page
    const nextCursor = await em.findByCursor(User, {}, {
      first: 10,
      after: currentCursor.endCursor, // opaque string
      orderBy: { id: 'desc' },
    });
    
    // to fetch next page
    const nextCursor2 = await em.findByCursor(User, {}, {
      first: 10,
      after: { id: lastSeenId }, // entity-like POJO
      orderBy: { id: 'desc' },
    });
    ```
    
    The `Cursor` object provides following interface:
    
    ```ts
    Cursor<User> {
      items: [
        User { ... },
        User { ... },
        User { ... },
        ...
      ],
      totalCount: 50,
      length: 10,
      startCursor: 'WzRd',
      endCursor: 'WzZd',
      hasPrevPage: true,
      hasNextPage: true,
    }
    ```

--- packages/core/src/utils/Utils.ts
+++ packages/core/src/utils/Utils.ts
@@ -730,7 +730,7 @@ export class Utils {
   /**
    * Checks whether the value is POJO (e.g. `{ foo: 'bar' }`, and not instance of `Foo`)
    */
-  static isPlainObject(value: any): value is Dictionary {
+  static isPlainObject<T extends Dictionary>(value: any): value is T {
     return (
       value !== null
       && typeof value === 'object'
@@ -1159,6 +1159,10 @@ export class Utils {
 
       return args;
     }, {} as Dictionary) as T;
+  }
+
+  static xor(a: boolean, b: boolean): boolean {
+    return (a || b) && !(a && b);
   }
 
 }
