5473734d3a76311f611dc850dfaa0ec9b4e602cb
Martin Adámek <banan23@gmail.com>
2023-02-06 10:16:23 +0100

    refactor: remove dependency on faker
    Related: #3089

--- packages/seeder/src/index.ts
+++ packages/seeder/src/index.ts
@@ -7,7 +7,3 @@
 export * from './Seeder';
 export * from './Factory';
 export * from './SeedManager';
-
-// reexport faker instance and Faker type
-import { faker, Faker } from '@faker-js/faker';
-export { faker, Faker };
