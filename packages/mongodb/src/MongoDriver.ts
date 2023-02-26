71e6c7e02d09fd66929b68308b1762b28da579c7
Martin Adámek <banan23@gmail.com>
2023-02-26 12:28:57 +0100

    Update packages/mongodb/src/MongoDriver.ts

--- packages/mongodb/src/MongoDriver.ts
+++ packages/mongodb/src/MongoDriver.ts
@@ -138,7 +138,7 @@ export class MongoDriver extends DatabaseDriver<MongoConnection> {
 
   private renameFields<T>(entityName: string, data: T, where = false): T {
     // copy to new variable to prevent changing the T type or doing as unknown casts
-    const copiedData: T & { $fulltext?: string; $text?: { $search: string }; $and?: {$fulltext?: string}[] } = Object.assign({}, data); // copy first
+    const copiedData: Dictionary = Object.assign({}, data); // copy first
     Utils.renameKey(copiedData, 'id', '_id');
     const meta = this.metadata.find(entityName);
 
