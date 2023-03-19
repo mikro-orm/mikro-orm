581fcba6b3debf091307996959cb0bff5cca9588
Martin Adámek <banan23@gmail.com>
2023-03-19 19:26:31 +0100

    feat(core): infer property type from default value (#4150)
    Type inference with reflect-metadata fails when a runtime value is used
    without an explicit type annotation, e.g. `created = new Date` instead
    of `created: Data = new Date`. This PR infers the default value from the
    property initializer (if possible) and uses its type in this
    automatically, so `created = new Date` will work fine.
    
    Closes #4060

--- packages/core/src/entity/ArrayCollection.ts
+++ packages/core/src/entity/ArrayCollection.ts
@@ -222,7 +222,7 @@ export class ArrayCollection<T extends object, O extends object> {
 
       /* istanbul ignore if */
       if (!meta) {
-        throw MetadataError.fromUnknownEntity((this.owner as object).constructor.name, 'Collection.property getter, maybe you just forgot to initialize the ORM?');
+        throw MetadataError.fromUnknownEntity(this.owner.constructor.name, 'Collection.property getter, maybe you just forgot to initialize the ORM?');
       }
 
       this._property = meta.relations.find(prop => this.owner[prop.name] === this)!;
@@ -313,7 +313,7 @@ export class ArrayCollection<T extends object, O extends object> {
     const hidden = ['items', 'owner', '_property', '_count', 'snapshot', '_populated', '_lazyInitialized'];
     hidden.forEach(k => delete object[k]);
     const ret = inspect(object, { depth });
-    const name = `${this.constructor.name}<${this.property.type}>`;
+    const name = `${this.constructor.name}<${this.property?.type ?? 'unknown'}>`;
 
     return ret === '[Object]' ? `[${name}]` : name + ' ' + ret;
   }
