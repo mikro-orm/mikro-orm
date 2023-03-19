a217b3982420fae4acd4ca277494d74263b90e1f
Martin Adámek <banan23@gmail.com>
2023-03-19 19:26:31 +0100

    feat(core): infer property type from default value (#4150)
    Type inference with reflect-metadata fails when a runtime value is used
    without an explicit type annotation, e.g. `created = new Date` instead
    of `created: Data = new Date`. This PR infers the default value from the
    property initializer (if possible) and uses its type in this
    automatically, so `created = new Date` will work fine.
    
    Closes #4060

--- tests/entities-sql/BaseEntity22.ts
+++ tests/entities-sql/BaseEntity22.ts
@@ -1,18 +1,5 @@
-import type { AnyEntity } from '@mikro-orm/core';
-import { Collection, ReferenceKind, Utils, wrap } from '@mikro-orm/core';
-
 export abstract class BaseEntity22 {
 
   abstract id: number;
-
-  constructor() {
-    const props = wrap(this, true).__meta.properties;
-
-    Utils.keys(props).forEach(prop => {
-      if ([ReferenceKind.ONE_TO_MANY, ReferenceKind.MANY_TO_MANY].includes(props[prop].kind)) {
-        (this as any)[prop] = new Collection(this as AnyEntity);
-      }
-    });
-  }
 
 }
