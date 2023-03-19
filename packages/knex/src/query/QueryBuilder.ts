ac6336dc8972db10d5e6daa9139a73c39489e04f
Martin Adámek <banan23@gmail.com>
2023-03-19 23:59:29 +0100

    feat(core): support atomic updates via `raw()` helper (#4094)
    When you want to issue an atomic update query via flush, you can use the
    static `raw()` helper:
    
    ```ts
    const ref = em.getReference(Author, 123);
    ref.age = raw(`age * 2`);
    
    await em.flush();
    console.log(ref.age); // real value is available after flush
    ```
    
    The `raw()` helper returns special raw query fragment object. It
    disallows serialization (via `toJSON`) as well as working with the value
    (via
    [`valueOf()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/valueOf)).
    Only single use of this value is allowed, if you try to reassign it to
    another entity, an error will be thrown to protect you from mistakes
    like this:
    
    ```ts
    order.number = raw(`(select max(num) + 1 from orders)`);
    user.lastOrderNumber = order.number; // throws, it could resolve to a different value
    JSON.stringify(order); // throws, raw value cannot be serialized
    ```
    
    Closes #3657

--- packages/knex/src/query/QueryBuilder.ts
+++ packages/knex/src/query/QueryBuilder.ts
@@ -23,6 +23,7 @@ import type {
   RequiredEntityData,
 } from '@mikro-orm/core';
 import {
+  raw,
   helper,
   LoadStrategy,
   LockMode,
@@ -190,7 +191,7 @@ export class QueryBuilder<T extends object = AnyEntity> {
     } else if (this.hasToManyJoins()) {
       this._fields = this.mainAlias.metadata!.primaryKeys;
     } else {
-      this._fields = [this.raw('*')];
+      this._fields = [raw('*')];
     }
 
     if (distinct) {
@@ -392,13 +393,6 @@ export class QueryBuilder<T extends object = AnyEntity> {
     return this.knex.ref(field);
   }
 
-  raw<R = Knex.Raw>(sql: string, bindings: Knex.RawBinding[] | Knex.ValueDict = []): R {
-    const raw = this.knex.raw(sql, bindings);
-    (raw as Dictionary).__raw = true; // tag it as there is now way to check via `instanceof`
-
-    return raw as unknown as R;
-  }
-
   limit(limit?: number, offset = 0): this {
     this.ensureNotFinalized();
     this._limit = limit;
@@ -521,7 +515,7 @@ export class QueryBuilder<T extends object = AnyEntity> {
       this.helper.getLockSQL(qb, this.lockMode, this.lockTables);
     }
 
-    this.helper.finalize(this.type ?? QueryType.SELECT, qb, this.mainAlias.metadata);
+    this.helper.finalize(this.type ?? QueryType.SELECT, qb, this.mainAlias.metadata, this._data);
 
     return qb;
   }
