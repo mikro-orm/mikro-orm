b269a49abe2d36ce47b85d6e36dfc88fee0c8bbf
Martin Adámek <banan23@gmail.com>
2023-03-27 01:13:04 +0200

    feat(core): add `GeneratedCacheAdapter` for production usage (#4167)
    MikroORM now lets you generate production cache bundle into a single
    JSON file via CLI:
    
    ```bash
    npx mikro-orm cache:generate --combined
    ```
    
    This will create `./temp/metadata.json` file which can be used together
    with `GeneratedCacheAdapter` in your production configuration:
    
    ```ts
    import { GeneratedCacheAdapter, MikroORM } from '@mikro-orm/core';
    
    await MikroORM.init({
      metadataCache: {
        enabled: true,
        adapter: GeneratedCacheAdapter, 
        options: { data: require('./temp/metadata.json') },
      },
      // ...
    });
    ```
    
    This way you can keep the `@mikro-orm/reflection` package as a
    development dependency only, use the CLI to create the cache bundle, and
    depend only on that in your production build.
    
    > The cache bundle can be statically imported, which is handy in case
    you are using some bundler.
    
    Closes #4164

--- packages/core/src/cache/index.ts
+++ packages/core/src/cache/index.ts
@@ -2,3 +2,4 @@ export * from './CacheAdapter';
 export * from './NullCacheAdapter';
 export * from './FileCacheAdapter';
 export * from './MemoryCacheAdapter';
+export * from './GeneratedCacheAdapter';
