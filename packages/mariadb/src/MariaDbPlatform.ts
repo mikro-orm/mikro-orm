f67a939080204df78d0332c02be6fed87704c821
Martin Adámek <banan23@gmail.com>
2023-04-07 20:07:15 +0200

    feat(core): require explicitly marked raw queries via `raw()` helper (#4197)
    Improves the new `raw()` helper that now replaces the removed `expr()`
    helper. Also adds new `sql` helper that allows to work with `raw` as
    with tagged template function.
    
    The `raw()` helper is now required to be used for any raw SQL, both with
    EM and QB, to protect from SQL injection.
    
    This raw SQL query fragment that can be assigned to a property or part
    of a filter. This fragment is represented by `RawQueryFragment` class
    instance that can be serialized to a string, so it can be used both as
    an object value and key. When serialized, the fragment key gets cached
    and only such cached key will be recognized by the ORM. This adds a
    runtime safety to the raw query fragments.
    
    > **`raw()` helper is required since v6 to use a raw fragment in your
    query, both through EntityManager and QueryBuilder.**
    
    ```ts
    // as a value
    await em.find(User, { time: raw('now()') });
    
    // as a key
    await em.find(User, { [raw('lower(name)')]: name.toLowerCase() });
    
    // value can be empty array
    await em.find(User, { [raw('(select 1 = 1)')]: [] });
    ```
    
    The `raw` helper supports several signatures, you can pass in a callback
    that receives the current property alias:
    
    ```ts
    await em.find(User, { [raw(alias => `lower(${alias}.name)`)]: name.toLowerCase() });
    ```
    
    You can also use the `sql` tagged template function, which works the
    same, but supports only the simple string signature:
    
    ```ts
    await em.find(User, { [sql`lower(name)`]: name.toLowerCase() });
    ```
    
    When using inside filters, you might have to use a callback signature to
    create new raw instance for every filter usage.
    
    ```ts
    @Filter({ name: 'long', cond: () => ({ [raw('length(perex)')]: { $gt: 10000 } }) })
    ```

--- packages/mariadb/src/MariaDbPlatform.ts
+++ packages/mariadb/src/MariaDbPlatform.ts
@@ -1,8 +1,7 @@
-import type { Dictionary } from '@mikro-orm/knex';
 import { AbstractSqlPlatform } from '@mikro-orm/knex';
 import { MariaDbSchemaHelper } from './MariaDbSchemaHelper';
 import { MariaDbExceptionConverter } from './MariaDbExceptionConverter';
-import { Utils, type SimpleColumnMeta, type Type, type expr } from '@mikro-orm/core';
+import { Utils, type SimpleColumnMeta, type Dictionary, type Type } from '@mikro-orm/core';
 
 export class MariaDbPlatform extends AbstractSqlPlatform {
 
