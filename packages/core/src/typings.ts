04ad72e968b997fdc113b11db605fbb119a52f77
Anytinz <anytinz.ovo@gmail.com>
2023-04-09 17:08:51 +0800

    fix(core): prevent tuple type properties from being converted to array when serializing entities (#4205)
    This commit fixes an issue where properties with tuple type were being converted to an array when entities were serialized. The fix ensures that tuple-type properties are correctly preserved during the serialization process.

--- packages/core/src/typings.ts
+++ packages/core/src/typings.ts
@@ -228,7 +228,7 @@ export type EntityDTOProp<T> = T extends Scalar
         : T extends { $: infer U }
           ? (U extends readonly (infer V)[] ? EntityDTO<V>[] : EntityDTO<U>)
           : T extends readonly (infer U)[]
-            ? U[]
+            ? (T extends readonly [infer U, ...infer V] ? T : U[])
             : T extends Relation<T>
               ? EntityDTO<T>
               : T;
