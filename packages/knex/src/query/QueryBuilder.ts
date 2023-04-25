6ad551b3012d31dd6de8faab51eeea2ab3c43652
Spencer Kaiser <SpencerKaiser@users.noreply.github.com>
2023-04-25 16:46:57 -0500

    feat(core): add customizable `LoggerContext` with labeling support (#4233)
    Closes #4230

--- packages/knex/src/query/QueryBuilder.ts
+++ packages/knex/src/query/QueryBuilder.ts
@@ -32,13 +32,15 @@ import {
   type QBQueryOrderMap,
   type QueryOrderMap,
   type QueryResult,
-  type RequiredEntityData } from '@mikro-orm/core';
+  type RequiredEntityData,
+  type LoggerContext,
+} from '@mikro-orm/core';
 import { QueryType } from './enums';
 import type { AbstractSqlDriver } from '../AbstractSqlDriver';
 import { QueryBuilderHelper, type Alias } from './QueryBuilderHelper';
 import type { SqlEntityManager } from '../SqlEntityManager';
 import { CriteriaNodeFactory } from './CriteriaNodeFactory';
-import type { Field, IQueryBuilder, JoinOptions } from '../typings';
+import type { Field, JoinOptions } from '../typings';
 
 export interface ExecuteOptions {
   mapResults?: boolean;
@@ -131,7 +133,8 @@ export class QueryBuilder<T extends object = AnyEntity> {
               private readonly context?: Knex.Transaction,
               alias?: string,
               private connectionType?: ConnectionType,
-              private readonly em?: SqlEntityManager) {
+              private readonly em?: SqlEntityManager,
+              private readonly loggerContext?: LoggerContext) {
     if (alias) {
       this.aliasCounter++;
       this._explicitAlias = true;
@@ -656,7 +659,7 @@ export class QueryBuilder<T extends object = AnyEntity> {
 
     const write = method === 'run' || !this.platform.getConfig().get('preferReadReplicas');
     const type = this.connectionType || (write ? 'write' : 'read');
-    const res = await this.driver.getConnection(type).execute(query.sql, query.bindings as any[], method, this.context);
+    const res = await this.driver.getConnection(type).execute(query.sql, query.bindings as any[], method, this.context, this.loggerContext);
     const meta = this.mainAlias.metadata;
 
     if (!options.mapResults || !meta) {
