mikro-orm/mikro-orm

commit 5594c469f05d2c1fc76f3cc1a388f5e7162f4e72

fix(core): rework JSON value processing (#4194)
Every driver behaves a bit differently when it comes to handling JSON
columns. While SQLite is not processing then anyhow, and MongoDB
supports them natively, all the others have rather quirky and not very
configurable JSON parsing and stringification implemented. It is crucial
to have the JSON values properly parsed, as well as normalized in the
entity snapshot, so we can correctly detect updates.

This PR makes the JSON parsing fail-safe, returning the value directly
if it is not a valid JSON string, and ensures the entity data are in the
right shape.

Closes #4193

packages/postgresql/src/PostgreSqlConnection.ts
@@ -1,4 +1,4 @@
-import { types, defaults } from 'pg';
+import TypeOverrides from 'pg/lib/type-overrides';
 import type { Dictionary } from '@mikro-orm/core';
 import type { Knex } from '@mikro-orm/knex';
 import { AbstractSqlConnection, MonkeyPatchable } from '@mikro-orm/knex';
@@ -16,11 +16,13 @@ export class PostgreSqlConnection extends AbstractSqlConnection {
 
   getConnectionOptions(): Knex.PgConnectionConfig {
     const ret = super.getConnectionOptions() as Knex.PgConnectionConfig;
+    const types = new TypeOverrides();
     [1082].forEach(oid => types.setTypeParser(oid, str => str)); // date type
 
     if (this.config.get('forceUtcTimezone')) {
       [1114].forEach(oid => types.setTypeParser(oid, str => new Date(str + 'Z'))); // timestamp w/o TZ type
-      (defaults as any).parseInputDatesAsUTC = true;
+      ret.parseInputDatesAsUTC = true;
+      ret.types = types as any;
     }
 
     return ret;

packages/core/src/entity/EntityFactory.ts
@@ -96,7 +96,7 @@ export class EntityFactory {
 
   mergeData<T extends object>(meta: EntityMetadata<T>, entity: T, data: EntityData<T>, options: FactoryOptions = {}): void {
     // merge unchanged properties automatically
-    data = QueryHelper.processParams({ ...data });
+    data = QueryHelper.processParams(Utils.copy(data));
     const existsData = this.comparator.prepareEntity(entity);
     const originalEntityData = helper(entity).__originalEntityData ?? {} as EntityData<T>;
     const diff = this.comparator.diffEntities(meta.className, originalEntityData, existsData);

packages/core/src/hydration/Hydrator.ts
@@ -16,7 +16,7 @@ export abstract class Hydrator implements IHydrator {
   /**
    * @inheritDoc
    */
-  hydrate<T>(entity: T, meta: EntityMetadata<T>, data: EntityData<T>, factory: EntityFactory, type: 'full' | 'returning' | 'reference', newEntity = false, convertCustomTypes = false, schema?: string): void {
+  hydrate<T extends object>(entity: T, meta: EntityMetadata<T>, data: EntityData<T>, factory: EntityFactory, type: 'full' | 'returning' | 'reference', newEntity = false, convertCustomTypes = false, schema?: string): void {
     this.running = true;
     const props = this.getProperties(meta, type);
 
@@ -29,7 +29,7 @@ export abstract class Hydrator implements IHydrator {
   /**
    * @inheritDoc
    */
-  hydrateReference<T>(entity: T, meta: EntityMetadata<T>, data: EntityData<T>, factory: EntityFactory, convertCustomTypes?: boolean, schema?: string): void {
+  hydrateReference<T extends object>(entity: T, meta: EntityMetadata<T>, data: EntityData<T>, factory: EntityFactory, convertCustomTypes?: boolean, schema?: string): void {
     this.running = true;
     meta.primaryKeys.forEach(pk => {
       this.hydrateProperty<T>(entity, meta.properties[pk], data, factory, false, convertCustomTypes);
@@ -41,7 +41,7 @@ export abstract class Hydrator implements IHydrator {
     return this.running;
   }
 
-  protected getProperties<T>(meta: EntityMetadata<T>, type: 'full' | 'returning' | 'reference'): EntityProperty<T>[] {
+  protected getProperties<T extends object>(meta: EntityMetadata<T>, type: 'full' | 'returning' | 'reference'): EntityProperty<T>[] {
     if (type === 'reference') {
       return meta.primaryKeys.map(pk => meta.properties[pk]);
     }
@@ -53,7 +53,7 @@ export abstract class Hydrator implements IHydrator {
     return meta.hydrateProps;
   }
 
-  protected hydrateProperty<T>(entity: T, prop: EntityProperty, data: EntityData<T>, factory: EntityFactory, newEntity?: boolean, convertCustomTypes?: boolean): void {
+  protected hydrateProperty<T extends object>(entity: T, prop: EntityProperty, data: EntityData<T>, factory: EntityFactory, newEntity?: boolean, convertCustomTypes?: boolean): void {
     entity[prop.name] = data[prop.name];
   }
 

packages/core/src/hydration/ObjectHydrator.ts
@@ -2,11 +2,11 @@ import type { EntityData, EntityMetadata, EntityProperty } from '../typings';
 import { Hydrator } from './Hydrator';
 import { Collection } from '../entity/Collection';
 import { Reference } from '../entity/Reference';
-import { Utils } from '../utils/Utils';
+import { parseJsonSafe, Utils } from '../utils/Utils';
 import { ReferenceType } from '../enums';
 import type { EntityFactory } from '../entity/EntityFactory';
 
-type EntityHydrator<T> = (entity: T, data: EntityData<T>, factory: EntityFactory, newEntity: boolean, convertCustomTypes: boolean, schema?: string) => void;
+type EntityHydrator<T extends object> = (entity: T, data: EntityData<T>, factory: EntityFactory, newEntity: boolean, convertCustomTypes: boolean, schema?: string) => void;
 
 export class ObjectHydrator extends Hydrator {
 
@@ -21,7 +21,7 @@ export class ObjectHydrator extends Hydrator {
   /**
    * @inheritDoc
    */
-  hydrate<T>(entity: T, meta: EntityMetadata<T>, data: EntityData<T>, factory: EntityFactory, type: 'full' | 'returning' | 'reference', newEntity = false, convertCustomTypes = false, schema?: string): void {
+  hydrate<T extends object>(entity: T, meta: EntityMetadata<T>, data: EntityData<T>, factory: EntityFactory, type: 'full' | 'returning' | 'reference', newEntity = false, convertCustomTypes = false, schema?: string): void {
     const hydrate = this.getEntityHydrator(meta, type);
     const running = this.running;
     this.running = true;
@@ -32,7 +32,7 @@ export class ObjectHydrator extends Hydrator {
   /**
    * @inheritDoc
    */
-  hydrateReference<T>(entity: T, meta: EntityMetadata<T>, data: EntityData<T>, factory: EntityFactory, convertCustomTypes = false, schema?: string): void {
+  hydrateReference<T extends object>(entity: T, meta: EntityMetadata<T>, data: EntityData<T>, factory: EntityFactory, convertCustomTypes = false, schema?: string): void {
     const hydrate = this.getEntityHydrator(meta, 'reference');
     const running = this.running;
     this.running = true;
@@ -43,7 +43,7 @@ export class ObjectHydrator extends Hydrator {
   /**
    * @internal Highly performance-sensitive method.
    */
-  getEntityHydrator<T>(meta: EntityMetadata<T>, type: 'full' | 'returning' | 'reference'): EntityHydrator<T> {
+  getEntityHydrator<T extends object>(meta: EntityMetadata<T>, type: 'full' | 'returning' | 'reference'): EntityHydrator<T> {
     const exists = this.hydrators[type].get(meta.className);
 
     if (exists) {
@@ -78,7 +78,7 @@ export class ObjectHydrator extends Hydrator {
       return ret;
     };
 
-    const hydrateScalar = <T, U>(prop: EntityProperty<T>, object: boolean | undefined, path: string[], dataKey: string): string[] => {
+    const hydrateScalar = (prop: EntityProperty<T>, object: boolean | undefined, path: string[], dataKey: string): string[] => {
       const entityKey = path.map(k => this.wrap(k)).join('');
       const preCond = preCondition(dataKey);
       const convertorKey = path.filter(k => !k.match(/\[idx_\d+]/)).map(k => this.safeKey(k)).join('_');
@@ -99,7 +99,7 @@ export class ObjectHydrator extends Hydrator {
           `      const value = convertToJSValue_${convertorKey}(data${dataKey});`,
         );
 
-        if (prop.customType.ensureComparable()) {
+        if (prop.customType.ensureComparable(meta, prop)) {
           ret.push(`      data${dataKey} = convertToDatabaseValue_${convertorKey}(value);`);
         }
 
@@ -158,7 +158,7 @@ export class ObjectHydrator extends Hydrator {
         }
       }
 
-      if (prop.customType?.ensureComparable()) {
+      if (prop.customType?.ensureComparable(meta, prop)) {
         context.set(`convertToDatabaseValue_${this.safeKey(prop.name)}`, (val: any) => prop.customType.convertToDatabaseValue(val, this.platform, { mode: 'hydration' }));
 
         ret.push(`  if (data${dataKey} != null && convertCustomTypes) {`);
@@ -210,9 +210,10 @@ export class ObjectHydrator extends Hydrator {
 
     const parseObjectEmbeddable = (prop: EntityProperty, dataKey: string, ret: string[]): void => {
       if (!this.platform.convertsJsonAutomatically() && (prop.object || prop.array)) {
+        context.set('parseJsonSafe', parseJsonSafe);
         ret.push(
           `  if (typeof data${dataKey} === 'string') {`,
-          `    data${dataKey} = JSON.parse(data${dataKey});`,
+          `    data${dataKey} = parseJsonSafe(data${dataKey});`,
           `  }`,
         );
       }
@@ -341,7 +342,7 @@ export class ObjectHydrator extends Hydrator {
     return hydrator;
   }
 
-  private createCollectionItemMapper<T>(prop: EntityProperty): string[] {
+  private createCollectionItemMapper<T extends object>(prop: EntityProperty): string[] {
     const meta = this.metadata.get(prop.type);
     const lines: string[] = [];
 

packages/core/src/platforms/Platform.ts
@@ -11,9 +11,10 @@ import {
   ArrayType, BigIntType, BlobType, BooleanType, DateType, DecimalType, DoubleType, JsonType, SmallIntType, TimeType,
   TinyIntType, Type, UuidType, StringType, IntegerType, FloatType, DateTimeType, TextType, EnumType, UnknownType, MediumIntType,
 } from '../types';
-import { Utils } from '../utils/Utils';
+import { parseJsonSafe, Utils } from '../utils/Utils';
 import { ReferenceType } from '../enums';
 import type { MikroORM } from '../MikroORM';
+import type { TransformContext } from '../types/Type';
 
 export const JsonProperty = Symbol('JsonProperty');
 
@@ -320,8 +321,17 @@ export abstract class Platform {
     throw new Error('Full text searching is not supported by this driver.');
   }
 
+  // TODO v6: remove the `marshall` parameter
   convertsJsonAutomatically(marshall = false): boolean {
-    return !marshall;
+    return true;
+  }
+
+  convertJsonToDatabaseValue(value: unknown, context?: TransformContext): unknown {
+    return JSON.stringify(value);
+  }
+
+  convertJsonToJSValue(value: unknown): unknown {
+    return parseJsonSafe(value);
   }
 
   getRepositoryClass<T extends object>(): Constructor<EntityRepository<T>> {

packages/core/src/types/JsonType.ts
@@ -1,28 +1,29 @@
+import type { TransformContext } from './Type';
 import { Type } from './Type';
 import type { Platform } from '../platforms';
-import type { EntityProperty } from '../typings';
-import { Utils } from '../utils';
+import type { EntityMetadata, EntityProperty } from '../typings';
 
 export class JsonType extends Type<unknown, string | null> {
 
-  convertToDatabaseValue(value: unknown, platform: Platform): string | null {
-    if (platform.convertsJsonAutomatically(true) || value === null) {
-      return value as string;
+  // TODO v6: remove the boolean variant
+  convertToDatabaseValue(value: unknown, platform: Platform, context?: TransformContext | boolean): string | null {
+    if (value == null) {
+      return value as null;
     }
 
-    return JSON.stringify(value);
+    return platform.convertJsonToDatabaseValue(value, typeof context === 'boolean' ? { fromQuery: context } : context) as string;
   }
 
   convertToJSValue(value: string | unknown, platform: Platform): unknown {
-    if (!platform.convertsJsonAutomatically() && Utils.isString(value)) {
-      return JSON.parse(value);
-    }
-
-    return value;
+    return platform.convertJsonToJSValue(value);
   }
 
   getColumnType(prop: EntityProperty, platform: Platform): string {
     return platform.getJsonDeclarationSQL();
+  }
+
+  ensureComparable<T extends object>(meta: EntityMetadata<T>, prop: EntityProperty<T>): boolean {
+    return !prop.embedded || !meta.properties[prop.embedded[0]].object;
   }
 
 }

packages/core/src/types/Type.ts
@@ -5,7 +5,7 @@ import type { Constructor, EntityMetadata, EntityProperty } from '../typings';
 export interface TransformContext {
   fromQuery?: boolean;
   key?: string;
-  mode?: 'hydration' | 'query' | 'discovery' | 'serialization';
+  mode?: 'hydration' | 'query' | 'query-data' | 'discovery' | 'serialization';
 }
 
 export abstract class Type<JSType = string, DBType = JSType> {
@@ -53,7 +53,7 @@ export abstract class Type<JSType = string, DBType = JSType> {
    * as often the raw database response is not the same as the `convertToDatabaseValue` result.
    * This allows to disable the additional conversion in case you know it is not needed.
    */
-  ensureComparable(): boolean {
+  ensureComparable<T extends object>(meta: EntityMetadata<T>, prop: EntityProperty<T>): boolean {
     return true;
   }
 

packages/core/src/types/index.ts
@@ -1,4 +1,4 @@
-import { Type } from './Type';
+import { Type, TransformContext } from './Type';
 import { DateType } from './DateType';
 import { TimeType } from './TimeType';
 import { DateTimeType } from './DateTimeType';
@@ -24,7 +24,7 @@ import { UnknownType } from './UnknownType';
 export {
   Type, DateType, TimeType, DateTimeType, BigIntType, BlobType, ArrayType, EnumArrayType, EnumType,
   JsonType, IntegerType, SmallIntType, TinyIntType, MediumIntType, FloatType, DoubleType, BooleanType, DecimalType,
-  StringType, UuidType, TextType, UnknownType,
+  StringType, UuidType, TextType, UnknownType, TransformContext,
 };
 
 export const types = {

commit a7d1d09e710497d08d2c12346a76c664ed2d67e5

fix(postgres): do not convert `date` column type to `Date` object automatically
This fixes a regression introduced in #4194.

Closes #4276

packages/postgresql/src/PostgreSqlConnection.ts
@@ -18,11 +18,11 @@ export class PostgreSqlConnection extends AbstractSqlConnection {
     const ret = super.getConnectionOptions() as Knex.PgConnectionConfig;
     const types = new TypeOverrides();
     [1082].forEach(oid => types.setTypeParser(oid, str => str)); // date type
+    ret.types = types as any;
 
     if (this.config.get('forceUtcTimezone')) {
       [1114].forEach(oid => types.setTypeParser(oid, str => new Date(str + 'Z'))); // timestamp w/o TZ type
       ret.parseInputDatesAsUTC = true;
-      ret.types = types as any;
     }
 
     return ret;

tests/MikroORM.test.ts
@@ -1,3 +1,5 @@
+import TypeOverrides from 'pg/lib/type-overrides';
+
 const knex = jest.fn();
 (knex as any).Client = Object;
 const raw = jest.fn();
@@ -156,6 +158,7 @@ describe('MikroORM', () => {
       user: 'postgres',
       password: 'pass1',
       database: 'mikro-orm-test',
+      types: expect.any(TypeOverrides),
     });
 
     await MikroORM.init({
@@ -168,6 +171,7 @@ describe('MikroORM', () => {
       user: 'postgres',
       password: 'pass2',
       database: 'mikro-orm-test',
+      types: expect.any(TypeOverrides),
     });
 
     await MikroORM.init({
@@ -180,6 +184,7 @@ describe('MikroORM', () => {
       user: 'postgres',
       password: 'pass3',
       database: 'mikro-orm-test',
+      types: expect.any(TypeOverrides),
     });
 
     await MikroORM.init({