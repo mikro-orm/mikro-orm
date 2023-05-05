d9d325b70c6ba7c5e65b95e893dad0a787e6a11c
Spencer Kaiser <SpencerKaiser@users.noreply.github.com>
2023-05-05 05:34:32 -0500

    feat(core): allow overriding global logging options on per-query basis (#4273)
    Closes #4223

--- packages/knex/src/query/QueryBuilder.ts
+++ packages/knex/src/query/QueryBuilder.ts
@@ -33,7 +33,7 @@ import {
   type QueryOrderMap,
   type QueryResult,
   type RequiredEntityData,
-  type LoggerContext,
+  type LoggingOptions,
 } from '@mikro-orm/core';
 import { QueryType } from './enums';
 import type { AbstractSqlDriver } from '../AbstractSqlDriver';
@@ -134,7 +134,7 @@ export class QueryBuilder<T extends object = AnyEntity> {
               alias?: string,
               private connectionType?: ConnectionType,
               private readonly em?: SqlEntityManager,
-              private readonly loggerContext?: LoggerContext) {
+              private readonly logging?: LoggingOptions) {
     if (alias) {
       this.aliasCounter++;
       this._explicitAlias = true;
@@ -659,7 +659,7 @@ export class QueryBuilder<T extends object = AnyEntity> {
 
     const write = method === 'run' || !this.platform.getConfig().get('preferReadReplicas');
     const type = this.connectionType || (write ? 'write' : 'read');
-    const res = await this.driver.getConnection(type).execute(query.sql, query.bindings as any[], method, this.context, this.loggerContext);
+    const res = await this.driver.getConnection(type).execute(query.sql, query.bindings as any[], method, this.context, this.logging);
     const meta = this.mainAlias.metadata;
 
     if (!options.mapResults || !meta) {
