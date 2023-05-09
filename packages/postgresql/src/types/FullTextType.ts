07c92bb2dacb2b644f6586f491d160108feee8d0
jsprw <56411932+jsprw@users.noreply.github.com>
2023-05-09 11:39:14 +0200

    feat(postgres): add support for weighted tsvectors and a custom regconfig (#3805)
    Adds support for a custom regconfig and weighted tsvectors as requested
    by
    https://github.com/mikro-orm/mikro-orm/pull/3317#issuecomment-1330035868.
    
    For more context, see
    https://github.com/mikro-orm/mikro-orm/pull/3317#issuecomment-1330555809.
    
    Docs: also (hopefully) fixed the tabs which caused the confusion as seen
    here:
    https://github.com/mikro-orm/mikro-orm/pull/3317#issuecomment-1279882693.

--- packages/postgresql/src/types/FullTextType.ts
+++ packages/postgresql/src/types/FullTextType.ts
@@ -1,6 +1,21 @@
-import { Type } from '@mikro-orm/core';
+import { raw, Type } from '@mikro-orm/core';
+import type { TransformContext, RawQueryFragment } from '@mikro-orm/core';
+import type { PostgreSqlPlatform } from '../PostgreSqlPlatform';
 
-export class FullTextType extends Type<string, string> {
+// Postgres has four levels of full text weights
+// https://www.postgresql.org/docs/current/textsearch-controls.html
+type FullTextWeight = 'A' | 'B' | 'C' | 'D';
+
+// Null types are allowed, mainly for readability so the user does not have to do null checks
+// themselves and keep using `onUpdate: (book: Book) => { A: book.title, B: book.description }`
+// with nullable properties.
+export type WeightedFullTextValue = { [K in FullTextWeight]?: string | null };
+
+export class FullTextType extends Type<string | WeightedFullTextValue, string | null | RawQueryFragment> {
+
+  constructor(public regconfig: string = 'simple') {
+    super();
+  }
 
   override compareAsType(): string {
     return 'string';
@@ -10,8 +25,57 @@ export class FullTextType extends Type<string, string> {
     return 'tsvector';
   }
 
-  override convertToDatabaseValueSQL(key: string) {
-    return `to_tsvector('simple', ${key})`;
+  // Use convertToDatabaseValue to prepare insert queries as this method has
+  // access to the raw JS value. Return Knex#raw to prevent QueryBuilderHelper#mapData
+  // from sanitizing the returned chaing of SQL functions.
+  override convertToDatabaseValue(value: string | WeightedFullTextValue, platform: PostgreSqlPlatform, context?: TransformContext | boolean): string | null | RawQueryFragment {
+    // Don't convert to values from select queries to the to_tsvector notation
+    // these should be compared as string using a special oparator or function
+    // this behaviour is defined in Platform#getFullTextWhereClause.
+    // This is always a string.
+    if (typeof context === 'object' && context.fromQuery) {
+      return value as string;
+    }
+
+    // Null values should not be processed
+    if (!value) {
+      return null;
+    }
+
+    // the object from that looks like { A: 'test data', B: 'test data2' ... }
+    // must be converted to
+    // setweight(to_tsvector(regconfig, value), A) || setweight(to_tsvector(regconfig, value), B)... etc
+    // use Knex#raw to do binding of the values sanitization of the boundvalues
+    // as we return a raw string which should not be sanitzed anymore
+    if (typeof value === 'object') {
+      const bindings: string[] = [];
+      const sqlParts: string[] = [];
+
+      for (const [weight, data] of Object.entries(value)) {
+        // Check whether the weight is valid according to Postgres,
+        // Postgres allows the weight to be upper and lowercase.
+        if (!['A', 'B', 'C', 'D'].includes(weight.toUpperCase())) {
+          throw new Error('Weight should be one of A, B, C, D.');
+        }
+
+        // Ignore all values that are not a string
+        if (typeof data === 'string') {
+          sqlParts.push('setweight(to_tsvector(?, ?), ?)');
+          bindings.push(this.regconfig, data, weight);
+        }
+      }
+
+      // Return null if the object has no valid strings
+      if (sqlParts.length === 0) {
+        return null;
+      }
+
+      // Join all the `setweight` parts using the PostgreSQL tsvector `||` concatenation operator
+      return raw(sqlParts.join(' || '), bindings);
+    }
+
+    // if it's not an object, it is expected to be string which does not have to be wrapped in setweight.
+    return raw('to_tsvector(?, ?)', [this.regconfig, value]);
   }
 
 }
