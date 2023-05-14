033b71d8ecb3313a51fb042b088c886f303c72f0
Martin Adámek <banan23@gmail.com>
2023-05-14 21:15:28 +0200

    fix(core): deprecate `@Subscriber()` decorator

--- packages/core/src/decorators/Subscriber.ts
+++ packages/core/src/decorators/Subscriber.ts
@@ -2,6 +2,7 @@ import type { Constructor } from '../typings';
 import { MetadataStorage } from '../metadata';
 import type { EventSubscriber } from '../events';
 
+/** @deprecated This decorator will be removed in v6, prefer the `subscribers` option in the ORM config. */
 export function Subscriber() {
   return function (target: Constructor<EventSubscriber>) {
     const subscribers = MetadataStorage.getSubscriberMetadata();
