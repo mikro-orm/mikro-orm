f84f6115ffdf9fff38163ae781dcf08bb4f5bd07
Martin Adámek <banan23@gmail.com>
2023-05-24 17:45:12 +0200

    chore: improve decorator types

--- tests/features/cursor-based-pagination/complex-cursor.test.ts
+++ tests/features/cursor-based-pagination/complex-cursor.test.ts
@@ -51,7 +51,7 @@ describe.each(['sqlite', 'better-sqlite', 'mysql', 'postgresql', 'mongo'] as con
 
     orm = await MikroORM.init({
       entities: [User],
-      dbName: type.includes('sqlite') ? ':memory:' : 'mikro_orm_cursor',
+      dbName: type.includes('sqlite') ? ':memory:' : 'mikro_orm_cursor_complex',
       driver: PLATFORMS[type],
       loggerFactory: options => new SimpleLogger(options),
       ...options,
