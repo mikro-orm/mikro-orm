42dbaf37a9db63256e8831a2982dcbf806271e00
Martin Adámek <banan23@gmail.com>
2023-06-09 22:09:45 +0200

    refactor: fix aliasing of STI conditions when joining m:n
    Closes #4422

--- packages/knex/src/query/QueryBuilderHelper.ts
+++ packages/knex/src/query/QueryBuilderHelper.ts
@@ -213,6 +213,7 @@ export class QueryBuilderHelper {
 
     return {
       prop, type, cond, ownerAlias, alias,
+      inverseAlias: ownerAlias,
       table: pivotMeta.collection,
       schema: pivotMeta.schema,
       joinColumns: prop.joinColumns,
@@ -249,7 +250,7 @@ export class QueryBuilderHelper {
 
       if (join.prop.targetMeta!.discriminatorValue && !join.path?.endsWith('[pivot]')) {
         const typeProperty = join.prop.targetMeta!.root.discriminatorColumn!;
-        const alias = !join.prop.owner ? join.inverseAlias ?? join.alias : join.ownerAlias;
+        const alias = join.inverseAlias ?? join.alias;
         join.cond[`${alias}.${typeProperty}`] = join.prop.targetMeta!.discriminatorValue;
       }
 
