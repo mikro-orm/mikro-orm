59a76c9002608fee242287ae168f7c9da0010ae4
Elliot Maisl <elliot.maisl@gmail.com>
2023-07-18 20:34:27 +0200

    fix(seeder): allow run method to return without promise

--- packages/seeder/src/Seeder.ts
+++ packages/seeder/src/Seeder.ts
@@ -2,7 +2,7 @@ import type { Dictionary, EntityManager } from '@mikro-orm/core';
 
 export abstract class Seeder<T extends Dictionary = Dictionary> {
 
-  abstract run(em: EntityManager, context?: T): Promise<void>;
+  abstract run(em: EntityManager, context?: T): void | Promise<void>;
 
   protected async call(em: EntityManager, seeders: { new(): Seeder }[], context: T = {} as T): Promise<void> {
     for (const Seeder of seeders) {
