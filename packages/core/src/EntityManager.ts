0dfe1f225719c88240354e8472b22552ce6a3593
Martin Adámek <banan23@gmail.com>
2023-09-20 10:07:17 +0200

    fix(core): respect schema option for entity instances in `em.insert/Many`
    Closes #4424

--- packages/core/src/EntityManager.ts
+++ packages/core/src/EntityManager.ts
@@ -1190,6 +1190,10 @@ export class EntityManager<D extends IDatabaseDriver = IDatabaseDriver> {
     }
 
     if (Utils.isEntity<Entity>(data)) {
+      if (options.schema && helper(data).getSchema() == null) {
+        helper(data).setSchema(options.schema);
+      }
+
       // the entity might have been created via `em.create()`, which adds it to the persist stack automatically
       em.unitOfWork.getPersistStack().delete(data);
 
@@ -1231,6 +1235,10 @@ export class EntityManager<D extends IDatabaseDriver = IDatabaseDriver> {
     if (Utils.isEntity<Entity>(data[0])) {
       const meta = helper<Entity>(data[0]).__meta;
       const css = data.map(row => {
+        if (options.schema && helper(row).getSchema() == null) {
+          helper(row).setSchema(options.schema);
+        }
+
         // the entity might have been created via `em.create()`, which adds it to the persist stack automatically
         em.unitOfWork.getPersistStack().delete(row);
 
