532f4c6380fe280cfe5a22708fcb90c0cc5ff5f7
Strengthless <dickykam3@gmail.com>
2023-10-07 21:31:53 +0800

    test(gh4796): insert array of single backslash without errors

---
+++ tests/issues/GH4796.test.ts
@@ -0,0 +1,51 @@
+import {
+  Entity,
+  OptionalProps,
+  PrimaryKey,
+  Property,
+  SimpleLogger,
+} from '@mikro-orm/core';
+import { MikroORM } from '@mikro-orm/postgresql';
+import { mockLogger } from '../helpers';
+
+@Entity()
+class User {
+
+  [OptionalProps]?: 'options';
+
+  @PrimaryKey()
+  id!: number;
+
+  @Property({ type: 'string[]', default: ['foo'] })
+  options = ['foo'];
+
+}
+
+let orm: MikroORM;
+
+beforeAll(async () => {
+  orm = await MikroORM.init({
+    entities: [User],
+    dbName: 'mikro_orm_test_gh_4796',
+    loggerFactory: options => new SimpleLogger(options),
+  });
+  await orm.schema.refreshDatabase();
+});
+
+afterAll(async () => {
+  await orm.close();
+});
+
+test('4796', async () => {
+  const mock = mockLogger(orm, ['query', 'query-params']);
+  const u1 = orm.em.create(User, { options: ['\\'] });
+  await orm.em.flush();
+
+  expect(mock.mock.calls).toEqual([
+    ['[query] begin'],
+    [
+      '[query] insert into "user" ("options") values ( E\'{"\\\\\\\\"}\') returning "id", "options"',
+    ],
+    ['[query] commit'],
+  ]);
+});
