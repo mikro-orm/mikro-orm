3a0ad4ece046cb58d718255e8f5347310e114d43
AbdlrahmanSaberAbdo <abdlrahmansaber652@gmail.com>
2023-10-18 14:50:45 +0300

    feat: add new method in Utils `stripRelativePath` t

--- packages/reflection/src/TsMorphMetadataProvider.ts
+++ packages/reflection/src/TsMorphMetadataProvider.ts
@@ -136,9 +136,7 @@ export class TsMorphMetadataProvider extends MetadataProvider {
     if (!this.sources) {
       await this.initSourceFiles();
     }
-
-    const source = this.sources.find(s => s.getFilePath().endsWith(tsPath.replace(/^\.+/, '')));
-
+    const source = this.sources.find(s => s.getFilePath().endsWith(Utils.stripRelativePath(tsPath)));
     if (!source && validate) {
       throw new MetadataError(`Source file '${tsPath}' not found. Check your 'entitiesTs' option and verify you have 'compilerOptions.declaration' enabled in your 'tsconfig.json'. If you are using webpack, see https://bit.ly/35pPDNn`);
     }
