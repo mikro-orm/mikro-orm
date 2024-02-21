e2d52b0b2fd86d7b353da5f1e04ce2e37275d6c5
Martin Adámek <banan23@gmail.com>
2024-02-21 21:34:37 +0100

    ci: retry tests only in CI

--- tests/setup.ts
+++ tests/setup.ts
@@ -4,6 +4,9 @@ process.env.MIKRO_ORM_ALLOW_GLOBAL_CONTEXT = '1';
 process.env.MIKRO_ORM_ALLOW_GLOBAL_CLI = '1';
 process.env.MIKRO_ORM_ALLOW_VERSION_MISMATCH = '1';
 
+if (process.env.RETRY_TESTS) {
+  jest.retryTimes(+process.env.RETRY_TESTS);
+}
+
 jest.restoreAllMocks();
-jest.retryTimes(1);
 MetadataStorage.clear();
