c8021da020d7afa0f338319811a72ef4c1877d12
Steve Keep <85143753+steve-keep@users.noreply.github.com>
2024-02-26 22:39:45 +1100

    fix(mysql): apply current context when fetching `auto_increment_increment` (#5280)
    Closes #5279
    
    ---------
    
    Co-authored-by: Steve Keep <stephen.keep@techin.site>
    Co-authored-by: Martin Adámek <banan23@gmail.com>

---
+++ tests/issues/GH5279.test.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryKey } from '@mikro-orm/core';
+import { MikroORM } from '@mikro-orm/mysql';
+
+@Entity()
+class Product {
+
+  @PrimaryKey()
+  id!: string;
+
+}
+
+test('should create an entity using a single connection', async () => {
+  const orm = await MikroORM.init({
+    dbName: 'mikro_orm_5279',
+    entities: [Product],
+    port: 3308,
+    pool: {
+      min: 1,
+      max: 1,
+    },
+  });
+  await orm.schema.dropSchema();
+  await orm.schema.createSchema();
+  await orm.schema.ensureDatabase();
+
+  const em = orm.em.fork();
+
+  const product = new Product();
+  product.id = '1';
+  await em.persistAndFlush(product);
+
+  expect(product.id).toBe('1');
+
+  await orm.close(true);
+});
