6324487cb16226f23f84e2dd936ae2c626809e3a
Martin Adámek <banan23@gmail.com>
2024-07-01 22:52:25 +0200

    feat(libsql): support connecting to remote turso database
    To be able to connect to a remote [Turso](https://docs.turso.tech/introduction) database, you need to use the `@mikro-orm/libsql` driver. Use the `password` option to set the `authToken`.
    
    ```ts
    import { defineConfig } from '@mikro-orm/libsql';
    
    export default defineConfig({
      dbName: process.env.LIBSQL_URL,
      password: process.env.LIBSQL_AUTH_TOKEN,
    });
    ```
    
    To set the additional options like `syncUrl` or `syncPeriod`, use the `driverOptions.connection`:
    
    ```ts
    import { defineConfig } from '@mikro-orm/libsql';
    
    export default defineConfig({
      dbName: 'local.db',
      password: process.env.LIBSQL_AUTH_TOKEN,
      driverOptions: {
        connection: {
          syncUrl: process.env.LIBSQL_URL,
          syncPeriod: 0.5, // 500ms
        },
      },
    });
    ```

--- packages/libsql/src/LibSqlConnection.ts
+++ packages/libsql/src/LibSqlConnection.ts
@@ -1,10 +1,22 @@
-import { LibSqlKnexDialect, BaseSqliteConnection } from '@mikro-orm/knex';
+import { LibSqlKnexDialect, BaseSqliteConnection, Utils, type Knex } from '@mikro-orm/knex';
 
 export class LibSqlConnection extends BaseSqliteConnection {
 
   override createKnex() {
     this.client = this.createKnexClient(LibSqlKnexDialect as any);
     this.connected = true;
+  }
+
+  protected override getKnexOptions(type: string): Knex.Config {
+    return Utils.mergeConfig({
+      client: type,
+      connection: {
+        filename: this.config.get('dbName'),
+        authToken: this.config.get('password'),
+      },
+      pool: this.config.get('pool'),
+      useNullAsDefault: true,
+    }, this.config.get('driverOptions'));
   }
 
 }
