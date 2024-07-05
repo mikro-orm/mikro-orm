aa54ab43ea4da5225fecd4cca8cd9bdd47553989
Martin Adámek <banan23@gmail.com>
2024-07-05 10:53:25 +0200

    coverage

--- tests/UnderscoreNamingStrategy.test.ts
+++ tests/UnderscoreNamingStrategy.test.ts
@@ -1,9 +1,10 @@
-import { UnderscoreNamingStrategy } from '@mikro-orm/core';
+import { ReferenceKind, UnderscoreNamingStrategy } from '@mikro-orm/core';
 
 describe('UnderscoreNamingStrategy', () => {
 
+  const ns = new UnderscoreNamingStrategy();
+
   test('should convert camel case to snake case', async () => {
-    const ns = new UnderscoreNamingStrategy();
     expect(ns.classToTableName('BookTag')).toBe('book_tag');
     expect(ns.joinColumnName('bookTag')).toBe('book_tag_id');
     expect(ns.joinKeyColumnName('BookTag')).toBe('book_tag_id');
@@ -13,13 +14,20 @@ describe('UnderscoreNamingStrategy', () => {
   });
 
   test('should convert column names to camel cased properties', async () => {
-    const ns = new UnderscoreNamingStrategy();
     expect(ns.columnNameToProperty('BookTag')).toBe('BookTag');
     expect(ns.columnNameToProperty('bookTag')).toBe('bookTag');
     expect(ns.columnNameToProperty('book_tag')).toBe('bookTag');
     expect(ns.columnNameToProperty('book tag')).toBe('bookTag');
     expect(ns.columnNameToProperty('book-tag')).toBe('bookTag');
     expect(ns.columnNameToProperty('Book__--  _- tag')).toBe('BookTag');
+  });
+
+  test('inverse side name', async () => {
+    expect(ns.inverseSideName('BookTag', 'book', ReferenceKind.MANY_TO_ONE)).toBe('bookTag');
+    expect(ns.inverseSideName('BookTag', 'book', ReferenceKind.ONE_TO_ONE)).toBe('bookTag');
+    expect(ns.inverseSideName('BookTag', 'book', ReferenceKind.ONE_TO_MANY)).toBe('bookTagCollection');
+    expect(ns.inverseSideName('B', 'book', ReferenceKind.ONE_TO_MANY)).toBe('bCollection');
+    expect(ns.inverseSideName('User', 'friends', ReferenceKind.MANY_TO_MANY)).toBe('friendsInverse');
   });
 
 });
