54b30cb8e043cd9cc5d960a609363cc7d5c4ba5a
Martin Adámek <banan23@gmail.com>
2024-07-05 17:33:14 +0200

    fix(postgres): support enum arrays with special characters
    Closes #5781

--- packages/postgresql/src/PostgreSqlPlatform.ts
+++ packages/postgresql/src/PostgreSqlPlatform.ts
@@ -181,8 +181,17 @@ export class PostgreSqlPlatform extends AbstractSqlPlatform {
       return [];
     }
 
-    /* istanbul ignore next */
-    return value.substring(1, value.length - 1).split(',').map(v => v === `""` ? '' : v);
+    return value.substring(1, value.length - 1).split(',').map(v => {
+      if (v === `""`) {
+        return '';
+      }
+
+      if (v.match(/"(.*)"/)) {
+        return v.substring(1, v.length - 1).replaceAll('\\"', '"');
+      }
+
+      return v;
+    });
   }
 
   override getVarcharTypeDeclarationSQL(column: { length?: number }): string {
