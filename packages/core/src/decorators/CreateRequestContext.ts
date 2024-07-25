0c34fa3323bbe66adcdd3a8688a996ecf3a1afce
Martin Adámek <banan23@gmail.com>
2024-07-25 16:19:37 +0200

    Apply suggestions from code review

--- packages/core/src/decorators/CreateRequestContext.ts
+++ packages/core/src/decorators/CreateRequestContext.ts
@@ -17,11 +17,11 @@ export function CreateRequestContext<T>(getContext?: MikroORM | Promise<MikroORM
       let em: unknown;
 
       if (typeof getContext === 'function') {
-        orm = await (getContext(this) ?? (this as any).orm);
-        em = await (getContext(this) ?? (this as any).em);
+        const context = await getContext(this)
+        orm = context ?? await (this as any).orm;
+        em = context ?? await (this as any).em;
       } else if (getContext) {
-        orm = await getContext;
-        em = await getContext;
+        orm = em = await getContext;
       } else {
         orm = await (this as any).orm;
         em = await (this as any).em;
