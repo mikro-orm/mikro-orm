d286727b7cbbf054b572217c343c8232d613f47f
Martin Adámek <banan23@gmail.com>
2024-08-03 14:54:30 +0200

    test: update mongo memory server
    Related: https://github.com/typegoose/mongodb-memory-server/issues/883

--- tests/globalSetup.ts
+++ tests/globalSetup.ts
@@ -1,26 +1,12 @@
 import { MongoMemoryReplSet } from 'mongodb-memory-server-core';
 
 export = async function globalSetup() {
-  let instance;
-  let error;
-
-  for (let i = 0; i < 5; i++) {
-    try {
-      instance = await MongoMemoryReplSet.create({
-        replSet: {
-          name: 'rs',
-          count: 3,
-        },
-      });
-      break;
-    } catch (err) {
-      error = err;
-    }
-  }
-
-  if (!instance) {
-    throw new Error('Cannot start mongodb memory server', { cause: error });
-  }
+  const instance = await MongoMemoryReplSet.create({
+    replSet: {
+      name: 'rs',
+      count: 3,
+    },
+  });
 
   await instance.waitUntilRunning();
   const uri = instance.getUri();
