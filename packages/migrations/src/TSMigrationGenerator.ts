d2f7b83557431da53c0fe0c2011cddee67ceac05
Martin Adámek <banan23@gmail.com>
2024-08-06 09:44:17 +0200

    fix(migrations): use `override` keyword in TS migration files
    Closes #5634

--- packages/migrations/src/TSMigrationGenerator.ts
+++ packages/migrations/src/TSMigrationGenerator.ts
@@ -8,12 +8,12 @@ export class TSMigrationGenerator extends MigrationGenerator {
   generateMigrationFile(className: string, diff: { up: string[]; down: string[] }): string {
     let ret = `import { Migration } from '@mikro-orm/migrations';\n\n`;
     ret += `export class ${className} extends Migration {\n\n`;
-    ret += `  async up(): Promise<void> {\n`;
+    ret += `  override async up(): Promise<void> {\n`;
     diff.up.forEach(sql => ret += this.createStatement(sql, 4));
     ret += `  }\n\n`;
 
     if (diff.down.length > 0) {
-      ret += `  async down(): Promise<void> {\n`;
+      ret += `  override async down(): Promise<void> {\n`;
       diff.down.forEach(sql => ret += this.createStatement(sql, 4));
       ret += `  }\n\n`;
     }
