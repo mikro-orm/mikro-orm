c5ac932bd1c90be206ea582a14b8717402280609
Martin Adámek <banan23@gmail.com>
2024-09-26 10:46:08 +0200

    test: improve test and make it close connections properly

--- tests/issues/GH6057.test.ts
+++ tests/issues/GH6057.test.ts
@@ -1,129 +1,50 @@
-import { EntitySchema, type EntitySchemaMetadata, MikroORM, type Options } from '@mikro-orm/core';
-import { MikroORM as MySqlORM } from '@mikro-orm/mysql';
-import { MikroORM as MariaDbORM } from '@mikro-orm/mariadb';
+import { EntitySchema, IDatabaseDriver, MikroORM } from '@mikro-orm/core';
+import { MySqlDriver } from '@mikro-orm/mysql';
+import { MariaDbDriver } from '@mikro-orm/mariadb';
 
-type Key = 'bigint1' | 'bigint2' | 'bigint3' | 'number1' | 'number2' | 'number3';
-
-type Property = { id: number };
-
-const entity: Record<Key, EntitySchemaMetadata<Property>> = {
-  bigint1: {
-    name: 'entity',
-    properties: { id: { primary: true, type: 'bigint', unsigned: false } },
-  },
-  bigint2: {
-    name: 'entity',
-    properties: { id: { primary: true, type: 'bigint', unsigned: true } },
-  },
-  bigint3: {
-    name: 'entity',
-    properties: { id: { primary: true, type: 'bigint' } },
-  },
-
-  number1: {
-    name: 'entity',
-    properties: { id: { primary: true, type: 'number', unsigned: false } },
-  },
-  number2: {
-    name: 'entity',
-    properties: { id: { primary: true, type: 'number', unsigned: true } },
-  },
-  number3: {
-    name: 'entity',
-    properties: { id: { primary: true, type: 'number' } },
-  },
-};
-
-let orm: MikroORM;
-
-afterEach(async () => {
-  await orm.close(true);
+const bigint1 = new EntitySchema({
+  name: 'entity1',
+  properties: { id: { primary: true, type: 'bigint', unsigned: false } },
+});
+const bigint2 = new EntitySchema({
+  name: 'entity2',
+  properties: { id: { primary: true, type: 'bigint', unsigned: true } },
+});
+const bigint3 = new EntitySchema({
+  name: 'entity3',
+  properties: { id: { primary: true, type: 'bigint' } },
+});
+const int1 = new EntitySchema({
+  name: 'entity4',
+  properties: { id: { primary: true, type: 'number', unsigned: false } },
+});
+const int2 = new EntitySchema({
+  name: 'entity5',
+  properties: { id: { primary: true, type: 'number', unsigned: true } },
+});
+const int3 = new EntitySchema({
+  name: 'entity6',
+  properties: { id: { primary: true, type: 'number' } },
 });
 
-describe('MySQL supports unsigned increment primary keys.', () => {
-  const dbInfo: Options = { dbName: 'GH6057-mysql', port: 3308 };
+describe.each(['mariadb', 'mysql'])('GH 6057 [%s]', type => {
+  let orm: MikroORM;
 
-  test('Should be signed bigint.', async () => {
-    const signedBigintRegex = /^create table `entity` \(`id` bigint not null auto_increment primary key\).*/;
-
-    orm = await MySqlORM.init({ entities: [new EntitySchema(entity.bigint1)], ...dbInfo });
-    const sql1 = await orm.schema.getCreateSchemaSQL({ wrap: false });
-    expect(sql1).toMatch(signedBigintRegex);
+  beforeAll(async () => {
+    orm = await MikroORM.init<IDatabaseDriver>({
+      entities: [bigint1, bigint2, bigint3, int1, int2, int3],
+      dbName: '6057',
+      port: type === 'mysql' ? 3308 : 3309,
+      driver: type === 'mysql' ? MySqlDriver : MariaDbDriver,
+    });
   });
 
-  test('Should be unsigned bigint.', async () => {
-    const unsignedBigintRegex = /^create table `entity` \(`id` bigint unsigned not null auto_increment primary key\).*/;
-
-    orm = await MySqlORM.init({ entities: [new EntitySchema(entity.bigint2)], ...dbInfo });
-    const sql2 = await orm.schema.getCreateSchemaSQL({ wrap: false });
-    expect(sql2).toMatch(unsignedBigintRegex);
-
-    orm = await MySqlORM.init({ entities: [new EntitySchema(entity.bigint3)], ...dbInfo });
-    const sql3 = await orm.schema.getCreateSchemaSQL({ wrap: false });
-    expect(sql3).toMatch(unsignedBigintRegex);
+  afterAll(async () => {
+    await orm.close(true);
   });
 
-  test('Should be signed int.', async () => {
-    const signedIntRegex = /^create table `entity` \(`id` int not null auto_increment primary key\).*/;
-
-    orm = await MySqlORM.init({ entities: [new EntitySchema(entity.number1)], ...dbInfo });
-    const sql4 = await orm.schema.getCreateSchemaSQL({ wrap: false });
-    expect(sql4).toMatch(signedIntRegex);
-  });
-
-  test('Should be unsigned int.', async () => {
-    const unsignedIntRegex = /^create table `entity` \(`id` int unsigned not null auto_increment primary key\).*/;
-
-    orm = await MySqlORM.init({ entities: [new EntitySchema(entity.number2)], ...dbInfo });
-    const sql5 = await orm.schema.getCreateSchemaSQL({ wrap: false });
-    expect(sql5).toMatch(unsignedIntRegex);
-
-    orm = await MySqlORM.init({ entities: [new EntitySchema(entity.number3)], ...dbInfo });
-    const sql6 = await orm.schema.getCreateSchemaSQL({ wrap: false });
-    expect(sql6).toMatch(unsignedIntRegex);
-  });
-});
-
-describe('MariaDB supports unsigned increment primary keys.', () => {
-  const dbInfo: Options = { dbName: 'GH6057-mariadb', port: 3309 };
-
-  test('Should be signed bigint.', async () => {
-    const signedBigintRegex = /^create table `entity` \(`id` bigint not null auto_increment primary key\).*/;
-
-    orm = await MariaDbORM.init({ entities: [new EntitySchema(entity.bigint1)], ...dbInfo });
-    const sql1 = await orm.schema.getCreateSchemaSQL({ wrap: false });
-    expect(sql1).toMatch(signedBigintRegex);
-  });
-
-  test('Should be unsigned bigint.', async () => {
-    const unsignedBigintRegex = /^create table `entity` \(`id` bigint unsigned not null auto_increment primary key\).*/;
-
-    orm = await MariaDbORM.init({ entities: [new EntitySchema(entity.bigint2)], ...dbInfo });
-    const sql2 = await orm.schema.getCreateSchemaSQL({ wrap: false });
-    expect(sql2).toMatch(unsignedBigintRegex);
-
-    orm = await MariaDbORM.init({ entities: [new EntitySchema(entity.bigint3)], ...dbInfo });
-    const sql3 = await orm.schema.getCreateSchemaSQL({ wrap: false });
-    expect(sql3).toMatch(unsignedBigintRegex);
-  });
-
-  test('Should be signed int.', async () => {
-    const signedIntRegex = /^create table `entity` \(`id` int not null auto_increment primary key\).*/;
-
-    orm = await MariaDbORM.init({ entities: [new EntitySchema(entity.number1)], ...dbInfo });
-    const sql4 = await orm.schema.getCreateSchemaSQL({ wrap: false });
-    expect(sql4).toMatch(signedIntRegex);
-  });
-
-  test('Should be unsigned int.', async () => {
-    const unsignedIntRegex = /^create table `entity` \(`id` int unsigned not null auto_increment primary key\).*/;
-
-    orm = await MariaDbORM.init({ entities: [new EntitySchema(entity.number2)], ...dbInfo });
-    const sql5 = await orm.schema.getCreateSchemaSQL({ wrap: false });
-    expect(sql5).toMatch(unsignedIntRegex);
-
-    orm = await MariaDbORM.init({ entities: [new EntitySchema(entity.number3)], ...dbInfo });
-    const sql6 = await orm.schema.getCreateSchemaSQL({ wrap: false });
-    expect(sql6).toMatch(unsignedIntRegex);
+  test('check schema', async () => {
+    const diff = await orm.schema.getCreateSchemaSQL();
+    expect(diff).toMatchSnapshot();
   });
 });
