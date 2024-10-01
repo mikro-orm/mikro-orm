mikro-orm/mikro-orm

commit 55df57ff1aa84e5d45188d849ba09e91ae6d3642

fix(core): fix diffing of object properties
Closes #6050

packages/core/src/utils/Utils.ts
@@ -61,11 +61,14 @@ export function compareObjects(a: any, b: any) {
     return a.sql === b.sql && compareArrays(a.params, b.params);
   }
 
-  if (a.valueOf !== Object.prototype.valueOf && typeof a.valueOf === 'function' && typeof b.valueOf === 'function') {
-    return a.valueOf() === b.valueOf();
-  }
-
-  if (a.toString !== Object.prototype.toString && typeof a.toString === 'function' && typeof b.toString === 'function') {
+  if (
+    (typeof a === 'function' && typeof b === 'function') ||
+    (typeof a === 'object' && a.client && ['Ref', 'Raw'].includes(a.constructor.name) && typeof b === 'object' && b.client && ['Ref', 'Raw'].includes(b.constructor.name)) || // knex qb
+    (a instanceof Date && b instanceof Date) ||
+    (a instanceof RegExp && b instanceof RegExp) ||
+    (a instanceof String && b instanceof String) ||
+    (a instanceof Number && b instanceof Number)
+  ) {
     return a.toString() === b.toString();
   }
 

tests/features/embeddables/GH2391-2.test.ts
@@ -119,7 +119,7 @@ describe('onCreate and onUpdate in embeddables (GH 2283 and 2391)', () => {
     const tmp1 = line.fooAudit1.archived = new Date(1698010995749);
     await orm.em.flush();
     expect(mock).toHaveBeenCalledTimes(3);
-    expect(mock.mock.calls[1][0]).toMatch('update `my_entity` set `foo_audit1_archivedAt` = 1698010995749, `foo_audit1_updated_at` = 1698010995749, `foo_audit1_nested_audit1_updated_at` = 1698010995749, `bar_audit2` = \'{"updatedAt":"2023-10-22T21:43:15.749Z","created":"2023-10-22T21:43:15.740Z","nestedAudit1":{"updatedAt":"2023-10-22T21:43:15.749Z","created":"2023-10-22T21:43:15.740Z"}}\' where `id` = 1');
+    expect(mock.mock.calls[1][0]).toMatch('update `my_entity` set `foo_audit1_archivedAt` = 1698010995749, `foo_audit1_updated_at` = 1698010995749, `foo_audit1_nested_audit1_updated_at` = 1698010995749 where `id` = 1');
     mock.mockReset();
 
     const tmp2 = line.barAudit2.archived = new Date(1698010995750);

tests/features/embeddables/GH2391.test.ts
@@ -92,7 +92,7 @@ describe('onCreate and onUpdate in embeddables (GH 2283 and 2391)', () => {
     const tmp1 = line.audit1.archived = new Date();
     await orm.em.flush();
     expect(mock).toHaveBeenCalledTimes(3);
-    expect(mock.mock.calls[1][0]).toMatch('update `my_entity` set `audit1_archived` = ?, `audit1_updated` = ?, `audit1_nested_audit1_updated` = ?, `audit2` = ? where `id` = ?');
+    expect(mock.mock.calls[1][0]).toMatch('update `my_entity` set `audit1_archived` = ?, `audit1_updated` = ?, `audit1_nested_audit1_updated` = ? where `id` = ?');
     mock.mockReset();
 
     const tmp2 = line.audit2.archived = new Date();

packages/mongodb/src/MongoPlatform.ts
@@ -79,7 +79,7 @@ export class MongoPlatform extends Platform {
   }
 
   override convertJsonToDatabaseValue(value: unknown): unknown {
-    return Utils.copy(value);
+    return structuredClone(value);
   }
 
   override convertJsonToJSValue(value: unknown, prop: EntityProperty): unknown {

tests/features/custom-types/GH372.test.ts
@@ -99,7 +99,7 @@ test(`multi insert with custom types and knex.raw() (GH #1841)`, async () => {
   await orm.em.flush();
   orm.em.clear();
 
-  const a1 = await orm.em.find(A, {});
+  const a1 = await orm.em.find(A, {}, { orderBy: { id: 1 } });
   expect(a1[0].prop).toEqual({ x: 5, y: 9 });
   expect(a1[1].prop).toEqual({ x: 6, y: 10 });
   expect(a1[2].prop).toEqual({ x: 7, y: 11 });
@@ -112,16 +112,17 @@ test(`multi insert with custom types and knex.raw() (GH #1841)`, async () => {
   await orm.em.flush();
   orm.em.clear();
 
-  const a2 = await orm.em.find(A, {});
+  const a2 = await orm.em.find(A, {}, { orderBy: { id: 1 } });
   expect(a2[0].prop).toEqual({ x: 65, y: 100 });
   expect(a2[1].prop).toEqual({ x: 77, y: 111 });
+  expect(a2[2].prop).toEqual({ x: 7, y: 11 });
 
   expect(mock.mock.calls[0][0]).toMatch('begin');
   expect(mock.mock.calls[1][0]).toMatch('insert into "a" ("prop") values ($1), ($2), ($3) returning "id"');
   expect(mock.mock.calls[2][0]).toMatch('commit');
   expect(mock.mock.calls[3][0]).toMatch('select "a0".* from "a" as "a0"');
   expect(mock.mock.calls[4][0]).toMatch('begin');
-  expect(mock.mock.calls[5][0]).toMatch('update "a" set "prop" = case when ("id" = $1) then $2 when ("id" = $3) then $4 when ("id" = $5) then $6 else "prop" end where "id" in ($7, $8, $9)');
+  expect(mock.mock.calls[5][0]).toMatch('update "a" set "prop" = case when ("id" = $1) then $2 when ("id" = $3) then $4 else "prop" end where "id" in ($5, $6)');
   expect(mock.mock.calls[6][0]).toMatch('commit');
   expect(mock.mock.calls[7][0]).toMatch('select "a0".* from "a" as "a0"');
 });

tests/features/custom-types/GH6050.test.ts
@@ -1,0 +1,79 @@
+import { Entity, MikroORM, ObjectId, PrimaryKey, Property } from '@mikro-orm/mongodb';
+
+type Devices = { mouse: string; keyboard: string };
+
+@Entity()
+class User {
+
+  @PrimaryKey()
+  _id!: ObjectId;
+
+  @Property({ unique: true })
+  email!: string;
+
+  @Property({ type: 'json' })
+  devices?: Devices;
+
+}
+
+let orm: MikroORM;
+
+beforeAll(async () => {
+  orm = await MikroORM.init({
+    dbName: '6050',
+    entities: [User],
+  });
+  await orm.schema.refreshDatabase();
+});
+
+beforeEach(async () => {
+  await orm.schema.clearDatabase();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+it('should populate a json property that contains a default value in its prototype at entity creation', async () => {
+  // create object with prototype containing a default value for the mouse brand
+  const devices: Devices = Object.create({
+    mouse: 'no-brand',
+  });
+  // set actual values
+  devices.mouse = 'acme';
+  devices.keyboard = 'acme';
+  orm.em.create(User, {
+    email: 'test@test.com',
+    devices,
+  });
+  await orm.em.flush();
+  orm.em.clear();
+
+  const user2 = await orm.em.findOneOrFail(User, { email: 'test@test.com' });
+  expect(user2.devices).toEqual({ mouse: 'acme', keyboard: 'acme' }); // this fails, property "mouse" is missing
+});
+
+it('should populate a json property that contains a default value in its prototype at entity update', async () => {
+  orm.em.create(User, {
+    email: 'test@test.com',
+    devices: { mouse: 'nobrand', keyboard: 'nobrand' },
+  });
+  await orm.em.flush();
+  orm.em.clear();
+
+  const user2 = await orm.em.findOneOrFail(User, { email: 'test@test.com' });
+  expect(user2.devices).toEqual({ mouse: 'nobrand', keyboard: 'nobrand' });
+  // create object with prototype containing default value
+  const devices: Devices = Object.create({
+    mouse: 'nobrand',
+  });
+  // set actual values
+  devices.mouse = 'acme';
+  devices.keyboard = 'acme';
+  user2.devices = devices;
+  await orm.em.flush();
+  orm.em.clear();
+
+  const user3 = await orm.em.findOneOrFail(User, { email: 'test@test.com' });
+  expect(user3.devices).toEqual({ mouse: 'acme', keyboard: 'acme' }); // this fails, property "mouse" is missing
+});

commit cd7ce829a07c844b16430056b032db6947e07356

fix(core): fix same-second dates diffing (#6094)
This small PR fixes a minor regression that I think was introduced in
https://github.com/mikro-orm/mikro-orm/commit/55df57ff1aa84e5d45188d849ba09e91ae6d3642.

## Issue
When updating an entity and changing a date property to another one very
close to it (by a difference in microseconds), the change is not
persisted in the database. Indeed, diffing dates by comparing the
`toString()` output doesn't give enough precision to compare
microseconds shifts. Example:
- `toString()` output: `Tue Oct 01 2024 08:54:48 GMT+0000 (Coordinated
Universal Time)`
- `toISOString()` output: `2024-10-01T08:54:48.651Z`

Two dates can have the same `toString()` output even though their
`toISOString()` output is different.

The impact is low and probably only concerns the test scenarios: It's
rare to persist the same entity twice in the same second without
changing anything other than a date.

## Fix
In this PR I'm switching the dates comparison to use `toISOString()` and
adding a new unit test.

---------

Co-authored-by: Martin Adámek <banan23@gmail.com>

docker-compose.yml
@@ -61,6 +61,8 @@ services:
       MSSQL_SA_PASSWORD: Root.Root
       MSSQL_TELEMETRY_ENABLED: 0
       ACCEPT_EULA: 1
+    healthcheck:
+      test: /opt/mssql-tools/bin/sqlcmd -S mssql -U sa -P 'Root.Root' -Q 'select 1'
 
 volumes:
   mysql8:

packages/core/src/utils/Utils.ts
@@ -61,10 +61,13 @@ export function compareObjects(a: any, b: any) {
     return a.sql === b.sql && compareArrays(a.params, b.params);
   }
 
+  if ((a instanceof Date && b instanceof Date)) {
+    return a.toISOString() === b.toISOString();
+  }
+
   if (
     (typeof a === 'function' && typeof b === 'function') ||
     (typeof a === 'object' && a.client && ['Ref', 'Raw'].includes(a.constructor.name) && typeof b === 'object' && b.client && ['Ref', 'Raw'].includes(b.constructor.name)) || // knex qb
-    (a instanceof Date && b instanceof Date) ||
     (a instanceof RegExp && b instanceof RegExp) ||
     (a instanceof String && b instanceof String) ||
     (a instanceof Number && b instanceof Number)

tests/Utils.test.ts
@@ -88,6 +88,7 @@ describe('Utils', () => {
     expect(compareObjects(sql`select 1`, sql`select 1`)).toBe(true);
     expect(compareObjects(sql`select ${1}`, sql`select ${1}`)).toBe(true);
     expect(compareObjects(sql`select ${1}`, sql`select ${2}`)).toBe(false);
+    expect(compareObjects(new Date('2024-10-01T08:54:48.651Z'), new Date('2024-10-01T08:54:48.676Z'))).toBe(false);
     expect(Utils.equals(NaN, NaN)).toBe(true);
   });
 

tests/features/embeddables/GH2391-2.test.ts
@@ -119,7 +119,7 @@ describe('onCreate and onUpdate in embeddables (GH 2283 and 2391)', () => {
     const tmp1 = line.fooAudit1.archived = new Date(1698010995749);
     await orm.em.flush();
     expect(mock).toHaveBeenCalledTimes(3);
-    expect(mock.mock.calls[1][0]).toMatch('update `my_entity` set `foo_audit1_archivedAt` = 1698010995749, `foo_audit1_updated_at` = 1698010995749, `foo_audit1_nested_audit1_updated_at` = 1698010995749 where `id` = 1');
+    expect(mock.mock.calls[1][0]).toMatch('update `my_entity` set `foo_audit1_archivedAt` = 1698010995749, `foo_audit1_updated_at` = 1698010995749, `foo_audit1_nested_audit1_updated_at` = 1698010995749, `bar_audit2` = \'{"updatedAt":"2023-10-22T21:43:15.749Z","created":"2023-10-22T21:43:15.740Z","nestedAudit1":{"updatedAt":"2023-10-22T21:43:15.749Z","created":"2023-10-22T21:43:15.740Z"}}\' where `id` = 1');
     mock.mockReset();
 
     const tmp2 = line.barAudit2.archived = new Date(1698010995750);

tests/features/embeddables/GH2391.test.ts
@@ -92,7 +92,7 @@ describe('onCreate and onUpdate in embeddables (GH 2283 and 2391)', () => {
     const tmp1 = line.audit1.archived = new Date();
     await orm.em.flush();
     expect(mock).toHaveBeenCalledTimes(3);
-    expect(mock.mock.calls[1][0]).toMatch('update `my_entity` set `audit1_archived` = ?, `audit1_updated` = ?, `audit1_nested_audit1_updated` = ? where `id` = ?');
+    expect(mock.mock.calls[1][0]).toMatch('update `my_entity` set `audit1_archived` = ?, `audit1_updated` = ?, `audit1_nested_audit1_updated` = ?, `audit2` = ? where `id` = ?');
     mock.mockReset();
 
     const tmp2 = line.audit2.archived = new Date();