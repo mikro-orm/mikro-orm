mikro-orm/mikro-orm

commit ec521c9792b0a96dd98cfe36fc6c5d77de525b15

fix(core): fix mapping of version properties from batch updates
Closes #6016
Closes #6022

packages/core/src/unit-of-work/ChangeSetPersister.ts
@@ -228,13 +228,14 @@ export class ChangeSetPersister {
 
     const res = await this.driver.nativeUpdateMany(meta.className, cond, changeSets.map(cs => cs.payload), options);
 
-    changeSets.forEach((changeSet, idx) => {
+    for (const changeSet of changeSets) {
       if (res.rows) {
-        this.mapReturnedValues(changeSet.entity, changeSet.payload, res.rows[idx], meta);
+        const row = res.rows.find(row => Utils.equals(Utils.extractPK(row, meta), changeSet.getPrimaryKey()));
+        this.mapReturnedValues(changeSet.entity, changeSet.payload, row, meta);
       }
 
       changeSet.persisted = true;
-    });
+    }
   }
 
   private mapPrimaryKey<T extends object>(meta: EntityMetadata<T>, value: IPrimaryKey, changeSet: ChangeSet<T>): void {

packages/knex/src/AbstractSqlDriver.ts
@@ -703,19 +703,20 @@ export abstract class AbstractSqlDriver<Connection extends AbstractSqlConnection
     const keys = new Set<EntityKey<T>>();
     const fields = new Set<string>();
     const returning = new Set<EntityKey<T>>();
-    data.forEach(row => {
-      Utils.keys(row).forEach(k => {
+
+    for (const row of data) {
+      for (const k of Utils.keys(row)) {
         keys.add(k as EntityKey<T>);
 
         if (Utils.isRawSql(row[k])) {
           returning.add(k);
         }
-      });
-    });
+      }
+    }
 
     // reload generated columns and version fields
     meta?.props
-      .filter(prop => (prop.generated && !prop.primary) || prop.version)
+      .filter(prop => prop.generated || prop.version || prop.primary)
       .forEach(prop => returning.add(prop.name));
 
     const pkCond = Utils.flatten(meta.primaryKeys.map(pk => meta.properties[pk].fieldNames)).map(pk => `${this.platform.quoteIdentifier(pk)} = ?`).join(' and ');
@@ -737,7 +738,7 @@ export abstract class AbstractSqlDriver<Connection extends AbstractSqlConnection
       params.push(value);
     };
 
-    keys.forEach(key => {
+    for (const key of keys) {
       const prop = meta.properties[key];
 
       prop.fieldNames.forEach((fieldName: string, fieldNameIdx: number) => {
@@ -767,7 +768,7 @@ export abstract class AbstractSqlDriver<Connection extends AbstractSqlConnection
 
         return sql;
       });
-    });
+    }
 
     if (meta.versionProperty) {
       const versionProperty = meta.properties[meta.versionProperty];

tests/EntityManager.sqlite2.test.ts
@@ -1054,7 +1054,7 @@ describe.each(['sqlite', 'better-sqlite', 'libsql'] as const)('EntityManager (%s
 
     expect(mock.mock.calls[0][0]).toMatch('begin');
     expect(mock.mock.calls[1][0]).toMatch('select `f0`.`id` from `foo_bar4` as `f0` where ((`f0`.`id` = ? and `f0`.`version` = ?) or (`f0`.`id` = ? and `f0`.`version` = ?))');
-    expect(mock.mock.calls[2][0]).toMatch('update `foo_bar4` set `foo_bar_id` = case when (`id` = ?) then ? else `foo_bar_id` end, `updated_at` = case when (`id` = ?) then ? when (`id` = ?) then ? else `updated_at` end, `version` = `version` + 1 where `id` in (?, ?) returning `version`');
+    expect(mock.mock.calls[2][0]).toMatch('update `foo_bar4` set `foo_bar_id` = case when (`id` = ?) then ? else `foo_bar_id` end, `updated_at` = case when (`id` = ?) then ? when (`id` = ?) then ? else `updated_at` end, `version` = `version` + 1 where `id` in (?, ?) returning `id`, `version`');
     expect(mock.mock.calls[3][0]).toMatch('update `foo_bar4` set `foo_bar_id` = ?, `updated_at` = ?, `version` = `version` + 1 where `id` = ? and `version` = ? returning `version`');
     expect(mock.mock.calls[4][0]).toMatch('commit');
   });

tests/features/single-table-inheritance/GH5224.test.ts
@@ -124,22 +124,6 @@ test('optimistic locks and STI', async () => {
   await orm.em.flush();
   expect(mock.mock.calls[0][0]).toMatch('begin');
   expect(mock.mock.calls[1][0]).toMatch('select `u0`.`id` from `user` as `u0` where ((`u0`.`id` = 1 and `u0`.`version` = 1) or (`u0`.`id` = 2 and `u0`.`version` = 1))');
-  expect(mock.mock.calls[2][0]).toMatch('update `user` set `name` = case when (`id` = 1) then \'new name 1\' when (`id` = 2) then \'new name 2\' else `name` end, `version` = `version` + 1 where `id` in (1, 2) returning `version`');
+  expect(mock.mock.calls[2][0]).toMatch('update `user` set `name` = case when (`id` = 1) then \'new name 1\' when (`id` = 2) then \'new name 2\' else `name` end, `version` = `version` + 1 where `id` in (1, 2) returning `id`, `version`');
   expect(mock.mock.calls[3][0]).toMatch('commit');
 });
-
-// describe('Polymorphic versioned entities', async () => {
-//   test('Should not fail with OptimisticLockError', async () => {
-//       /**
-//        * When the previous transactional context ends, it leads to the
-//        * following exception:
-//        *
-//        * NotNullConstraintViolationException:
-//        * update "public"."User" set "email" = case when ("id" = 1)
-//        * then NULL when ("id" = 2) then NULL else "email" end,
-//        * "version" = "version" + 1 where "id" in (1, 2)
-//        * returning "version" - null value in column "email" of
-//        * relation "User" violates not-null constraint
-//        */
-//   });
-// });

tests/issues/GH6016.test.ts
@@ -1,0 +1,91 @@
+import { Collection, Entity, ManyToOne, MikroORM, OneToMany, OptionalProps, PrimaryKey, Property } from '@mikro-orm/sqlite';
+
+@Entity()
+class TestCase {
+
+  [OptionalProps]?: 'version';
+
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+
+  @Property({ version: true })
+  version!: number;
+
+  @OneToMany(() => TestCaseRevision, 'testCase')
+  revisions = new Collection<TestCaseRevision>(this);
+
+}
+
+@Entity()
+class TestCaseRevision {
+
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+
+  @Property() // Regular field
+  version!: number;
+
+  @ManyToOne(() => TestCase)
+  testCase!: TestCase;
+
+}
+
+let orm: MikroORM;
+
+beforeAll(async () => {
+  orm = await MikroORM.init({
+    dbName: ':memory:',
+    entities: [TestCase, TestCaseRevision],
+  });
+
+  await orm.schema.refreshDatabase();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+it('test', async () => {
+  await orm.em.insertMany(TestCase, [
+    { name: 'a', version: 10 }, // id 1
+    { name: 'b', version: 100 }, // id 2
+    { name: 'c', version: 1 }, // id 3
+  ]);
+
+  await orm.em.insertMany(TestCaseRevision, [
+    { testCase: 3, name: 'c', version: 1 },
+    { testCase: 1, name: 'a', version: 10 },
+    { testCase: 2, name: 'b', version: 100 },
+  ]);
+
+  const revisions = await orm.em.findAll(TestCaseRevision, {
+    populate: ['testCase'],
+  });
+
+  const testCases = revisions.map(it => it.testCase);
+
+  expect(testCases).toMatchObject([
+    { name: 'c', version: 1, id: 3 },
+    { name: 'a', version: 10, id: 1 },
+    { name: 'b', version: 100, id: 2 },
+  ]);
+
+  testCases[0].name = 'c0';
+  testCases[1].name = 'a0';
+  testCases[2].name = 'b0';
+
+  await orm.em.flush();
+
+  // Correct result
+  expect(testCases).toMatchObject([
+    { name: 'c0', version: 2, id: 3 },
+    { name: 'a0', version: 11, id: 1 },
+    { name: 'b0', version: 101, id: 2 },
+  ]);
+});

commit 6caa82acf3d57ab7fcd11af7f625c48d8e989222

perf(core): simplify lookup of returned values from batch updates
This perf issue was introduced by ec521c9.

Signed-off-by: Martin Adámek <banan23@gmail.com>

packages/core/src/unit-of-work/ChangeSetPersister.ts
@@ -220,17 +220,23 @@ export class ChangeSetPersister {
       convertCustomTypes: false,
       processCollections: false,
     });
-    const cond = changeSets.map(cs => cs.getPrimaryKey(true) as FilterQuery<T>);
+    const cond = [] as FilterQuery<T>[];
+    const payload = [] as EntityData<T>[];
 
-    changeSets.forEach((changeSet, idx) => {
-      this.checkConcurrencyKeys(meta, changeSet, cond[idx]);
-    });
+    for (const changeSet of changeSets) {
+      const where = changeSet.getPrimaryKey(true) as FilterQuery<T>;
+      this.checkConcurrencyKeys(meta, changeSet, where);
+      cond.push(where);
+      payload.push(changeSet.payload);
+    }
 
-    const res = await this.driver.nativeUpdateMany(meta.className, cond, changeSets.map(cs => cs.payload), options);
+    const res = await this.driver.nativeUpdateMany(meta.className, cond, payload, options);
+    const map = new Map<string, Dictionary>();
+    res.rows?.forEach(item => map.set(Utils.getCompositeKeyHash(item as EntityData<T>, meta, true, this.platform, true), item));
 
     for (const changeSet of changeSets) {
       if (res.rows) {
-        const row = res.rows.find(row => Utils.equals(Utils.extractPK(row, meta), changeSet.getPrimaryKey()));
+        const row = map.get(helper(changeSet.entity).getSerializedPrimaryKey());
         this.mapReturnedValues(changeSet.entity, changeSet.payload, row, meta);
       }
 