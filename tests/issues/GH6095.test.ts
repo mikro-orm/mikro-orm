b654517e4ad628b55a7569e397bff9411445d43c
lsh4711 <dowkwll@naver.com>
2024-10-03 02:05:57 +0900

    remove invalid entity in test

--- tests/issues/GH6095.test.ts
+++ tests/issues/GH6095.test.ts
@@ -1,7 +1,7 @@
 import { Entity, MikroORM, PrimaryKey } from '@mikro-orm/better-sqlite';
 
 @Entity()
-class Entity1 {
+class User {
 
   @PrimaryKey({ type: 'bigserial' })
   id!: string;
@@ -10,29 +10,6 @@ class Entity1 {
     this.id = id;
   }
 
-}
-
-@Entity()
-class Entity2 {
-
-  @PrimaryKey({ autoincrement: true, type: 'number' })
-  id!: string;
-
-}
-
-@Entity()
-class Entity3 {
-
-  @PrimaryKey({ autoincrement: true, type: 'int' })
-  id!: string;
-
-}
-
-@Entity()
-class Entity4 {
-
-  @PrimaryKey({ autoincrement: true, type: 'Integer' })
-  id!: string;
 
 }
 
@@ -41,12 +18,7 @@ let orm: MikroORM;
 beforeAll(async () => {
   orm = await MikroORM.init({
     dbName: ':memory:',
-    entities: [
-      Entity1,
-      Entity2,
-      Entity3,
-      Entity4,
-    ],
+    entities: [User],
   });
   await orm.schema.refreshDatabase();
 });
@@ -59,17 +31,7 @@ test('6095', async () => {
   const sql = await orm.schema.getUpdateSchemaSQL();
   expect(sql).toBe('');
 
-  await orm.em.persistAndFlush([
-    new Entity1('id1'),
-    new Entity2(),
-    new Entity3(),
-    new Entity4(),
-  ]);
-  const counts = await Promise.all([
-    orm.em.count(Entity1),
-    orm.em.count(Entity2),
-    orm.em.count(Entity3),
-    orm.em.count(Entity4),
-  ]);
-  expect(counts).toEqual([1, 1, 1, 1]);
+  await orm.em.persistAndFlush(new User('id'));
+  const count = await orm.em.count(User);
+  expect(count).toBe(1);
 });
