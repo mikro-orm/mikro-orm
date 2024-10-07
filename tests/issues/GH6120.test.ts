mikro-orm/mikro-orm

commit cd7ce829a07c844b16430056b032db6947e07356

fix(core): fix same-second dates diffing (#6094)
This small PR fixes a minor regression that I think was introduced in
https://github.com/mikro-orm/mikro-orm/commit/55df57ff1aa84e5d45188d849ba09e91ae6d3642.

## Issue
When updating an entity and changing a date property to another one very
close to it (by a difference in microseconds), the change is not
persisted in the database. Indeed, diffing dates by comparing the
`toString()` output doesn't give enough precision to compare
microseconds shifts. Example:
- `toString()` output: `Tue Oct 01 2024 08:54:48 GMT+0000 (Coordinated
Universal Time)`
- `toISOString()` output: `2024-10-01T08:54:48.651Z`

Two dates can have the same `toString()` output even though their
`toISOString()` output is different.

The impact is low and probably only concerns the test scenarios: It's
rare to persist the same entity twice in the same second without
changing anything other than a date.

## Fix
In this PR I'm switching the dates comparison to use `toISOString()` and
adding a new unit test.

---------

Co-authored-by: Martin Adámek <banan23@gmail.com>

packages/core/src/utils/Utils.ts
@@ -61,10 +61,13 @@ export function compareObjects(a: any, b: any) {
     return a.sql === b.sql && compareArrays(a.params, b.params);
   }
 
+  if ((a instanceof Date && b instanceof Date)) {
+    return a.toISOString() === b.toISOString();
+  }
+
   if (
     (typeof a === 'function' && typeof b === 'function') ||
     (typeof a === 'object' && a.client && ['Ref', 'Raw'].includes(a.constructor.name) && typeof b === 'object' && b.client && ['Ref', 'Raw'].includes(b.constructor.name)) || // knex qb
-    (a instanceof Date && b instanceof Date) ||
     (a instanceof RegExp && b instanceof RegExp) ||
     (a instanceof String && b instanceof String) ||
     (a instanceof Number && b instanceof Number)

tests/Utils.test.ts
@@ -88,6 +88,7 @@ describe('Utils', () => {
     expect(compareObjects(sql`select 1`, sql`select 1`)).toBe(true);
     expect(compareObjects(sql`select ${1}`, sql`select ${1}`)).toBe(true);
     expect(compareObjects(sql`select ${1}`, sql`select ${2}`)).toBe(false);
+    expect(compareObjects(new Date('2024-10-01T08:54:48.651Z'), new Date('2024-10-01T08:54:48.676Z'))).toBe(false);
     expect(Utils.equals(NaN, NaN)).toBe(true);
   });
 

docker-compose.yml
@@ -61,6 +61,8 @@ services:
       MSSQL_SA_PASSWORD: Root.Root
       MSSQL_TELEMETRY_ENABLED: 0
       ACCEPT_EULA: 1
+    healthcheck:
+      test: /opt/mssql-tools/bin/sqlcmd -S mssql -U sa -P 'Root.Root' -Q 'select 1'
 
 volumes:
   mysql8:

tests/features/embeddables/GH2391-2.test.ts
@@ -119,7 +119,7 @@ describe('onCreate and onUpdate in embeddables (GH 2283 and 2391)', () => {
     const tmp1 = line.fooAudit1.archived = new Date(1698010995749);
     await orm.em.flush();
     expect(mock).toHaveBeenCalledTimes(3);
-    expect(mock.mock.calls[1][0]).toMatch('update `my_entity` set `foo_audit1_archivedAt` = 1698010995749, `foo_audit1_updated_at` = 1698010995749, `foo_audit1_nested_audit1_updated_at` = 1698010995749 where `id` = 1');
+    expect(mock.mock.calls[1][0]).toMatch('update `my_entity` set `foo_audit1_archivedAt` = 1698010995749, `foo_audit1_updated_at` = 1698010995749, `foo_audit1_nested_audit1_updated_at` = 1698010995749, `bar_audit2` = \'{"updatedAt":"2023-10-22T21:43:15.749Z","created":"2023-10-22T21:43:15.740Z","nestedAudit1":{"updatedAt":"2023-10-22T21:43:15.749Z","created":"2023-10-22T21:43:15.740Z"}}\' where `id` = 1');
     mock.mockReset();
 
     const tmp2 = line.barAudit2.archived = new Date(1698010995750);

tests/features/embeddables/GH2391.test.ts
@@ -92,7 +92,7 @@ describe('onCreate and onUpdate in embeddables (GH 2283 and 2391)', () => {
     const tmp1 = line.audit1.archived = new Date();
     await orm.em.flush();
     expect(mock).toHaveBeenCalledTimes(3);
-    expect(mock.mock.calls[1][0]).toMatch('update `my_entity` set `audit1_archived` = ?, `audit1_updated` = ?, `audit1_nested_audit1_updated` = ? where `id` = ?');
+    expect(mock.mock.calls[1][0]).toMatch('update `my_entity` set `audit1_archived` = ?, `audit1_updated` = ?, `audit1_nested_audit1_updated` = ?, `audit2` = ? where `id` = ?');
     mock.mockReset();
 
     const tmp2 = line.audit2.archived = new Date();

commit 1aa940bbd2b1eeddfb3d662c740b346e72e4d74b

fix(core): improve error handling for comparing invalid dates (#6120)
This PR fixes an issue I introduced in
https://github.com/mikro-orm/mikro-orm/pull/6094

Using `toISOString()` on an invalid date throws a [RangeError
exception](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString#exceptions).
We should test if the date is valid before comparing it with
`toISOString()`.

packages/core/src/utils/Utils.ts
@@ -62,7 +62,12 @@ export function compareObjects(a: any, b: any) {
   }
 
   if ((a instanceof Date && b instanceof Date)) {
-    return a.toISOString() === b.toISOString();
+    const timeA = a.getTime();
+    const timeB = b.getTime();
+    if (isNaN(timeA) || isNaN(timeB)) {
+      throw new Error('Comparing invalid dates is not supported');
+    }
+    return timeA === timeB;
   }
 
   if (

tests/Utils.test.ts
@@ -88,7 +88,10 @@ describe('Utils', () => {
     expect(compareObjects(sql`select 1`, sql`select 1`)).toBe(true);
     expect(compareObjects(sql`select ${1}`, sql`select ${1}`)).toBe(true);
     expect(compareObjects(sql`select ${1}`, sql`select ${2}`)).toBe(false);
+    expect(compareObjects(new Date('2024-10-01T08:54:48.651Z'), new Date('2024-10-01T08:54:48.651Z'))).toBe(true);
     expect(compareObjects(new Date('2024-10-01T08:54:48.651Z'), new Date('2024-10-01T08:54:48.676Z'))).toBe(false);
+    expect(() => compareObjects(new Date('2024-10-01T08:54:48.651Z'), new Date(NaN))).toThrowError('Comparing invalid dates is not supported');
+    expect(() => compareObjects(new Date(NaN), new Date(NaN))).toThrowError('Comparing invalid dates is not supported');
     expect(Utils.equals(NaN, NaN)).toBe(true);
   });
 

tests/issues/GH6120.test.ts
@@ -1,0 +1,51 @@
+import { Entity, MikroORM, ObjectId, PrimaryKey, Property, SerializedPrimaryKey } from '@mikro-orm/mongodb';
+
+interface Event {
+  name: string;
+  date: Date;
+}
+
+@Entity()
+class User {
+
+  @PrimaryKey()
+  _id!: ObjectId;
+
+  @SerializedPrimaryKey()
+  id!: string;
+
+  @Property({ unique: true })
+  email!: string;
+
+  @Property({ type: 'json' })
+  events: Event[] = [];
+
+}
+
+let orm: MikroORM;
+
+beforeAll(async () => {
+  orm = await MikroORM.init({
+    dbName: '6120',
+    entities: [User],
+    driverOptions: {},
+  });
+  await orm.schema.refreshDatabase();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+test('MongoDB driver should throw when updating a date to an invalid value in a JSON properties', async () => {
+  orm.em.create(User, {
+    email: 'foo',
+    events: [{ name: 'creation', date: new Date() }],
+  });
+  await orm.em.flush();
+  orm.em.clear();
+
+  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
+  user.events[0].date = new Date(undefined as any);
+  await expect(orm.em.flush()).rejects.toThrowError('Comparing invalid dates is not supported');
+});