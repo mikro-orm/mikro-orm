b3d6e789ed1666f0f9e90addf2d291d4bc2930e5
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2024-10-14 07:06:53 +0200

    test(entity-generator): add `SmallDateTimeType` test

---
+++ tests/features/entity-generator/SmallDateTimeType.mssql.test.ts
@@ -0,0 +1,35 @@
+import { MikroORM } from '@mikro-orm/mssql';
+import { EntityGenerator } from '@mikro-orm/entity-generator';
+
+const schema = `
+  CREATE XML SCHEMA COLLECTION sampleCollection AS '
+<schema xmlns="http://www.w3.org/2001/XMLSchema">
+      <element name="root" type="string"/>
+</schema>
+';
+
+  CREATE TABLE [test] (
+    [id] int identity(1,1) not null primary key,
+    [at] smalldatetime default current_timestamp
+    );
+`;
+
+test('small-date-time-type', async () => {
+  const orm = await MikroORM.init({
+    dbName: 'small-date-time-type-test',
+    password: 'Root.Root',
+    discovery: {
+      warnWhenNoEntities: false,
+    },
+    ensureDatabase: false,
+    extensions: [EntityGenerator],
+  });
+
+  if (await orm.schema.ensureDatabase({ create: true })) {
+    await orm.schema.execute(schema);
+  }
+
+  const dump = await orm.entityGenerator.generate();
+  expect(dump).toMatchSnapshot();
+  await orm.close(true);
+});
