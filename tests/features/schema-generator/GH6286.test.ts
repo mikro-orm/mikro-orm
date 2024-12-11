9a458774072cae4c469bf86809759a9e20f419dc
lsh4711 <dowkwll@naver.com>
2024-12-11 19:29:10 +0900

    refactor(test): add test to ensure support for other drivers

--- tests/features/schema-generator/GH6286.test.ts
+++ tests/features/schema-generator/GH6286.test.ts
@@ -1,33 +1,46 @@
-import { Entity, MikroORM, PrimaryKey } from '@mikro-orm/mysql';
+import { Entity, type IDatabaseDriver, MikroORM, PrimaryKey, Utils } from '@mikro-orm/core';
+import { PLATFORMS } from '../../bootstrap';
 
 @Entity()
 class TestEntity {
 
-  @PrimaryKey({ comment: `This
+  @PrimaryKey({
+    comment: `This
 is
 a
-comment` })
+comment`,
+  })
   id!: bigint;
 
 }
 
-let orm: MikroORM;
+const options = {
+  mysql: { port: 3308 },
+  mariadb: { port: 3309 },
+  mssql: { port: 1433, password: 'Root.Root' },
+  postgresql: {},
+};
 
-beforeAll(async () => {
-  orm = await MikroORM.init({
-    dbName: '6286',
-    port: 3308,
-    entities: [TestEntity],
+describe.each(Utils.keys(options))('6286 [%s]', type => {
+  let orm: MikroORM;
+
+  beforeAll(async () => {
+    orm = await MikroORM.init<IDatabaseDriver>({
+      dbName: '6286',
+      entities: [TestEntity],
+      driver: PLATFORMS[type],
+      ...options[type],
+    });
+    await orm.schema.refreshDatabase();
   });
-  await orm.schema.refreshDatabase();
-});
 
-afterAll(async () => {
-  await orm.schema.dropDatabase();
-  await orm.close(true);
-});
+  afterAll(async () => {
+    await orm.schema.dropDatabase();
+    await orm.close(true);
+  });
 
-test('6286', async () => {
-  const sources = await orm.entityGenerator.generate();
-  expect(sources[0]).toMatch(`This\nis\na\ncomment`);
+  test('6286', async () => {
+    const sources = await orm.entityGenerator.generate();
+    expect(sources[0]).toMatch(`This\nis\na\ncomment`);
+  });
 });
