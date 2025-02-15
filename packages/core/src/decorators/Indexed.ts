2886e1c42961e4bebb1e5064cdb79459636f781e
cAttte <26514199+cAttte@users.noreply.github.com>
2025-02-15 07:22:40 -0300

    fix(decorators): use AutoPath in Indexed

--- packages/core/src/decorators/Indexed.ts
+++ packages/core/src/decorators/Indexed.ts
@@ -1,9 +1,9 @@
 import { MetadataStorage } from '../metadata';
-import type { EntityClass, Dictionary } from '../typings';
+import type { EntityClass, Dictionary, AutoPath } from '../typings';
 import { Utils } from '../utils/Utils';
 import type { DeferMode } from '../enums';
 
-function createDecorator<T extends object>(options: IndexOptions<T> | UniqueOptions<T>, unique: boolean) {
+function createDecorator<T extends object>(options: IndexOptions<T, string> | UniqueOptions<T, string>, unique: boolean) {
   return function (target: T, propertyName?: T extends EntityClass<unknown> ? undefined : keyof T) {
     const meta = MetadataStorage.getMetadataFromDecorator(propertyName ? target.constructor : target);
     options.properties ??= propertyName;
@@ -18,26 +18,27 @@ function createDecorator<T extends object>(options: IndexOptions<T> | UniqueOpti
   };
 }
 
-export function Index<T extends object>(options: IndexOptions<T> = {}) {
+export function Index<T extends object, H extends string>(options: IndexOptions<T, H> = {}) {
   return createDecorator(options, false);
 }
 
-export function Unique<T extends object>(options: UniqueOptions<T> = {}) {
+export function Unique<T extends object, H extends string>(options: UniqueOptions<T, H> = {}) {
   return createDecorator(options, true);
 }
 
-type Properties<T> = keyof T | (keyof T)[];
-interface BaseOptions<T> {
+type MaybeArray<T> = T | T[];
+type Properties<T, H extends string> = MaybeArray<AutoPath<T, H>>;
+interface BaseOptions<T, H extends string> {
   name?: string;
-  properties?: T extends EntityClass<infer P> ? Properties<P> : Properties<T>;
+  properties?: (T extends EntityClass<infer P> ? Properties<P, H> : Properties<T, H>);
   options?: Dictionary;
   expression?: string;
 }
 
-export interface UniqueOptions<T> extends BaseOptions<T> {
+export interface UniqueOptions<T, H extends string = string> extends BaseOptions<T, H> {
   deferMode?: DeferMode | `${DeferMode}`;
 }
 
-export interface IndexOptions<T> extends BaseOptions<T> {
+export interface IndexOptions<T, H extends string = string> extends BaseOptions<T, H> {
   type?: string;
 }
