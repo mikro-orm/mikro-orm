b5932b7c9d9c6b405b08918ccea4b77c78a79a06
Amadeus Alfa <amadeus@torwell.de>
2025-03-20 14:42:06 +0100

    WIP: fix test

--- tests/features/embeddables/embedded-entities.mongo.test.ts
+++ tests/features/embeddables/embedded-entities.mongo.test.ts
@@ -252,7 +252,7 @@ describe('embedded entities in mongo', () => {
     const createCollection = jest.spyOn(MongoConnection.prototype, 'createCollection');
     createCollection.mockResolvedValue({} as any);
     await orm.schema.createSchema();
-    expect(createCollection.mock.calls.map(c => c[0])).toEqual(['custom-user', 'parent', 'user', 'mikro_orm_migrations']);
+    expect(createCollection.mock.calls.map(c => c[0])).toEqual(['custom-user', 'parent', 'user']);
     createCollection.mockRestore();
   });
 
