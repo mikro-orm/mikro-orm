859c4e1f0a5491b2cd3e10e103f4a71e0e5e1539
Martin Adámek <banan23@gmail.com>
2025-04-18 19:58:14 +0200

    feat(core): propagate persist stack in `em.transactional` fork
    Closes #6381

--- packages/core/src/EntityManager.ts
+++ packages/core/src/EntityManager.ts
@@ -1965,6 +1965,10 @@ export class EntityManager<Driver extends IDatabaseDriver = IDatabaseDriver> {
         fork.unitOfWork.register(entity);
       }
 
+      for (const entity of em.unitOfWork.getPersistStack()) {
+        fork.unitOfWork.persist(entity);
+      }
+
       for (const entity of em.unitOfWork.getOrphanRemoveStack()) {
         fork.unitOfWork.getOrphanRemoveStack().add(entity);
       }
