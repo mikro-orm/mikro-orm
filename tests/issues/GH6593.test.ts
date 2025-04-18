980e53e3879ce009de7db4a1cac3abf969ca11c0
Martin Adámek <banan23@gmail.com>
2025-04-18 12:13:31 +0200

    fix(core): don't remove generated primary keys from entity snapshot
    Closes #6593

---
+++ tests/issues/GH6593.test.ts
@@ -0,0 +1,66 @@
+import {
+  Entity,
+  helper,
+  MikroORM,
+  Opt,
+  PrimaryKey,
+  Property,
+} from '@mikro-orm/postgresql';
+
+@Entity()
+class User {
+
+  @PrimaryKey({ type: 'integer', generated: 'by default as identity' })
+  id!: number & Opt;
+
+  @Property()
+  name: string;
+
+  @Property({ unique: true })
+  email: string;
+
+  constructor(name: string, email: string) {
+    this.name = name;
+    this.email = email;
+  }
+
+}
+
+let orm: MikroORM;
+
+beforeAll(async () => {
+  orm = await MikroORM.init({
+    dbName: '6593',
+    entities: [User],
+  });
+  await orm.schema.refreshDatabase();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+test('6593', async () => {
+  let em = orm.em.fork();
+  em.create(User, { name: 'Foo', email: 'foo' });
+  em.create(User, { name: 'Foo1', email: 'foo1' });
+  em.create(User, { name: 'Foo2', email: 'foo2' });
+  await em.flush();
+
+  em = orm.em.fork();
+  const user = await em.findOneOrFail(User, { email: 'foo' });
+  expect(user.name).toBe('Foo');
+  user.name = 'Bar';
+  expect(helper(user).__originalEntityData).toEqual({ id: 1, name: 'Foo', email: 'foo' });
+  await em.flush();
+  expect(helper(user).__originalEntityData).toEqual({ id: 1, name: 'Bar', email: 'foo' });
+
+  const user1 = await em.findOneOrFail(User, { email: 'foo' });
+
+  user1.name = 'Foo';
+  await em.flush();
+  em.clear();
+
+  const user2 = await em.findOneOrFail(User, { email: 'foo1' });
+  expect(user2.name).toBe('Foo1');
+});
