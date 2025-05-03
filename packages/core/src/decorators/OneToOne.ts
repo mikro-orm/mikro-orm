9680c1c7e288e83f8503d79c9a6073fc4cc0f9a7
Thomas Berdy <Tberdy@hotmail.fr>
2025-05-03 10:59:57 +0200

    feat(schema): allow setting a custom FK constraint name (#6623)
    Closes #6624

--- packages/core/src/decorators/OneToOne.ts
+++ packages/core/src/decorators/OneToOne.ts
@@ -39,4 +39,7 @@ export interface OneToOneOptions<Owner, Target> extends Partial<Omit<OneToManyOp
 
   /** Set the constraint type. Immediate constraints are checked for each statement, while deferred ones are only checked at the end of the transaction. Only for postgres unique constraints. */
   deferMode?: DeferMode | `${DeferMode}`;
+
+  /** Set a custom foreign key constraint name, overriding NamingStrategy.indexName(). */
+  foreignKeyName?: string;
 }
