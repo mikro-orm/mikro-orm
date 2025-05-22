01c9cbc16749a2a26748e16e176407c3e4c27d60
Jordan B <j0rd4n68@gmail.com>
2025-05-22 08:58:37 +0200

    refactor: only add temporary table when insert query actually returns fields (#6671)
    Co-authored-by: Jordan BAO <jordan.bao@spurgin.fr>
    Co-authored-by: Martin Adámek <banan23@gmail.com>

--- tests/platforms/output-statements.test.ts
+++ tests/platforms/output-statements.test.ts
@@ -24,12 +24,23 @@ class WithoutTriggers {
 
 }
 
+@Entity()
+class WithoutAutoincrement {
+
+  @PrimaryKey({ autoincrement: false })
+  id!: number;
+
+  @Property({ type: 'string', length: 255, nullable: true })
+  value?: string | null;
+
+}
+
 describe('Output statements [mssql]', () => {
   let orm: MikroORM;
 
   beforeAll(async () => {
     orm = await MikroORM.init({
-      entities: [WithTriggers, WithoutTriggers],
+      entities: [WithTriggers, WithoutTriggers, WithoutAutoincrement],
       driver: MsSqlDriver,
       dbName: 'order-by',
       loggerFactory: SimpleLogger.create,
@@ -56,9 +67,14 @@ describe('Output statements [mssql]', () => {
     withTriggersAndIdentifyInsert.id = 2;
     withTriggersAndIdentifyInsert.value = 'entity with triggers and identity insert';
 
+    const withoutAutoincrement = new WithoutAutoincrement();
+    withoutAutoincrement.id = 1;
+    withoutAutoincrement.value = 'entity without autoincrement';
+
     await orm.em.insert(withTriggers);
     await orm.em.insert(withoutTriggers);
     await orm.em.insert(withTriggersAndIdentifyInsert);
+    await orm.em.insert(withoutAutoincrement);
 
     orm.em.clear();
 
@@ -67,6 +83,7 @@ describe('Output statements [mssql]', () => {
     expect(mock.mock.calls[0][0]).toMatch('[query] select top(0) [t].[id] into #out from [with_triggers] as t left join [with_triggers] on 0 = 1; insert into [with_triggers] ([value]) output inserted.[id] into #out values (N\'entity with triggers\'); select [t].[id] from #out as t; drop table #out');
     expect(mock.mock.calls[1][0]).toMatch('[query] insert into [without_triggers] ([value]) output inserted.[id] values (N\'entity without triggers\')');
     expect(mock.mock.calls[2][0]).toMatch('[query] set identity_insert [with_triggers] on; insert into [with_triggers] ([id], [value]) values (2, N\'entity with triggers and identity insert\'); set identity_insert [with_triggers] off');
+    expect(mock.mock.calls[3][0]).toMatch('[query] insert into [without_autoincrement] ([id], [value]) values (1, N\'entity without autoincrement\')');
     expect(withTriggersReselected?.id).toBe(withTriggers.id); // ensure select from #out table works.
   });
 
@@ -88,8 +105,14 @@ describe('Output statements [mssql]', () => {
       .insert({ id: 2, value: 'entity with triggers' })
       .execute();
 
+    await orm.em
+      .createQueryBuilder(WithoutAutoincrement)
+      .insert({ id: 1, value: 'entity without autoincrement' })
+      .execute();
+
     expect(mock.mock.calls[0][0]).toMatch('[query] select top(0) [t].[id] into #out from [with_triggers] as t left join [with_triggers] on 0 = 1; insert into [with_triggers] ([value]) output inserted.[id] into #out values (N\'entity with triggers\'); select [t].[id] from #out as t; drop table #out');
     expect(mock.mock.calls[1][0]).toMatch('[query] insert into [without_triggers] ([value]) output inserted.[id] values (N\'entity with triggers\')');
     expect(mock.mock.calls[2][0]).toMatch('[query] set identity_insert [with_triggers] on; insert into [with_triggers] ([id], [value]) values (2, N\'entity with triggers\'); set identity_insert [with_triggers] off;');
+    expect(mock.mock.calls[3][0]).toMatch('[query] insert into [without_autoincrement] ([id], [value]) values (1, N\'entity without autoincrement\')');
   });
 });
