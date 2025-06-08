5df50b750b1091d05aba6d1853a16118dd47a2c6
Martin Adámek <banan23@gmail.com>
2025-06-08 10:43:25 +0200

    docs: fix docs build

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -121,7 +121,16 @@ module.exports = {
   onBrokenMarkdownLinks: 'throw',
   onDuplicateRoutes: 'throw',
   future: {
-    experimental_faster: true,
+    experimental_faster: {
+      swcJsLoader: true,
+      swcJsMinimizer: true,
+      swcHtmlMinimizer: true,
+      lightningCssMinimizer: true,
+      mdxCrossCompilerCache: true,
+      rspackBundler: true,
+      rspackPersistentCache: true,
+      ssgWorkerThreads: false,
+    },
     v4: {
       removeLegacyPostBuildHeadAttribute: true,
       useCssCascadeLayers: false, // this breaks styles on homepage and link colors everywhere
