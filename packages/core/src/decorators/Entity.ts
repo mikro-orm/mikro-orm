dc0e90edb5f0c7a6a375986d8df4df5bae4547a1
Martin Adámek <banan23@gmail.com>
2025-07-27 11:35:54 +0200

    docs: add jsdoc to `EntityOptions`
    Related: #6775

--- packages/core/src/decorators/Entity.ts
+++ packages/core/src/decorators/Entity.ts
@@ -16,21 +16,34 @@ export function Entity<T extends EntityClass<unknown>>(options: EntityOptions<T>
 }
 
 export type EntityOptions<T, E = T extends EntityClass<infer P> ? P : T> = {
+  /** Override default collection/table name. Alias for `collection`. */
   tableName?: string;
+  /** Sets the schema name. */
   schema?: string;
+  /** Override default collection/table name. Alias for `tableName`. */
   collection?: string;
+  /** For {@doclink inheritance-mapping#single-table-inheritance | Single Table Inheritance}. */
   discriminatorColumn?: (T extends EntityClass<infer P> ? keyof P : string) | AnyString;
+  /** For {@doclink inheritance-mapping#single-table-inheritance | Single Table Inheritance}. */
   discriminatorMap?: Dictionary<string>;
+  /** For {@doclink inheritance-mapping#single-table-inheritance | Single Table Inheritance}. */
   discriminatorValue?: number | string;
+  /**	Enforce use of constructor when creating managed entity instances. */
   forceConstructor?: boolean;
+  /** Specify comment to table. (SQL only) */
   comment?: string;
+  /**	Marks entity as abstract, such entities are inlined during discovery. */
   abstract?: boolean;
+  /** Disables change tracking - such entities are ignored during flush. */
   readonly?: boolean;
+  /** Marks entity as {@doclink virtual-entities | virtual}. This is set automatically when you use `expression` option. */
   virtual?: boolean;
-  // used to make ORM aware of externally defined triggers, can change resulting SQL in some condition like when inserting in mssql
+  /** Used to make ORM aware of externally defined triggers. This is needed for MS SQL Server multi inserts, ignored in other dialects. */
   hasTriggers?: boolean;
   // we need to use `em: any` here otherwise an expression would not be assignable with more narrow type like `SqlEntityManager`
   // also return type is unknown as it can be either QB instance (which we cannot type here) or array of POJOs (e.g. for mongodb)
+  /** SQL query that maps to a {@doclink virtual-entities | virtual entity}. */
   expression?: string | ((em: any, where: ObjectQuery<E>, options: FindOptions<E, any, any, any>) => object);
+  /** Set {@doclink repositories#custom-repository | custom repository class}. */
   repository?: () => Constructor;
 };
