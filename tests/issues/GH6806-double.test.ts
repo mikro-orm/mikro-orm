3282908d891739fe645605d35d36168a02e58336
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2025-08-21 15:59:23 +0200

    test: add test for `DoubleType`

---
+++ tests/issues/GH6806-double.test.ts
@@ -0,0 +1,43 @@
+import {
+  DoubleType,
+  Entity,
+  MikroORM,
+  PrimaryKey,
+  Property,
+} from '@mikro-orm/sqlite';
+
+@Entity()
+class Amount {
+
+  @PrimaryKey()
+  id!: number;
+
+  @Property({ type: DoubleType })
+  amount!: string;
+
+}
+
+let orm: MikroORM;
+
+beforeAll(async () => {
+  orm = await MikroORM.init({
+    dbName: ':memory:',
+    entities: [Amount],
+    debug: ['query', 'query-params'],
+    allowGlobalContext: true, // only for testing
+  });
+  await orm.schema.refreshDatabase();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+test('double returned as number', async () => {
+  orm.em.create(Amount, { amount: '12.3' });
+  await orm.em.flush();
+  orm.em.clear();
+
+  const { amount } = await orm.em.findOneOrFail(Amount, { amount: '12.3' });
+  expect(amount).toBe('12.3');
+});
