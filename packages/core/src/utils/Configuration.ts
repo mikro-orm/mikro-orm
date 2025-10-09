fea8ce3ff94c98e93c3ea3920aef318204675a68
Martin Adámek <banan23@gmail.com>
2025-10-09 13:45:34 +0200

    refactor: revert removal of `dynamicImportProvider` since it is still needed for vitest

--- packages/core/src/utils/Configuration.ts
+++ packages/core/src/utils/Configuration.ts
@@ -173,6 +173,7 @@ export class Configuration<D extends IDatabaseDriver = IDatabaseDriver, EM exten
       fileName: (className: string) => className,
     },
     preferReadReplicas: true,
+    dynamicImportProvider: /* v8 ignore next */ (id: string) => import(id),
   } satisfies MikroORMOptions;
 
   private readonly options: MikroORMOptions<D, EM>;
@@ -183,6 +184,10 @@ export class Configuration<D extends IDatabaseDriver = IDatabaseDriver, EM exten
   private readonly extensions = new Map<string, () => unknown>();
 
   constructor(options: Options, validate = true) {
+    if (options.dynamicImportProvider) {
+      Utils.setDynamicImportProvider(options.dynamicImportProvider);
+    }
+
     this.options = Utils.mergeConfig({} as MikroORMOptions<D, EM>, Configuration.DEFAULTS, options);
     this.options.baseDir = Utils.absolutePath(this.options.baseDir);
     this.options.preferTs ??= options.preferTs;
@@ -679,6 +684,7 @@ export interface MikroORMOptions<D extends IDatabaseDriver = IDatabaseDriver, EM
   metadataProvider: { new(config: Configuration): MetadataProvider };
   seeder: SeederOptions;
   preferReadReplicas: boolean;
+  dynamicImportProvider: (id: string) => Promise<unknown>;
   hashAlgorithm: 'md5' | 'sha256';
 }
 
