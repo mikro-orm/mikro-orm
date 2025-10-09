ff360b0b3dc83c1b63b38d2fa2e818aa351e6005
Martin Adámek <banan23@gmail.com>
2025-10-09 17:23:03 +0200

    fix(schema): support raw fragments in check constraint expressions
    Closes #6914

---
+++ tests/features/schema-generator/GH6914.test.ts
@@ -0,0 +1,66 @@
+import { Entity, PrimaryKey, Property, Check, MikroORM, quote } from '@mikro-orm/postgresql';
+
+@Entity({ tableName: 'user' })
+class User0 {
+
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  @Check({
+    name: 'name_max_length',
+    expression: c => quote`char_length(${c.name}) <= 123`,
+  })
+  name!: string;
+
+}
+
+@Entity({ tableName: 'user' })
+class User1 {
+
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  @Check({
+    name: 'name_max_length',
+    expression: c => quote`char_length(${c.name}) <= 123`,
+  })
+  name!: string;
+
+}
+
+let orm: MikroORM;
+
+beforeAll(async () => {
+  orm = await MikroORM.init({
+    entities: [User0],
+    dbName: 'mikro_orm_test_gh_6914',
+  });
+  await orm.schema.ensureDatabase();
+  await orm.schema.dropSchema();
+});
+
+afterAll(() => orm.close(true));
+
+test('GH #6914', async () => {
+  const testMigration = async (e1: any, e2: any, snap: string) => {
+    if (e2) {
+      orm.discoverEntity(e2, e1.name);
+    }
+
+    const diff = await orm.schema.getUpdateSchemaMigrationSQL({ wrap: false });
+    expect(diff).toMatchSnapshot(snap);
+    await orm.schema.execute(diff.up);
+
+    return diff.down;
+  };
+
+  const down: string[] = [];
+  down.push(await testMigration(User0, undefined, '0. create schema'));
+  down.push(await testMigration(User0, User1, '1. check no updates'));
+
+  for (const sql of down.reverse()) {
+    await orm.schema.execute(sql);
+  }
+});
