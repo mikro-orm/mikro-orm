8fab4196a62784eeee15f9054049ba9dc1b7a272
xcfox <xcfox@qq.com>
2025-10-22 22:52:48 +0800

    feat(tests): enhance InferKyselyDB tests with async schema generation and primary key definition

--- tests/features/infer-kysely-database.test.ts
+++ tests/features/infer-kysely-database.test.ts
@@ -1,8 +1,9 @@
 import { defineEntity, p } from '@mikro-orm/core';
 import { InferKyselyDB } from '@mikro-orm/knex';
+import { MikroORM } from '@mikro-orm/sqlite';
 
 describe('InferKyselyDB', () => {
-  test('infer table and column', () => {
+  test('infer table and column', async () => {
     const User = defineEntity({
       name: 'User',
       properties: {
@@ -17,7 +18,7 @@ describe('InferKyselyDB', () => {
     const UserProfile = defineEntity({
       name: 'UserProfile',
       properties: {
-        user: () => p.oneToOne(User).owner(true),
+        user: () => p.oneToOne(User).owner(true).primary(),
         bio: p.string().nullable(),
         avatar: p.string().nullable(),
         location: p.string().nullable(),
@@ -36,6 +37,24 @@ describe('InferKyselyDB', () => {
 
     type KyselyDB = InferKyselyDB<[typeof User, typeof UserProfile, typeof Post]>;
     type UserTable = KyselyDB['user'];
+
+    const orm = MikroORM.initSync({
+      entities: [User, UserProfile, Post],
+      dbName: ':memory:',
+    });
+
+    const generator = orm.schema;
+    const createDump = await generator.getCreateSchemaSQL();
+    expect(createDump).toMatchInlineSnapshot(`
+      "create table \`user_profile\` (\`user_full_name\` text not null primary key, \`bio\` text null, \`avatar\` text null, \`location\` text null, constraint \`user_profile_user_full_name_foreign\` foreign key (\`user_full_name\`) references \`user\` (\`full_name\`) on update cascade on delete cascade);
+
+      create table \`user\` (\`full_name\` text not null primary key, \`email\` text null, \`first_name\` text not null, \`the_last_name\` text not null, \`profile_user_full_name\` text not null, constraint \`user_profile_user_full_name_foreign\` foreign key (\`profile_user_full_name\`) references \`user_profile\` (\`user_full_name\`) on update cascade);
+      create unique index \`user_profile_user_full_name_unique\` on \`user\` (\`profile_user_full_name\`);
+
+      create table \`post\` (\`id\` integer not null primary key autoincrement, \`title\` text not null, \`description\` text not null, \`author_full_name\` text not null, constraint \`post_author_full_name_foreign\` foreign key (\`author_full_name\`) references \`user\` (\`full_name\`) on update cascade);
+      create index \`post_author_full_name_index\` on \`post\` (\`author_full_name\`);
+      "
+    `);
 
     expectTypeOf<UserTable>().toEqualTypeOf<{
       full_name: string;
