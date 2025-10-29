2c60a0d4f04325d8940cbb95d3f9d840fcc9e128
xcfox <xcfox@qq.com>
2025-10-30 01:42:18 +0800

    refactor(plugin): rename MikroORMTransformer to MikroTransformer for consistency

--- packages/knex/src/plugin/transformer.ts
+++ packages/knex/src/plugin/transformer.ts
@@ -2,7 +2,7 @@ import type { EntityMetadata, EntityProperty } from '@mikro-orm/core';
 import { type ColumnNode, type QueryId, OperationNodeTransformer, TableNode } from 'kysely';
 import type { MikroPluginOptions } from '.';
 
-export class MikroORMTransformer extends OperationNodeTransformer {
+export class MikroTransformer extends OperationNodeTransformer {
 
   constructor(protected readonly entityMap: Map<string, EntityMetadata>, protected readonly options: Pick<MikroPluginOptions, 'columnNamingStrategy' | 'tableNamingStrategy'> = {}) {
     super();
