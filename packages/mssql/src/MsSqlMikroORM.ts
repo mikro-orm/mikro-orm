274fe077b0a1f3a94c48b90b2db137495d5bb806
Martin Adámek <banan23@gmail.com>
2025-11-23 10:49:35 +0100

    refactor: simplify folder-based discovery code (#6992)
    This PR streamlines the metadata discovery code, so the folder-based
    discovery only handles one thing: finding all potential exports. The
    discovery process is then unified.

--- packages/mssql/src/MsSqlMikroORM.ts
+++ packages/mssql/src/MsSqlMikroORM.ts
@@ -1,7 +1,6 @@
 import {
   type AnyEntity,
   type EntityClass,
-  type EntityClassGroup,
   type EntitySchema,
   defineConfig,
   MikroORM,
@@ -15,12 +14,12 @@ import { MsSqlDriver } from './MsSqlDriver.js';
 
 export type MsSqlOptions<
   EM extends SqlEntityManager<MsSqlDriver> = SqlEntityManager<MsSqlDriver>,
-  Entities extends (string | EntityClass<AnyEntity> | EntityClassGroup<AnyEntity> | EntitySchema)[] = (string | EntityClass<AnyEntity> | EntityClassGroup<AnyEntity> | EntitySchema)[],
+  Entities extends (string | EntityClass<AnyEntity> | EntitySchema)[] = (string | EntityClass<AnyEntity> | EntitySchema)[],
 > = Options<MsSqlDriver, EM, Entities>;
 
 export function defineMsSqlConfig<
   EM extends SqlEntityManager<MsSqlDriver> = SqlEntityManager<MsSqlDriver>,
-  Entities extends (string | EntityClass<AnyEntity> | EntityClassGroup<AnyEntity> | EntitySchema)[] = (string | EntityClass<AnyEntity> | EntityClassGroup<AnyEntity> | EntitySchema)[],
+  Entities extends (string | EntityClass<AnyEntity> | EntitySchema)[] = (string | EntityClass<AnyEntity> | EntitySchema)[],
 >(options: MsSqlOptions<EM, Entities>) {
   return defineConfig({ driver: MsSqlDriver, ...options });
 }
@@ -30,7 +29,7 @@ export function defineMsSqlConfig<
  */
 export class MsSqlMikroORM<
   EM extends SqlEntityManager<MsSqlDriver> = SqlEntityManager<MsSqlDriver>,
-  Entities extends (string | EntityClass<AnyEntity> | EntityClassGroup<AnyEntity> | EntitySchema)[] = (string | EntityClass<AnyEntity> | EntityClassGroup<AnyEntity> | EntitySchema)[],
+  Entities extends (string | EntityClass<AnyEntity> | EntitySchema)[] = (string | EntityClass<AnyEntity> | EntitySchema)[],
 > extends MikroORM<MsSqlDriver, EM, Entities> {
 
   /**
@@ -39,7 +38,7 @@ export class MsSqlMikroORM<
   static override async init<
     D extends IDatabaseDriver = MsSqlDriver,
     EM extends EntityManager = D[typeof EntityManagerType] & EntityManager,
-    Entities extends (string | EntityClass<AnyEntity> | EntityClassGroup<AnyEntity> | EntitySchema)[] = (string | EntityClass<AnyEntity> | EntityClassGroup<AnyEntity> | EntitySchema)[],
+    Entities extends (string | EntityClass<AnyEntity> | EntitySchema)[] = (string | EntityClass<AnyEntity> | EntitySchema)[],
   >(options: Options<D, EM, Entities>): Promise<MikroORM<D, EM, Entities>> {
     return super.init(defineMsSqlConfig(options as any) as any);
   }
