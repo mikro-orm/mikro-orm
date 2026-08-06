42f1c25767d255e90a0b2361f26b2fb33736e0ee
Vasil Rangelov <boen.robot@gmail.com>
2026-08-06 16:49:03 +0300

    feat(cli): the "--combined" argument of cache:generate can optionally be a path to the cache file (#7963)

--- tests/features/cli/GenerateCacheCommand.test.ts
+++ tests/features/cli/GenerateCacheCommand.test.ts
@@ -1,5 +1,6 @@
+import { existsSync, rmSync } from 'node:fs';
 import { FileCacheAdapter } from '@mikro-orm/core/fs-utils';
-import { MetadataStorage } from '@mikro-orm/core';
+import { DefaultLogger, MetadataStorage } from '@mikro-orm/core';
 import { Configuration, MetadataDiscovery } from '@mikro-orm/core';
 import { CLIHelper } from '@mikro-orm/cli';
 import { GenerateCacheCommand } from '../../../packages/cli/src/commands/GenerateCacheCommand.js';
@@ -40,5 +41,27 @@ describe('GenerateCacheCommand', () => {
     await expect(cmd.handler({ ts: true } as any)).resolves.toBeUndefined();
     expect(discoverMock.mock.calls.length).toBe(2);
     expect(discoverMock.mock.calls[1][0]).toBe(true);
+
+    discoverMock.mockRestore();
+    getConfigurationMock.mockRestore();
+  });
+  test('combined argument', async () => {
+    vi.spyOn(CLIHelper, 'dump').mockImplementation(i => i);
+    vi.spyOn(DefaultLogger, 'create').mockImplementation(
+      options => new DefaultLogger({ ...options, writer: m => ({}) }),
+    );
+
+    const cmd = new GenerateCacheCommand();
+
+    await expect(cmd.handler({ config: ['./tests/cli-config.ts'], combined: '' } as any)).resolves.toBeUndefined();
+    expect(existsSync('./temp/metadata.json')).toBe(true);
+    rmSync('./temp/metadata.json');
+
+    await expect(
+      cmd.handler({ config: ['./tests/cli-config.ts'], combined: './my-metadata.json' } as any),
+    ).resolves.toBeUndefined();
+    expect(existsSync('./temp/metadata.json')).toBe(false);
+    expect(existsSync('./temp/my-metadata.json')).toBe(true);
+    rmSync('./temp/my-metadata.json');
   });
 });
